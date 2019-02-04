04a95a3b3cd900374b065c68d840140955b4d2b4
tim.menzies <tim.menzies@gmail.com>
2019-02-03 23:46:22 -0500

    saving

--- lisp/oo1.lisp
+++ lisp/oo1.lisp
@@ -14,11 +14,12 @@
        (defclass ,x (,parent)
 	 ,(loop for (slot form) in slots collect (uses slot x form))))))
 
-(defmethod slots ((x magic))
+(defmethod slots ((x magic) &optional show)
   (labels 
     ((hide (y)
+        (and (not show)
 	   (and (symbolp y) 
-		(equal (elt (symbol-name y) 0) #\_)))
+		(equal (elt (symbol-name y) 0) #\_))))
      (slots1 (y) ; what are the slots of a class?
 	     #+clisp (class-slots (class-of y))
 	     #+sbcl  (sb-mop:class-slots (class-of y)))
@@ -33,6 +34,7 @@
 
 (defmethod show ((x t)) (print (has x)))
 
+;;; has
 (defmethod has ((x t))    x)
 (defmethod has ((x cons)) (mapcar #'has x))
 (defmethod has ((x magic))  
@@ -40,8 +42,19 @@
 	(mapcar #'(lambda(slot)
 		    `(,slot . ,(has (slot-value x slot))))
 		(slots x))))
+;;; visit
+(defmethod visit ((x t) f) (funcall f x))
+(defmethod visit ((x function) f) (funcall f (format nil "~a" x)))
+(defmethod visit ((x cons) f) (mapcar (lambda(y) (visit y f))  x))
+(defmethod visit ((x magic) f)  
+  (mapcar #'(lambda(slot)
+	      (visit (slot-value x slot) f))
+	  (slots x t)))
 
 (show `(1 2 #'_has "asdas" ,(make-instance 'stuff :b 20)))
 (show `(1 2 #'_has "asdas" ,(stuff :b 20)))
 (show `(1 2 #'_has "asdas" ,(stuff)))
+
+
+(visit `(1 2 #'_has "asdas" ,(stuff :b 20)) #'print)
 
