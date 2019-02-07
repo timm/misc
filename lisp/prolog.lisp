5518ac90217bc84f871d2d633a9cae8fbe1ce197
Tim Menzies <tim.menzies@gmail.com>
2019-02-06 22:57:22 -0500

    saving

---
+++ lisp/prolog.lisp
@@ -0,0 +1,138 @@
+;  vim: set filetype=lisp tabstop=2 shiftwidth=2 expandtab : 
+
+(defun unify (x y &optional binds)
+  (cond 
+    ((eql x y)        (values binds t))
+    ((assoc x binds)  (unify (known x binds) y binds))
+    ((assoc y binds)  (unify x (known y binds) binds))
+    ((var? x)         (values (cons (cons x y) binds) t))
+    ((var? y)         (values (cons (cons y x) binds) t))
+    (t
+      (when (and (consp x) (consp y))
+        (multiple-value-bind (b2 yes) 
+          (unify (car x) (car y) binds)
+          (and yes (unify (cdr x) (cdr y) b2)))))))
+
+(defun var? (x)
+  (and (symbolp x) 
+       (eql (char (symbol-name x) 0) #\?)))
+
+(defun known (x binds)
+  (let ((b (assoc x binds)))
+    (if b
+      (or (known (cdr b) binds)
+          (cdr b)))))
+
+(defvar *rules* (make-hash-table))
+
+;xxx implement some siple utilities
+
+(defmacro <- (con &optional ant)
+  `(length (push (cons (cdr ',con) ',ant)
+                 (gethash (car ',con) *rules*))))
+
+; father not working? why
+; sibling seems brokem... person cant be their own siblings 
+(defun data0 ()
+  (clrhash *rules*)
+  (<- (= ?x ?x))
+  (<- (parent donald nancy))
+  (<- (parent donald debbie))
+  (<- (male donald))
+  (<- (chain ?a ?b)
+         (and (= ?a ?b)
+              (= ?b ?c)
+              (do (xxx ?c))
+              (= ?c 1)))
+  (<- (father ?x ?y) 
+      (and 
+        (parent ?x ?y) 
+        (male ?x)))
+  (<- (sibling ?x ?y) 
+      (and (parent ?z ?x)
+           (parent ?z ?y)
+           (not (= ?x ?y)))))
+
+(defun xxx (x) (format t "[~a]~%" x))
+
+(defun prove (expr &optional binds)
+  (case (car expr)
+    (and  (ands    (reverse (cdr expr)) binds))
+    (or   (ors     (cdr expr) binds))
+    (not  (negation    (cadr expr) binds))
+    (do    (prove-code (cadr expr) binds))
+    (t    (prove1 (car expr) (cdr expr) binds))))
+
+(defun prove-code (expr binds)
+  (labels 
+    ((lets (binds want)
+           (mapcar 
+             #'(lambda (x) 
+                 `(,x ',(known x binds))) 
+             want)))
+    ; (eval code)
+    (eval `(let ,(lets binds (has-vars expr))
+             ,expr))
+    (list binds)))
+
+(defun prove1 (pred args binds)
+  ;(format t "preds ~a args ~a~&" pred args)
+  (mapcan 
+    #'(lambda (r)
+        (multiple-value-bind (b2 yes) 
+          (unify args (car r) 
+                 binds)
+          (when yes
+            (if (cdr r) 
+              (prove (cdr r) b2) 
+              (list b2)))))
+    (mapcar #'renames
+            (gethash pred *rules*))))
+
+(defun renames (r)
+  (sublis (mapcar #'(lambda (v) (cons v (gensym "?")))
+                  (has-vars r))
+          r))
+
+(defun has-vars (expr)
+  (if (atom expr)
+      (if (var? expr) (list expr))
+      (union (has-vars (car expr))
+             (has-vars (cdr expr)))))
+
+(defun ands (goals binds)
+  (if (null goals)
+      (list binds)
+      (mapcan #'(lambda (b)
+                  (prove (car goals) b))
+              (ands (cdr goals) binds))))
+
+(defun ors(goals binds)
+  (mapcan #'(lambda (c) (prove c binds))
+          goals))
+
+(defun negation (goal binds)
+  (unless (prove goal binds)
+    (list binds)))
+
+(defmacro query (question &body body)
+  (let ((binds (gensym)))
+    `(dolist (,binds (prove ',question))
+       (let ,(mapcar #'(lambda (v)
+                         `(,v (known ',v ,binds)))
+         (has-vars question))
+   (declare (ignorable ,@(has-vars question)))
+   ,@body))))
+
+(defun test1 ()
+  (data0)
+  (query 
+    (father ?x ?y)
+    (format t "~A is the father of ~A~%" ?x ?y))
+  (query 
+    (sibling ?x ?y)
+    (format t "~A is the sibling of ~A.~%" ?x ?y))
+  (query 
+    (chain ?x 1)
+    (format t "?x matches to ~A.~%" ?x)))
+
