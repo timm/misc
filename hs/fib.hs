6b096decde0756962e605f8767e2de5533922ac1
Tim Menzies <tim.menzies@gmail.com>
2019-02-14 22:23:20 -0500

    saving

--- hs/fib.hs
+++ hs/fib.hs
@@ -2,6 +2,12 @@ fib 1 = 1
 fib 2 = 2
 fib x = fib (x - 1) + fib (x - 2)
 
---doubles = if x > 100  then x  else 2*x   
+doubles x = if x > 100  then x  else 2*x   
 
-main = putStrLn(show(fib(10)))
+rightTri = [ (a,b,c) | c <- [1..10], 
+                              b <- [1..c], 
+                              a <- [1..b], 
+                              a^2 + b^2 == c^2, a+b+c == 24]  
+-- main = putStrLn(show(doubles(fib(10))))
+main = putStrLn(show(rightTri))
+
