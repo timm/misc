cbda49476479900cfe0864cf7d7ade5d5335f343
tim.menzies <tim.menzies@gmail.com>
2019-04-22 23:48:59 -0400

    saving

--- lua/chi.py
+++ lua/chi.py
@@ -1,28 +1,32 @@
-def chisquare(d1,d2):
-  "returns true if same"
-  vals= [(1,  5.024), ( 3, 9.348), (  6, 14.449), (12,23.337),
-         (25,40.646), (50,71.420), (100,129.561)]
-  def threshold(df): 
-     if df <= vals[ 0][0]: return vals[ 0][1]
-     if df >= vals[-1][0]: return vals[-1][1]
-     df1,x1 = vals[0]
-     for df2,x2 in vals[1:]:
-       if df1 <= df < df2:
-         return x1 + (x2-x1) * (df - df1) / (df2-df1)
-       df1,x1 = df2,x2
-  chi,df = 0,-1
-  for k,e in d1.items():
-    df  += 1
-    o    = d2.get(k,0)
-    chi += (o  - e)**2/ e
-  return chi < threshold(df)
+def interpolate(x,xy): 
+  x1,y1 = xy[0]
+  if x < x1: return y1
+  for x2,y2 in xy[1:]:
+    if x1 <= x < x2:
+      y1 + (y2-y1) * (x - x1) / (x2 - x1)
+    x1,y1 = x2,y2
+  return y2
 
-d1=dict(a=20,b=10,c=60,d=80,e=100,f=10,g=20)
+def countsequal(obs,xpect):
+  x2,df = 0,-1
+  for k in xpect:
+    df += 1
+    e   = xpect[k]
+    o   = obs.get(k,0)
+    x2 += (o  - e)**2/ e
+  critical = interpolate(df,[ # 95% confidence
+                ( 1,  3.841), ( 5, 11.070), 
+                (10, 18.307), (15, 24.996), (20, 31.410),
+                (30, 43.66),  (60, 79.08)])
+  #print(dict(df=df,x2=x2,critical=critical))
+  return x2 <= critical
 
-x,y=0,0.025
-while x < 2:
-  y *= 2
-  x  = 1+y
-  d2 = {k:v*x for k,v in d1.items()}
-  print(x, chisquare(d1,d2))
-
+if __name__== "__main__":
+  d1 = dict(a=20,b=10,c=60,d=80,e=100,f=10,g=20)
+  x,y=0,0.025
+  while x < 2:
+    y *= 1.5
+    x  = 1+y
+    d2 = {k:v*x for k,v in d1.items()}
+    print(x, countsequal(d2,d1))
+  
