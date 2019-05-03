0ea621121ce6b6bd0d96c203f1c79ca8d741a099
Tim Menzies <timm@ieee.org>
2019-05-03 16:01:47 -0400

    saving

--- py/select.py
+++ py/select.py
@@ -3,6 +3,8 @@ from random import random as r
 def plus(x,d):
     d[x] = d.get(x,0) + 1
 
+# if b bins and if b size > 1/n then at prob 1/b split that bucket
+# if bucket cotnaisn one item, return it. if it contains two pull at random
 def one(d):
    sum =0
    for _,v in d.items(): sum += v
@@ -13,9 +15,9 @@ def one(d):
        return k
 
 d={}
-for x in 'abcabaee': plus(x,d)
+for x in '0123456789': plus(x,d)
 e={}
-n=10000
+n=100
 [plus(one(d),e) for _ in range(n)]
 for k,v in sorted(e.items()):
     print(k,v/n)
