595574b239d0173274af7962018047bfafc80842
Tim Menzies <timm@ieee.org>
2019-05-05 01:45:03 -0400

    saving

--- py/select.py
+++ py/select.py
@@ -8,7 +8,7 @@ say=sys.stdout.write
 random.seed(1)
 
 class Nums :
-  def __init__(i,lo,hi,b=8):
+  def __init__(i,lo,hi,b=16):
     i.lo, i.hi, i.n, i.b, i.bins = lo, hi, 0, b, []
     skip = (hi - lo)/b
     while lo  < i.hi: 
@@ -23,25 +23,33 @@ class Nums :
     i.bins  = i.bins + [(i.hi, mid, n1), (mid,ceiling,n2)]
     i.hi    = ceiling
   def descend(i,floor):
-    say("-")
     mid     = i.lo - (i.lo - floor)/2
     n       = i.bins[0][-1]
     n1      = n // 2
     n2      = n - n1
     i.bins  = [(floor,mid, n1), (mid,i.lo,n2)] + i.bins
     i.lo    = floor
+  def split(i,pos):
+    b4      = i.bins[:pos]
+    after   = i.bins[pos+1:]
+    lo,hi,n = i.bins[pos]
+    n1      = n//2
+    n2      = n - n1
+    step    = lo + (hi-lo)/2
+    return  b4 + [(lo,step,n1), (step,hi,n2)] + after
   def put(i,a, n=1):
     def bsearch(a):
       lo, hi = 0, len(i.bins) - 1
       while lo <= hi:
         mid = (lo + hi) // 2
-        k   = i.bins[mid][0]
-        if   k < a : lo = mid + 1
-        elif a < k : hi = mid - 1
-        else       : return mid
+        k   = i.bins[mid]
+        if   k[0] <= a < k[1]: return mid
+        elif k[0] < a    : lo = mid + 1
+        elif a    < k[0] : hi = mid - 1
+        else             : return mid
       return mid
     #-------------------
-    if r() < 1/8:
+    if r() < 1/8 and len(i.bins) < 16:
       if a > i.hi : i.ascend(a)
       if a < i.lo : i.descend(a)
     #-------------------
@@ -51,32 +59,23 @@ class Nums :
       a = max(i.lo, min(a,i.hi)) 
       pos = bsearch(a)
       bin = i.bins[pos]
-      print(a,pos,bin)
     #-------------------
     lo,hi,n     = bin
     i.bins[pos] = (lo,hi, n+1)
     i.n     += 1
-    if (hi - lo) > (i.hi-i.lo)/32 and n >= 2*i.n/i.b:
+    if (n >= 1.5*i.n/i.b): #and (hi-lo) > (i.hi-i.lo)/i.b:
       i.bins = i.split(pos) 
-  def split(i,pos):
-    say("/")
-    b4      = i.bins[:pos]
-    after   = i.bins[pos+1:]
-    lo,hi,n = i.bins[pos]
-    n1      = n//2
-    n2      = n - n1
-    step    = lo + (hi-lo)/2
-    return  b4 + [(lo,step,n1), (step,hi,n2)] + after
   def get(i):
     a = r()*i.n
     for (lo,hi),n in i.bins:
       a -= n 
       if a <= 0: return lo + r()*(hi - lo)
 
-nums=Nums(30,70,4)
-for _ in range(30):
-  nums.put( int(random.gauss(50,20)) )
+# wont extend to the type
+nums=Nums(40,60)
+for _ in range(10**4):
+  nums.put( int(random.gauss(20,5)) +
+            int(random.gauss(80,5)) )
 
-print("")
 for lo,hi,n in sorted(nums.bins):
   print(lo,hi,n)
