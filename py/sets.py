a0518a4f12df94f3b03c6d26016c949363d4d4a7
Tim Menzies <tim.menzies@gmail.com>
2019-05-10 21:24:06 -0400

    saving

--- py/sets.py
+++ py/sets.py
@@ -3,20 +3,21 @@ import time
 
 r= random.random
 
-
 class fred(object):
    def __init__(self): self.lst = [r() for _ in range(20)]
-   #def __hash__(self): return self.id
-   #def __eq__(self, x): print(1); return x is self
-   #def __ne__(self, x): print(1); return not self == x #x is not self
 
-n=10**6
-some = [fred() for _ in range(n)]
-print(1)
+n          = int(0.5*10**6)
+some       = [fred() for _ in range(n)]
 start_time = time.time()
-s=set()
+s          = set()
+
 [s.add(x) for x in some]
-n1=len(s)
+
+n1         = len(s)
+
 [s.remove(x) for x in some]
-print("--- %s seconds ---" % ((time.time() - start_time)/n))
+
+t          = time.time() - start_time
+
+print("--- %s seconds %s rate---" % (t, t/n))
 print(n1,len(s))
--- py/withs.py
+++ py/withs.py
@@ -3,12 +3,12 @@ r=random.random
 #random.seed(1)  <== important! somewhere, you must maintain seed control
 
 class bins:
-  """Train a distributions with some samples. Draw from 'within'
-     that to recreate it. Draw from 'without' to avoid it."""
-  def __init__(i)       : i.n, i.d = 0, {}
-  def train(i, z, n=1)  : i.n += n; i.d[z] = i.d.get(z,0) + n
-  def within(i)         : return i.get(lambda z: z/i.n)
-  def without(i)        : return i.get(lambda z: (i.n - z)/i.n)
+  """Train a distributions with samples. Draw from 'within'
+  that to recreate it. Draw from 'without' to avoid it."""
+  def __init__(i)    : i.n, i.d = 0, {}
+  def train(i,z, n=1): i.n += n; i.d[z] = i.d.get(z,0) + n
+  def within(i)      : return i.get(lambda z: z/i.n)
+  def without(i)     : return i.get(lambda z: (i.n - z)/i.n)
   def get(f):
     tmp = r()
     for k,v in i.d.items():
@@ -18,21 +18,20 @@ class bins:
 class nums:
   "Using 'bins', keep track of nums (rounded to a width i.w)"
   def __init__(i,w=0.12): i.w, i.bins = w, bins
-  def within(i)         : return i.bins.within()   + r()*i.w
-  def without(i)        : return i.bins.without()  + r()*i.w
-  def train(i, z, n=1)  : return i.bins.train(z//i.w * i.w, n)
+  def within(i)       : return i.bins.within()   + r()*i.w
+  def without(i)      : return i.bins.without()  + r()*i.w
+  def train(i, z, n=1): return i.bins.train(z//i.w * i.w, n)
 
 class around:
-  """Using 'nums', whenever you train on z, spread that effect
-     around the nearby regions."""
-  def __init__(i, w=0.12,
+  """Using 'nums', whenever you train on z, spread 
+  that effect around the nearby regions."""
+  def __init__(i, w      = 0.12,
                   nearby = [-1.5, -0.5, 0, 0.5, 1.5],
                   fxs    = [1,     2,   6, 2,   1]):
     i.nums = nums(w)
-    i.todo = [(s,fx/sum(fxs)) for s,fx in zip(nearby,fxs)]
+    i.todo = [(s, fx/sum(fxs)) for s,fx in zip(nearby,fxs)]
   def within(i)       : return i.nums.within()
   def without(i)      : return i.nums.without()
   def train(i, z, n=1):
     for nearby, effect in i.todo:
       i.nums.train( z + i.nums.w*nearby, n*effect )
-
