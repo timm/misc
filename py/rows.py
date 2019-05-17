8c448c41c4eb5661960186f8361a6b8759f2a314
Tim Menzies <tim.menzies@gmail.com>
2019-05-17 01:14:07 -0400

    saving

--- py/rows.py
+++ py/rows.py
@@ -6,60 +6,78 @@ import re
 # return a row with ids and x and y
 # i need x,y
 
+class eg(object):
+   id=0
+   def __init__(i, egs, xs=[], ys=[]): 
+     eg.id = i.id = eg.id + 1
+     i.xs, i.ys, i.egs = xs, ys, egs
+
+class egs(object):
+  ignore = "?"
+  less   = "<"
+  more   = ">"
+  klass  = "!"
+  ys     = "<>!"
+  def __init__(i):
+    i.klass  = None
+    i.xs, i.ys, i.less, i.more = [],[],[],[]
+  def also(i,c):
+     if    egs.less  in c.name : i.less  += [c]
+     elif  egs.more  in c,name : i.more  += [c]
+     elif  egs.klass in c.name : i.klass  = c
+  def rows(i,file):
+    first = True
+    for xs,ys in xy(rows(file)):
+      if first:
+        first = False
+        i.xs = [       col(x,n)  for n,x in enumerate(xs)]
+        i.ys = [i.also(col(y,n)) for n,y in enumerate(ys)]
+      else:
+        yield eg(i,
+                 xs = [ lst[c.pos0] for c in i.xs ],
+                 ys = [ lst[c.pos0] for c in i.ys ])
+
+class col(object):
+  def __init__(i,  name,pos): 
+    i.name, i.pos = name, pos
+    i.log  = None # place to stats about a number
+    i.prep = None # coerce function string to something
+  def train(i, z, n=1):
+    if z is egs.ignore: return z
+    if not i.log:
+       z, i.prep = atom(z)
+       i.log     = bins() if i.prep == str else around()
+    i.log.train( pre(z), n )
+
+def xy(file):
+  xs, ys, first = [],[],True
+  for lst in rows(file):
+    if first:
+      first=False
+      for n,s in enumerate(lst)
+        what  = ys if s[0] in egs.ys else xs
+        what += [n]
+    yield [lst[n] for n in xs], lst[n] for n in ys]
+
+def rows(file):
+  """kill whitespace, comments, skip blanklines, join lines
+     ending with ',', return list of cells, one per line,
+     ignore columns maked with '?'
+     skip blanklines"""
+  txt,use = "",[]
+  with open(file) as fs:
+    for line in fs:
+      txt += re.sub(r'([\n\t\r ]|#.*)', '', line)
+      if txt and txt[-1] != ",":
+        lst = txt.split(",") 
+        if lst:
+          txt = ""
+          use = use or [n for n,s in enumerate(lst) 
+                        if s[0] != egs.ignore]
+          yield [ n for n in use ]
+
 def atom(x)  :
   try: return int(x), int
   except:
     try: return float(x), float
     except ValueError: return x, str
-
-class egs(object):
-  def __init__(i):
-    i.klass  = None
-    i.x, i.y, i.less, i.more, i.all = [],[],[],[],[]
-  def rows(file):
-    """kill whitespace, comments, skip blanklines, join lines
-       ending with ',', return list of cells, one per line,
-       skips columns marked '?' in column one.i
-       convert those string to nums as needed"""
-    usep   = lambda z: z[0] != '?'
-    same   = lambda z: z
-    pre    = lambda z: float if z[0] in '<>$' else same 
-    txt,fs = "", None
-    with open(file) as fs:
-      for line in fs:
-        txt += re.sub(r'([\n\t\r ]|#.*)', '', line)
-        if txt and txt[-1] != ",":
-          lst = txt.split(",") 
-          if lst:
-            txt = ""
-            fs  = fs or [ (n,pre(s)) for n,s in 
-                          enumerate(lst) if usep(s) ]
-            yield [ f(lst[n]) for n,f in fs]
-
-class thing(object):
-   """The first thing seen is the name.
-      The other things are either things to ignore 
-      or things we should use in training"""
-   less   = "<"
-   more   = ">"
-   klass  = "!"
-   ignore = "?"
-   def __init__(i,name="", inside=egs()): 
-     i.egs, i.name, i.it = inside, name, None
-     what = i.egs.y
-     if   thing.less  in name : i.egs.less += [self]
-     elif thing.more  in name : i.egs.more += [self]
-     elif thing.klass in name : i.egs.klass = self
-     else                     : what = i.egs.x
-     what  += [self]
-     i.all += [self]
-   def train(i, z, n=1):
-     if z is thing.ignore: return z
-     if not i.it:
-        z, i.what = atom(z)
-        i.it      = bins() if i.what==str else around()
-     i.it.train(z, n)
-
-# need a bins for ints
-
-
