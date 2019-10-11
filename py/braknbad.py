2f517647a014ecccfb313dc35807565359f3e954
Tim Menzies <tim.menzies@gmail.com>
2019-10-11 00:40:25 -0400

    saving

--- py/braknbad.py
+++ py/braknbad.py
@@ -11,24 +11,26 @@
     -=_________=-
       ___   ___
      |___)=(___|
-    
+          |
          ###
-        # = #
         #####
+        # = #
          ###
 
 - Divide data into regions of bad and better.
 - Find the least you need to do to nudge things away from bad.
 
-     Better
-    .------.
-    |    36| <--- a score you want to minimize
-    | Br   |
-    .______.______.
-           |    56| <--- a larger, hence worse, score
-           | Ba   |
-           .______.
-             Bad
+```
+Better
+.------.
+|    36| <--- a score you want to minimize
+| Br   |
+.______.______.
+       |    56| <--- a larger, hence worse, score
+       | Ba   |
+       .______.
+        Bad
+```
 
 """
 
@@ -55,15 +57,23 @@ def config():
    nb   = o(  k=1, m=2     ),
    row  = o(  doms = 64    ),
    two  = o(  better = 0.2 )
+   div  = o(  cohen  = 0.3,
+              trivial= 1.05,
+              min    = 0.6)
    )
 
 
 #--------- --------- --------- --------- -------- ----------
 import re,sys,traceback,random
+from copy import deepcopy as kopy
 
 class o:
+  """A class for quick specification of fields. 
+  And for pretty-printing instances."""
   def __init__(i,**d) : i.__dict__.update(**d)
   def __repr__(i):
+    """Print attributes in alphabetical order, skipping
+     'private' attributes (those starting with '_')."""
     lst= [(k, v) for k,v in i.__dict__.items() if k[0] != "_"]
     return i.__class__.__name__ + '{' + ", ".join(
            [('%s=%s' % (k, v)) for k,v in sorted(lst)]) +'}'
@@ -71,7 +81,7 @@ class o:
 any = random.choice
 
 def atom(x):
-  "coerce x into the right kind of atom"
+  "Coerce the string x into the right kind of atom."
   try: return int(x)
   except:
     try: return float(x)
@@ -79,7 +89,8 @@ def atom(x):
 
 
 def cli(d,args=sys.argv[1:]):
-  "Allow command lines args to update fields in the THE object" 
+  """Allow command lines args to update fields in the THE object.
+     Example usage: THE = cli(THE)."""
   args   = [atom(x) for x in args]
   what   = {}
   groups = d.__dict__
@@ -104,13 +115,20 @@ def cli(d,args=sys.argv[1:]):
 THE=config()
 
 class Eg(o):
-  all = []
-  y   = 0
+  "Manage a list of demo/test functions."
+  all = []  # asdas"
+  "Place to store demos/tests."
   n   = 0
+  "Number of called demos/tests."
   tell= "#Tests 0 tries 0 fails 100% passes."
+  "Report string (summarizes the run)."
+  y   = 0
+  "Number of demos/tests with assert failers"
   def run(): 
+    "Run the list of demos/tests."
     [Eg.run1(one) for one in Eg.all]
   def run1(f):
+    "Run a demo/test. If it fails, update the Eg.no counter."
     if re.match(THE.do.run, f.__name__):
       print("\n-----| %s |%s" % (f.__name__,"-"*40))
       if f.__doc__:
@@ -125,15 +143,19 @@ class Eg(o):
         Eg.tell = "#Tests %s tries %s fails %s%% passed." % (
                     y,n, round(100*(y/(y+n+0.0001)),0))
 
-def eg(f): Eg.all += [f]; return f
+def eg(f): 
+  """Convenience function. Decorator that adds 
+     functions to the list managed by the `Eg` class."""
+  Eg.all += [f]; return f
 
 class Row(o):
-  "Holder for lists of values."
+  "Holder for lists of values and their discretized ranges."
   def __init__(i,lst):
     i.cells=lst
     i.ranges=[None]*len(lst)
     i.count=0
   def dom(i,j,t):
+    "Check if this row 'i' is better than row 'j'."
     n = len(t.cols.objs)
     s1 = s2 = 0
     for c in t.cols.objs:
@@ -144,6 +166,9 @@ class Row(o):
     return s1/n < s2/n
 
 class Tbl(o):
+  """Manage a list of Rows. Keep statistics on each column
+   in `objs` or `decs` objects (which are either `Num` or
+  `Sym` instances)."""
   def __init__(i,names=[], rows=None):
     i.rows = []
     i.cols = o(all=[], decs = [], objs=[])
@@ -154,8 +179,10 @@ class Tbl(o):
       i.read(rows)
       i.names = [ c.txt for c in i.cols.all ]
   def centroid(i):
+    "Return the middle."
     return [ c.centroid() for c in i.cols.all ]
   def div(i):
+    "Divide the rows into those that dominate most/least."
     n = THE.row.doms
     for r1 in i.rows:
       if n < len(i.rows):
@@ -165,8 +192,11 @@ class Tbl(o):
       r1.count = tmp/n
     return Two(i, i.rows)
   def clone(i):
+    """Return an (empty) table that can read rows like 
+    those seen in this table."""
     return Tbl(i.names)
   def header(i,names):
+    "Convert a list of names into `Num`s and `Sym`s as appropriate."
     for n,s in enumerate(names):
       w     = -1 if s[0] == '<' else 1
       x     = (Num    if s[0] in "<>$" else Sym)(s,n,w)
@@ -174,13 +204,18 @@ class Tbl(o):
       what += [x]
       i.cols.all += [x]
       print(x)
-  def read(i,rows):
-    for row in rows:
+  def read(i,src):
+    """Read rows from some src. Add to this table.
+      If this table does not know what is columns are yet,
+      then read those from the first row."""
+    for row in src:
       i + row if i.cols.all else i.header(row)
   def __add__(i,lst):
+    "Add a new row.  Update the column statistics."
     lst     = [ c + lst[c.pos] for c in i.cols.all ]
     i.rows += [ Row(lst) ]
   def like(i,lst,ns):
+    "Report the likelihood that `lst` belongs in this table."
     n    = len(i.rows)
     k, m = THE.nb.k, THE.nb.m
     like = prior = (n + k) / (ns + k*2)
@@ -191,17 +226,33 @@ class Tbl(o):
       inc   = (f + m*prior) / (n + m)
       like *= inc
     return like
- 
-class Num(o):
-  def __init__(i,txt="",pos=0,w=1):
+
+class Thing(o):
+  id = 0
+  def identity(i):
+    i.id = Thing.id = Thing.id+ 1
+    return i.id
+  def __hash__(i):
+    return i.id
+  def xpect(i,j):
+    n = i.n + j.n
+    return i.n/n * i.variety() + j.n/n * j.variety()
+
+class Num(Thing):
+  def __init__(i,txt="",pos=0,w=1,key=same,inits=[]):
+    i.id = i.identity()
     i.txt,i.pos = txt,pos
     i.w=w
     i.n,i.mu,i.m2 = 0,0,0
     i.lo,i.hi     = 10**32, -10**32
+    i.key = same
+    [i + one for one in inits]
+  def variety(i) : return i.sd
   def centroid(i): return i.mu
   def norm(i,x): 
     return (x - i.lo) / (i.hi - i.lo + 10**-32)
   def __add__(i,x):
+    x = i.key(x)
     if x == THE.char.no: return x
     x = float(x)
     if x < i.lo: i.lo = x
@@ -217,19 +268,33 @@ class Num(o):
     if i.n  < 2: return 0
     return (i.m2/(i.n - 1 + 10**-32))**0.5
 
-class Sym(o):
-  def __init__(i,txt="",pos=0,w=1):
+class Sym(Thing):
+  def __init__(i,txt="",pos=0,w=1, key=same, inits=[]):
+    i.id = i.identity()
     i.txt,i.pos = txt,pos
     i.w=w
     i.n,i.most,i.mode,i.bag = 0,0,None,{}
+    i.key=key
+    i._ent=None
+    [i + one for one in inits]
+  def variety(i): return i.ent()
   def centroid(i): return i.mode
   def __add__(i,x):
+    x = i.key(x)
     if x == THE.char.no: return x
+    i._ent= None
     i.n += 1
     c = i.bag[x] = i.bag.get(x,0) + 1
     if c > i.most:
        i.most, i.mode = c, x
     return x
+  def ent(i):
+    if not i._ent:
+      i._ent = 0
+      for _,v in i.bag.items():
+        p =  v/i.n
+        i._ent -= p*math.log(p,2)
+    return i._ent
 
 class Two:
   """Stores two tables: one for `_bad` things
@@ -248,14 +313,65 @@ class Two:
     l1 = i._bad.like(   i,lst, ns)
     l2 = i._better.like(i,lst, ns)
     return l1/(l1+l2), l2/(l1+l2)
-   
-class Range(o):
-  id = 0
-  def __init__(i,lo,hi) : 
-    i.lo,i.hi=lo,hi
-    i.id = Range.id = Range.id + 1
-  def __hash__(i):
-    return i.id
+
+def first(lst): return lst[0]
+def last(lst) : return lst[-1]
+
+class Div2(o):
+  """
+  Recursively divide a list of numns by finding splits
+  that minimizing the expected value of the standard
+  deviation (after the splits).
+  """
+  def __init__(i,lst, x=first, xis=Num, y=last, yis=Num):
+    i.x, i.y   = x,y
+    i.xis      = lambda lst=[]: xis(inits=lst, key=i.x) # a collector for x things
+    i.yis      = lambda lst=[]: yis(inits=lst, key=i.y) # a collector for y things
+    i._lst     = sorted(one for one in lst 
+                        if x(one) is not THE.char.no, ,key=x)
+    i.xs       = i.xis(i._lst)
+    i.ys       = i.yis(i._lst)
+    i.step     = int(len(i._lst)**THE.div.min) # each split need >= 'step' items
+    i.stop     = x(last(i._lst))               # top list value
+    i.start    = x(first(i._lst))              # bottom list value
+    i.ranges   = []                            # the generted ranges
+    i.epsilon  = i.xs.sd() * THE.div.cohen     # bins must be seperated >= epsilon
+    i.__divide(1, len(i._lst), i.xs, i.ys, 1)
+
+  def __divide(i, lo, hi, xr, yr, rank):
+    """Find a split between lo and hi, then recurse on each split.
+     If no split can be found then assign everything a rank of 'rank'.
+     'xr' and 'yr' are statistics on the whole x,y space from lo to hi."""
+    xb4       = kopy(xr)
+    xl        = i.xis()
+    yl        = i.yis()
+    best      = yr.variety()
+    cut       = None
+    for j in range(lo,hi):
+      xl + i._lst[j]
+      yl + i._lst[j]
+      xr - i._lst[j]
+      yr - i._lst[j]
+      if xl.n >= i.step:
+        if xr.n >= i.step:
+          now   = i.x( i._lst[j]   )
+          after = i.x( i._lst[j+1] )
+          if now == after: continue
+          if abs(xr.mu - xl.mu) >= i.epsilon:
+            if after - i.start >= i.epsilon:
+              if i.stop - now >= i.epsilon:
+                xpect = yl.xpect(yr)
+                if xpect*THE.div.trivial < best:
+                  best, cut = xpect, j+1
+    if cut:
+      ls   = i._lst[lo:cut]
+      rs   = i._lst[cut:hi]
+      rank = i.__divide(lo, cut, i.xis(ls), i.yis(ls), rank) + 1
+      rank = i.__divide(cut, hi, i.xis(rs), i.yis(rs), rank)
+    else:
+      xb4.rank  = rank
+      i.ranges += [ xb4 ]
+    return rank
 
 def csv(file):
   use, txt = [], ""
