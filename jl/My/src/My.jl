581e65b09f78de254e930724b962ee23c9efcbed
Tim Menzies <tim.menzies@gmail.com>
2019-10-27 01:30:26 -0400

    saving

--- jl/My/src/My.jl
+++ jl/My/src/My.jl
@@ -1,7 +1,20 @@
 module My
 using Parameters 
+using Random
 
-greet() = print("Hello World!")
+Random.seed!(1)
+
+same(s)  = s
+anyi(s)  = floor(Int,round(s*rand()))
+any(lst) = lst[ anyi(size(lst)[1]) ]
+
+@with_kw mutable struct Config
+  char = (skip='?',)
+  str  = (skip="?",)
+  some = (max=256,)
+end
+
+THE = Config()
 
 @with_kw mutable struct Cols
   x  = (all  = [], nums = [], syms = [])
@@ -20,16 +33,81 @@ z=Cols(name="jane", pos=2)
 #dump(w)
 #dump(nothing)
 
-function dd()
-open("sherlock-holmes.txt") do f
-   line = 1
-   while !eof(f)
-     x = readline(f)
-     println("$line $x")
-     line += 1
-   end
- end
+
+@with_kw mutable struct Some 
+  key=same; all=[]; n=0; max=THE.some.max ;sorted=false;
 end
+
+function adds(t, lst=[], key=same)
+  u = t(key=same)
+  [add(u,x) for x in lst]
+  u
+end
+
+function add(s,x)
+  y = s.key(x)
+  if y != THE.char.skip 
+    s.n += 1
+    add1(s,x) end
+  y
+end
+
+function contents(s::Some)
+  if !s.sorted 
+    sort!(s.all,by=s.key); s.sorted = true end
+  s.all
+end
+
+len(i::Some)   = size(i.all)[1]
+p(  i::Some,n) = contents(i)[ floor(Int,n*len(i)) + 1 ]
+var(i::Some)   = (p(i,0.9) - p(i,0.1))/2.7
+    
+function add1(i::Some, x)
+  i.sorted=false
+  m = len(i)
+  if m < i.max
+    push!(i.all,x)
+  elseif rand() < m/i.n
+    r = floor(Int,m*rand()) + 1
+    i.all[ r ]= x end end
+
+@with_kw mutable struct Sym 
+  seen=Dict();  n=0; ent=nothing; most=0; mode="";key=same;
+end
+
+function var(i::Sym)
+  if i.ent == nothing
+    i.ent = 0
+    for (_,n) in i.seen
+      p = n/i.n
+      i.ent -= p*log(2,p) end end
+   i.ent
+end
+
+function add1(i::Sym,x)
+  i.ent = nothing
+  old = haskey(i.seen, x) ? i.seen[x] : 0
+  new = i.seen[x] = old + 1
+  println(x," ",i.seen[x])
+  if new > i.most
+    i.most, i.mode = new, x end
+end
+
+function sym1()
+  s=Sym()
+  [add(s,x) for x in "aaaabbc"]
+  println(">> ", s.seen['a'], " ", var(s))
+end
+
+function some1()
+  s=Some(max=32)
+  for j in 1:10000 add(s,j) end
+  println(contents(s))
+  println(var(s))
+end
+
+some1()
+sym1()
 
 @with_kw struct Lines file; src=open(file) end
 
@@ -39,13 +117,12 @@ function Base.iterate(it::Lines, (n,want)=(1,[]))
   "Split on comma, coerce strings to numbers or strings, as approriate." 
   coerce(s)  = map(coerce1, split(s,","))
   coerce1(s) = ((x = tryparse(Float64,s))==nothing) ? s : x
-  use(s)     = match(r"\?",s) == nothing
 
   "Coerce strings. If first row, check what columns we should use.
    Only return those columns."
   function cols(lst)
     if n == 1 
-      want = [i for (i,s) in enumerate(lst) if use(s)] end
+      want = [i for (i,s) in enumerate(lst) if !('?' in s)] end
     [lst[i] for i in want]  end 
   
   "Delete comments and whitespace. Lines ending in
@@ -65,7 +142,7 @@ function Base.iterate(it::Lines, (n,want)=(1,[]))
     (n, cols(coerce(new))) , (n+1,want) end 
  end
 
-function xomo()
+function Lines1()
   m=1
   print(m)
   for (n,tmp) in Lines(file= "data/xomo10000.csv")
