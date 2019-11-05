caab2f750fcfa5ce87e0ceccfe9365813ee0db1f
Tim Menzies <tim.menzies@gmail.com>
2019-11-04 20:53:14 -0500

    as

--- py/bayes.py
+++ py/bayes.py
@@ -4,25 +4,23 @@ r = random.random
 def p(s=2,hi=0.25,d=10,repeats=10000):
   tried,passed = 0.0001,0
   for _ in range(repeats):
-    m = int(r()*5) + 1
-    n = int(r()*5) + 1
+    m = int(r()*20) + 1
     x = r()*hi
-    y = r()*hi/d
-    a = r()*hi
-    b = r()*hi
-    if b> 0 and a> 0 and x>=a and y>=b and a<1 and b<1 and x<1 and y<1:
-      tried = tried+ 1
-      if (a/x)**s/(a/x + b/y) > a**s/(a+b):
-        passed=passed+1
-  return passed/tried
+    y = r()*hi
+    a = x**m
+    b = y**m
+    if b>0 and a>0 and x>=a and y>=b and a<1 and b<1 and x<1 and y<1:
+      if  x > y*d and m > 1:
+        tried = tried+ 1
+        if (a/x)**s/(a/x + b/y) < a**s/(a+b):
+          print(s,m,d,x,x/y)
 
 def s(z):  return int(z*100)
 
 hi=1
 while hi>0.1:
-   for d in [1,2,4,8,16]:
+   for d in [1,2,4,8,16,32]:
      hi *= 0.8
-     x = p(s=1,d=d,hi=hi)
-     y = p(s=2,d=d,hi=hi)
-     if y> x:
-       print(s(hi), d,s(x), s(y))
+     #x = p(s=1,d=d,hi=hi)
+     y = p(s=1.2,d=d,hi=hi)
+     #print(s(hi), d,s(x), s(y))
