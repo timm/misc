b4512fdab7f59b9fc90b1b71850a9784225c7d4e
Tim Menzies <tim.menzies@gmail.com>
2019-11-09 12:43:46 -0500

    as

--- lua/lib.lua
+++ lua/lib.lua
@@ -1,21 +1,50 @@
-#!/usr/bin/env lua
--- vim: paste filetype=lua nospell ts=2 sw=2 sts=2 et :
----------- --------- --------- --------- --------- --
+-- class.lua
+-- Compatible with Lua 5.1 (not 5.0).
+-- http://lua-users.org/wiki/SimpleLuaClasses
 
-Object = {}
+function class(base, init)
+   local c = {}    -- a new class instance
+   if not init and type(base) == 'function' then
+      init = base
+      base = nil
+   elseif type(base) == 'table' then
+    -- our new class is a shallow copy of the base class!
+      for i,v in pairs(base) do
+         c[i] = v
+      end
+      c._base = base
+   end
+   -- the class will be the metatable for all its objects,
+   -- and they will look up their methods in it.
+   c.__index = c
 
-do
-  local id = 0
-  function Object.new(t)
-    t = t or {}
-    t.ako = t.ako or "Object"
-    id = id + 1
-    t._isa= _ENV[t.ako]
-    t.id = id
-    return t end
+   -- expose a constructor which can be called by <classname>(<args>)
+   local mt = {}
+   mt.__call = function(class_tbl, ...)
+     local obj = {}
+     setmetatable(obj,c)
+     if class_tbl.init then
+        class_tbl.init(obj,...)
+     else 
+        -- make sure that any stuff from the base class is initialized!
+        if base and base.init then
+        base.init(obj, ...)
+        end
+     end
+     return obj
+   end
+   c.init = init
+   c.is_a = function(self, klass)
+      local m = getmetatable(self)
+      while m do 
+         if m == klass then return true end
+         m = m._base
+      end
+      return false
+   end
+   setmetatable(c, mt)
+   return c
 end
-
-function abs(x) return x<0 and -1*x or x end
 
 function ordered(t,  i,keys)
   i,keys = 0,{}
@@ -66,13 +95,3 @@ do
   end
 end
 
-function abs(x) return x<0 and -1*x or x end
-
-
-o{bb=1,aa=2,c={10,20,30}}
-rogues()
-
-function libmain()
-  x=Object.new()
-end
-return {main=libmain}
