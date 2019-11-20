c0d738974cabe8776f12bc6b6ed1d58033f6401e
TIm Menzies <timm@TIms-MacBook-Air.local>
2019-11-19 23:01:23 -0500

    saving

--- lisp/xy.lisp
+++ lisp/xy.lisp
@@ -13,37 +13,47 @@
                     trivial 1.05)
          seed 1))
 
-(defmacro ? (&rest fs) `(getr  getf *the* ,@fs))
-(defmacro getr (how obj f &rest fs)
-  (if fs `(getr ,how (,how ,obj ',f) ,@fs) `(,how ,obj ',f)))
+(defmacro ? (&rest fs) `(??  getf *the* ,@fs))
+
+(defmacro ?? (how obj f &rest fs)
+  (if fs 
+      `(?? ,how (,how ,obj ',f) ,@fs) 
+      `(,how ,obj ',f)))
 
 (defmacro while (test &body body) `(do () ((not ,test)) ,@body))
 
-(labels 
-  ((comma-split (string)
-      (loop for lo = 0 then (1+ hi)
-            for hi = (position #\, string :start lo)
-            collecting (string-trim " " (subseq string lo hi))
-            until (null hi))))
-  (defmacro with-lines ((line file) &body body)
-    (let ((str (gensym)))
-      `(with-open-file
-	   (,str ,file)
-         (while (setf ,line (read-line ,str nil))
-                (setf ,line (comma-split ,line))
-                ,@body)))))
+(defun word (s lo &optional hi) 
+  (string-trim '(#\Space #\Tab #\Newline) (subseq s lo hi)))
 
-(defun prefix (x y) (eql (char (symbol-name x) 0) y))
+(defun words (s &optional (lo 0) (hi (position #\, s :start (1+ lo))))
+  (cons (word s lo hi) 
+        (if hi (words s (1+ hi)))))
 
-(defun skip?    (x) (prefix x #\?))
+(defmacro with-lines ((line file) &body body)
+  (let ((str (gensym)))
+    `(with-open-file (,str ,file)
+       (while (setf ,line (read-line ,str nil))
+         (setf ,line (words ,line))
+         ,@body))))
 
-(defun more?    (x) (prefix x #\>))
-(defun less?    (x) (prefix x #\<))
-(defun klass?   (x) (prefix x #\!))
-(defun num?     (x) (prefix x #\$))
-(defun goal?    (x) (or (klass? x) (less? x) (more? x)))
-(defun numeric? (x) (or (num? x)   (less? x) (more? x)))
-(defun sym?     (x) (and (not (num? x)) (not (goal? x))))
+(defun has?     (x y) (eql (char x 0) y))
+(defun more?    (x)   (has? x (? char more)))
+(defun less?    (x)   (has? x (? char less)))
+(defun klass?   (x)   (has? x (? char klass)))
+(defun num?     (x)   (has? x (? char num)))
+(defun goal?    (x)   (or (klass? x) (less? x) (more? x)))
+(defun numeric? (x)   (or (num? x)   (less? x) (more? x)))
+(defun sym?     (x)   (and (not (num? x)) (not (goal? x))))
+
+(defun adds (&optional inits (i 'Some)) (incs (make-instance i) init 1))
+(defun subs (&optional inits (i 'Some)) (incs (make-instance i) init -1))
+(defun incs (i lst inc)
+   (let ((f (slot-value i 'keys))
+         (i (funcall f i)))
+     (if (not (ewl 
+   
+(defun add  (i x) (inc! i x 1))
+(defun sub  (i x) (inc! i x -1))
 
 (defstruct num (n 0) (a 0) b)
 (defstruct sym n a b)
