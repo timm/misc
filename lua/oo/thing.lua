425afbd2481d437b60c93aaf462f3550f4f8a116
Tim Menzies <timm@ieee.org>
2019-11-28 04:03:26 -0500

    saving

--- lua/oo/thing.lua
+++ lua/oo/thing.lua
@@ -1,19 +1,33 @@
 -- vim: nospell:sta:et:sw=2:ts=2:sts=2
 
-require "lib"
-Thing = Object:new{n=0, pos=0, w=1, txt="", key=same}
-Sym   = Thing:new{ counts={}, mode=nil, most=0}
+function deepcopy(t) 
+    return type(t) ~= 'table' and t or collect(t,deepcopy) end
 
-function Thing:prep(x) return x end
-function Thing:add(x)
-  x = self.same(x) 
-  if x ~= "?" then
-    x = self:prep(x)
-    self.n = self.n + 1
-    x:add1(x) 
-  end 
+function collect(t,f,  out)
+     out={}
+     f= f or function (z) return z end
+     if t then  
+       for i,v in pairs(t) do out[i] = f(v) end end
+     return out
+end 
+
+Object={}
+
+-- Object.new(self,o)
+function Object.new(o,new) 
+  for k,v in pairs(new) do
+    o[k] = new[k] or o[k]
+  end
+  return  o
 end
 
-function Thing:add1(x)
-  print(x) --
+Thing={}
+ 
+function Thing.new(i)
+  return Object.new({n=0, pos=100, w=1,txt="",all={}},i)
+end
+
+function Thing.add(i,x) 
+  i.all = i.all or {}
+  i.all[#i.all + 1]=x
 end
