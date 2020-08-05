551c5ad72bcad566c489a4236878fa5622fa48a0
Tim Menzies <tim.menzies@gmail.com>
2020-08-05 07:27:09 -0400

    saving

--- gold/gold.sh
+++ gold/gold.sh
@@ -1,55 +1,107 @@
 #!/usr/bin/env bash
-# vim: set ts=2 sw=2:  et:
 
 Sh=$(cd $( dirname "${BASH_SOURCE[0]}" ) && pwd )
 chmod +x $Sh
 mkdir -p $Sh/.var
 
 transpiles() {
-  for i in *.md; do
-    j=$Sh/.var/${i%.md}.awk
-    cat $i |
-    gawk -f $Sh/gold.awk --source '{use=md2awk(use) }' > $j
-  done
+  if [ -n "$*" ]; then
+    for i in $*; do 
+      j=$(basename $i .md)
+      k=$Sh/.var/$j.awk
+      cat $i |
+      gawk -f $Sh/gold.awk --source '{use=md2awk(use) }' > $k
+    done
+  fi
 } 
 
 go() {
   j=$Sh/.var/${2%.md}.awk
+  shift; shift;
   AWKPATH="$Sh/.var:$AWKPATH"
-  Com="gawk -f $Sh/gold.awk -f $j"
+  Com="gawk -f $Sh/gold.awk -f $j $*"
   if  [ -t 0 ]; then AWKPATH="$AWKPATH" $Com
   else       cat - | AWKPATH="$AWKPATH" $Com
   fi
 }
+banner() { tput bold; tput setaf 6; cat<<"EOF"
+
+                  ___        __  An object laer
+                 /\_ \      /\ \  for Gawk
+   __       ___  \//\ \     \_\ \  v3.0
+ /'_ `\    / __`\  \ \ \    /'_` \  
+/\ \L\ \  /\ \L\ \  \_\ \_ /\ \L\ \  
+\ \____ \ \ \____/  /\____\\ \___,_\  (ↄ) 2020  
+ \/___L\ \ \/___/   \/____/ \/__,_ /  Tim Menzies 
+   /\____/                            timm@ieee.org  
+   \_/__/               http://github.com/timm/gold
+
+EOF
+tput sgr0
+}
+
+
+if [ "$1" == "--help" ]; then 
+        banner
+        cat<<-'EOF'
+	Usage:
+	   sh gold.sh [options]
+	
+	Options:
+	
+	   --help      show help
+	   --all       transpiles to awk any src/*.md tests/*.md files
+	   -f x.md $*  transpiles, then runs gawk -f gold.awk -f x.awk $*
+	   --install   adds config files for bash, vim, git, tmux to ./.var
+	
+	If run with no options, drops the user into a shell where
+	the following commands are avaialble:
+
+	   gold    = sh gold.sh
+	   awk     = gold --all; AWKPATH='$Sh/.var' gawk -f $Sh/gold.awk 
+	   gg	   = git pull
+	   gp      = git commit -am saving; git push; git status
+	   gs	   = git status
+	   vims	   ensure all vim packags are loaded
+	   vi	   calls vim using ./.var/vimrc
+	   tmux	   calls tmux using ./.var/tmuxrc
+		
+	EOF
+   exit 0
+fi
+
+if [ "$1" == "--all" ]; then
+  transpiles $Sh/src/*.md $Sh/tests/*.md
+  exit 0
+fi
 
 if [ "$1" == "-f"   ]; then
-  transpiles
+  transpiles $Sh/src/*.md $Sh/tests/*.md
   go $*
   exit $?
 fi
 
 if [ "$1" != "--install"   ]; then
   if [ -f "$Sh/.var/bashrc" ]; then
+    banner
     Sh="$Sh"   bash --init-file $Sh/.var/bashrc -i  
   else
     echo "Missing config files. Try running sh gold.sh --install"
   fi
+
   exit 0
 fi
 
-echo ""
 echo "Installing tricks..."
-echo ""
-
 want=$Sh/.var/bashrc
 [ -f "$want" ] || cat<<'EOF'>$want
 f=$Sh/therepy/there  
 
+alias awk="gold --all; AWKPATH='$Sh/.var:$AWKPATH'  gawk -f $Sh/gold.awk "
 alias gold="bash $Sh/gold.sh "
 alias gg="git pull"   
 alias gs="git status"   
 alias gp="git commit -am 'saving'; git push; git status"    
-alias doc="sh $Sh/DOC.md; "
 
 matrix() { nice -20 cmatrix -b -C cyan;   }
 reload() { . $Sh/.var//bashrc;     }
@@ -60,7 +112,7 @@ alias tmux="tmux -f $Sh/.var/tmuxrc"
 
 here()  { cd $1; basename `pwd`; }    
 
-PROMPT_COMMAND='echo -ne "🔆 79 $(git branch 2>/dev/null | grep '^*' | colrm 1 2):";PS1="$(here ..)/$(here .):\!\e[m ▶ "'     
+PROMPT_COMMAND='echo -ne "🔆 GOLD $(git branch 2>/dev/null | grep '^*' | colrm 1 2):";PS1="$(here ..)/$(here .):\!\e[m ▶ "'     
 EOF
 
 want=$Sh/.var/vimrc;
