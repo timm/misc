e01a68458dc13e426d5a2fb541cbb2131f3562ae
Tim Menzies <timm@ieee.org>
2020-12-04 00:44:25 -0500

    saving

--- lua/axe/keys0.lua
+++ lua/axe/keys0.lua
@@ -25,144 +25,126 @@
 -- <li> Return the final best X% group.
 -- </ul>
 
-local of  = {
+local Of  = {
   synopois= "axe: optimization = cluster + contrast",
   author  = "Tim Menzies, timm@ieee.org",
   license = "MIT",
   year    = 2020,
   seed    = 1,
-  ch      = {skip="?", klass="!",sym="_", num=":", more=">", less="<"}
+  ch      = {skip="?", klass="!",sym="_", num=":", more=">", less="<"},
   keys    = {c=20, n=100, x=20, z=2}
 }
 
 ----------------------
 -- ## Things
-local thing, col = {ako="thing"}, {ako="col"}
-local lib, num,sym = {ako="lib"}, {ako="num"}, {ako="sym"}
-local skip = {ako="skip"}
-local row, tbl = {ako="row"}, {ako="tbl"}
 
--- All `thing`s have a unique id.
-do local id=0
-   function thing.new() id=id+1; return {Is=thing,Id=id} end end
+local Lib   = {} 
+local Col   = {ako="Col", pos=0,txt="",n=0}
+local Num   = {ako="Num", pos=0,txt="",n=0, 
+               mu=0, m2=0, sd=0, lo=math.huge, hi= -math.huge}
+local Sym   = {ako="Sym", pos=0,txt="",n=0, 
+               seen={}, most=0,mode=true}
+local Skip  = {ako="Skip"}
+local Row   = {ako="Row",  cells={}, bins={}}
+local Tbl   = {ako="Tbl",  rows={}, cols={}}
 
 ---------------------
 -- ## Shortcuts
-local function add(i, ...) return i.Is.add(i, ...) end
 local function cell(x) return not(type(x)=="string" and x=="?") end
+local isa
 
 ---------------------
 -- ## Column summaries
-function col.factory(j,s,t) 
-  local tmp = sym
-  if s:find(of.ch.num) then tmp = num  end
-  if s:find(of.ch.less) then tmp = num  end
-  if s:find(of.ch.more) then tmp = num  end
-  if s:find(of.ch.sym) then tmp = sym  end
-  if s:find(of.ch.skip) then tmp = skip end
+function Col.factory(j,s,t) 
+  local tmp = Sym
+  if s:find(Of.ch.num)   then tmp = Num  end
+  if s:find(Of.ch.less)  then tmp = Num  end
+  if s:find(Of.ch.more)  then tmp = Num  end
+  if s:find(Of.ch.sym)   then tmp = Sym  end
+  if s:find(Of.ch.skip)  then tmp = Skip end
   t.cols[j] = tmp.new(j,s)
-  if s:find(of.ch.klass) then t.class = t.cols[j] end 
-  end
+  if s:find(Of.ch.klass) then t.class = t.cols[j] end end
 
--- Generic columns
-function col.new(pos,txt,    i) 
-  i     = thing.new()
-  i.Is  = col
-  i.pos = pos or 0
-  i.txt = txt or ""
-  i.w   = i.txt:find(of.ch.less) and -1 or 1
-  return i end
-
--- Numeric columns
-function num.new(pos,txt,    i)  -- contractor
-  i = col.new(pos,txt)
-  i.Is, i.mu, i.m2, i.sd, i.n = num, 0, 0, 0, 0
-  i.lo = math.maxinteger
-  i.hi = math.mininteger
-  return i end
-
-function num.add(i,x) -- update
-  if cell(x) then
-    i.n = i.n + 1
-    local d = x - i.mu
-    i.mu = i.mu + d / i.n
-    i.m2 = i.m2 + d*(x - i.mu)
-    i.sd = i.m2<0 and 0 or (i.n<2 and 0 or (i.m2/(i.n -1))^0.5)
-    i.lo = math.min(i.lo,x)
-    i.hi = math.max(i.hi,x) end
+function Col.w(x)
+  x.w = x.txt:find(Of.ch.less) and -1 or 1
   return x end
 
--- No-op columns
-function skip.new(pos,txt,    i) -- constructor
-  i = col.new(pos,txt)
-  i.Is = skip 
-  return i end
+function Num.new(n,s) return Col.w(isa(Num, {txt=s, pos=n})) end
+function Sym.new(n,s) return Col.w(isa(Sym, {txt=s, pos=n})) end
+function Skip.new(n,s) return Col.w(isa(Skip,{txt=s, pos=n})) end 
 
-function skip.add(i, x) return x end -- update
-
--- Symbolic columns
-function sym.new(pos,txt,    i)  -- constructor
-  i = col.new(pos,txt)
-  i.Is, i.seen, i.most = sym, {}, 0
-  return i end
-
-function sym.add(i, x) -- update
+function Num:add(x) 
   if cell(x) then
-    i.seen[x] = (i.seen[x] or 0) + 1
-    if i.seen[x] > i.most then i.most,i.mode = i.seen[x],x end end
+    self.n = self.n + 1
+    local d = x - self.mu
+    self.mu = self.mu + d / self.n
+    self.m2 = self.m2 + d*(x - self.mu)
+    self.sd = self.m2<0 and 0 or (
+              self.n<2  and 0 or (
+              (self.m2/(self.n -1))^0.5))
+    self.lo = math.min(self.lo,x)
+    self.hi = math.max(self.hi,x) end
+  return x end
+
+function Skip:add(x) return x end 
+
+function Sym:add(x) 
+  if cell(x) then
+    self.seen[x] = (self.seen[x] or 0) + 1
+    if self.seen[x] > self.most then 
+      self.most, self.mode = self.seen[x], x end end
   return x end 
 
 ---------------------
 -- ## Row
-function row.new(tbl,cells,     i) -- constructor
-  i = thing.new()
-  i.Is, i.cells, i.bins = num, {}, {}
+function Row.new(row,tbl)
+  local i = isa(Row)
   for _,col in pairs(tbl.cols) do 
-    i.cells[col.pos] = add(col, cells[col.pos])
+    i.cells[col.pos] = col:add(row[col.pos])
     i.bins[col.pos] = i.cells[col.pos] end 
   return i end
 
 -- ## Tables 
--- Hold rows
-function tbl.new(i)  -- constructor
-  i = thing.new()
-  i.Is, i.cols, i.rows = tbl, {}, {}
-  return i end
+function Tbl.new() return isa(Tbl) end
 
-function tbl.add(i, t)  -- update
-  if #i.cols==0 then 
-    for j,x in pairs(t) do col.factory(j,x,i) end 
+function Tbl:add(t)  
+  if #self.cols==0 then 
+    for j,x in pairs(t) do Col.factory(j,x,self) end 
   else
-    i.rows[(#i.rows)+1] = row.new(i,t) end end
+    self.rows[(#self.rows)+1] = Row.new(t,self) end end
 
 -- Read from files
-function tbl.read(i,f) 
-  for t in lib.csv(f) do tbl.add(i, t) end
-  return i end
+function Tbl.read(f,    t) 
+  t=Tbl.new()
+  for row in Lib.csv(f) do t:add(row) end
+  return t end
 
 ---------------------
 -- ## Lib
 
-function lib.copy(obj,   old,new)
+function Lib.copy(obj,   old,new)
   if type(obj) ~= 'table' then return obj end
   if old and old[obj] then return old[obj] end
   old, new = old or {}, {}
   old[obj] = new
-  for k, v in pairs(obj) do new[k]=lib.copy(v, old) end
-end
+  for k, v in pairs(obj) do new[k]=Lib.copy(v, old) end
+  return new end
 
-local function lib.isa(klass,      new)
-  new = lib.copy(klass or {})
-  setmetatable(new, klass)
-  klass.__index = klass
-  return new
-end
+do
+  local id=0
+  function Lib.isa(klass,has,      new)
+    new = Lib.copy(klass or {})
+    for k,v in pairs(has or {}) do new[k] = v end
+    setmetatable(new, klass)
+    klass.__index = klass
+    id = id + 1
+    new.id = id
+    return new end end
 
--- Polymorphism (one ring to rule them all)
-function lib.go(i,f, ...) return i.Is[f](i, ...) end
+isa = Lib.isa
 
 -- Iterate on keys in sorted order
-function lib.order(t,  i,keys)
+function Lib.order(t,  i,keys)
   i,keys = 0,{}
   for key,_ in pairs(t) do keys[#keys+1] = key end
   table.sort(keys)
@@ -171,32 +153,32 @@ function lib.order(t,  i,keys)
       i=i+1; return keys[i], t[keys[i]] end end end 
 
 -- Simple print of a flat table
-function lib.o(z,pre,   s,c) 
+function Lib.o(z,pre,   s,c) 
   s, c = (pre or "")..'{', ""
-  for _,v in lib.order(z or {}) do s= s..c..tostring(v); c=", " end
+  for _,v in Lib.order(z or {}) do s= s..c..tostring(v); c=", " end
   print(s..'}') end
 
 -- Print nested tables. 
 -- Don't show private slots (those that start with `_`);
 -- show slots in sorted order;
 -- if `pre` is specified, then  print that as a prefix.
-function lib.oo(t,pre,    indent,fmt)
+function Lib.oo(t,pre,    indent,fmt)
   pre    = pre or ""
   indent = indent or 0
   if(indent==0) then print("") end
   if indent < 10 then
-    for k, v in lib.order(t or {}) do
-      if not (type(k)=='string' and k:match("^[A-Z]")) then
+    for k, v in Lib.order(t or {}) do
+      if not (type(k)=='string' and k:match("^_")) then
         if not (type(v)=='function') then
           fmt = pre..string.rep("|  ",indent)..tostring(k)..": "
           if type(v) == "table" then
             print(fmt)
-            lib.oo(v, pre, indent+1)
+            Lib.oo(v, pre, indent+1)
           else
             print(fmt .. tostring(v)) end end end end end end
 
 -- Warn about locals that have escaped into the global space
-function lib.rogues(    ignore,match)
+function Lib.rogues(    ignore,match)
   ignore = {
     jit=true, utf8=true,math=true, package=true, table=true, 
     coroutine=true, bit=true, os=true, io=true, 
@@ -209,14 +191,14 @@ function lib.rogues(    ignore,match)
 
 -- Return each row, split on ",", numstrings coerced to numbers,
 -- kills comments and whitespace.
-function lib.csv(file,     stream,tmp,t)
+function Lib.csv(file,     stream,tmp,t)
   stream = file and io.input(file) or io.input()
   tmp    = io.read()
   return function()
     if tmp then
       tmp = tmp:gsub("[\t\r ]*","") -- no whitespace
                :gsub("#.*","") -- no comemnts
-      t   = lib.split(tmp) 
+      t   = Lib.split(tmp) 
       tmp = io.read()
       if #t > 0 then 
         for j,x in pairs(t) do t[j] = tonumber(x) or x end
@@ -225,10 +207,10 @@ function lib.csv(file,     stream,tmp,t)
       io.close(stream) end end end
 
 -- Split the string `s` on separator `c`, defaults to "." 
-function lib.split(s,     c,t)
+function Lib.split(s,     c,t)
   t, c = {}, c or ","
   for y in string.gmatch(s, "([^" ..c.. "]+)") do t[#t+1] = y end
   return t end
 
 -- ## Return
-return {of=of, lib=lib, num=num, sym=sym, tbl=tbl}
+return {Of=Of, Lib=Lib, Num=Num, Sym=Sym, Tbl=Tbl}
---
+++ lua/axe/tests101.lua
@@ -0,0 +1,29 @@
+local m=require("keys0")
+math.randomseed(m.Of.seed)
+
+local o ,oo, go = m.Lib.o, m.Lib.oo, m.Lib.go
+local Num, Sym,Tbl  = m.Num, m.Sym, m.Tbl
+
+local function going(      x,y) 
+  x = Sym.new(1,"names")
+  y = Num.new(1,"door")
+  x:add("love")
+  x:add("hate")
+  x:add("hate")
+  y:add(20)
+  y:add(30)
+  oo(x)
+  oo(y) end
+
+local function csving()
+  for row in m.Lib.csv("data/weather.csv") do 
+    print(#row); o(row) end end
+
+local function rowsreading()
+  oo(Tbl.read("data/weather.csv"))
+end 
+
+going()
+csving()
+rowsreading()
+m.Lib.rogues()
