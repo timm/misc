8a1e4816b4403da2ca08944cc5b97f41bcff523e
Tim Menzies <timm@ieee.org>
2021-01-10 17:16:42 -0500

    saving

--- jl/jan21/col.jl
+++ jl/jan21/col.jl
@@ -15,7 +15,7 @@ function col(;txt="",pos=0, c=it.char)
 
 #---- Update -----------------------------------------------
 inc!(i,x)=
-  x==it.char.skip x : begin i.n += 1; inc1!(i,x); x end
+  x==it.char.skip ? x : begin i.n += 1; inc1!(i,x); x end
 
 function inc1!(i::Sym, x)
   new = i.seen[x] = 1 + get(i.seen,x,0)
@@ -33,16 +33,16 @@ function inc1!(i::Some, x::Number)
 
 #---- Query ------------------------------------------------
 norm!(i::Some,x, a=all(i), skip=x==it.char.skip) =
-  skip ? x : max(0,min(1, (x-a[1])/(a[end]-a[1]+1E-31))) 
+  skip ? x : max(0,min(1, (x-a[1]) / (a[end] - a[1]+1E-31))) 
 
 mid(i::Some;lo=no,hi=no) = per(i,p=.5,lo=lo,hi=hi)
 
-sd( i::Some;lo=no,hi=no) = 
+sd(i::Some;lo=no,hi=no) = 
   (per(i,p=.9,lo=lo,hi=hi) - per(i,p=.1,lo=lo,hi=hi))/2.54
 
 function per(i::Some;p=.5,lo=no,hi=no, lst=all(i))
-  hi = isnothing(hi) ? length(lst) : hi
-  lo = isnothing(lo) ? 1           : lo
+  hi = hi==no ? length(lst) : hi
+  lo = lo==no ? 1           : lo
   lst[ int(lo + p*(hi - lo +1)) ] end
 
 function all(i::Some)  
