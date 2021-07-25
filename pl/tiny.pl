fb77ad10612a00804ab1a579a0a31873c9df806a
Tim Menzies <timm@ieee.org>
2021-07-25 00:02:05 -0400

    as

--- pl/tiny.pl
+++ pl/tiny.pl
@@ -1,34 +1,46 @@
-:-  slot/6, (dynamic F), (discontiguous F).
-term_expantion(A=B,L) :- bagof(X,xpand(A=B,X),L)
+:- op(797,xfx,in).
 
-xpand1(Functor = Slots,
-    slot(Functor,Slot,Val1,Val2,T1,T2)) :-
-    length(Slots, Arity),
-    length(Vs1,    Arity),
-    length(Vs2,    Arity),
-    xpand2(Vs1,Vs2,Slots,Slot, Val1, Val2),
-    T1 =.. [Functor | Vs1],
-    T2 =.. [Functor | Vs2].
+:- discontiguous '_'/6.
+term_expansion(A=B,L) :- bagof(X,xpand(A=B,X),L).
+term_expansion((def(X0) --> Y0), goal_expansion(X,Y)) :-
+  dcg_translate_rule((X0 --> Y0), (X :- Y)).
 
-xpand2([V1|Vs],  [V2|Vs],  [Slot|_],  Slot, V1,V2).
-xpand2([V |V1s], [V |V2s], [_|Slots], Slot, V1,V2) :-
-  xpand2( V1s,      V2s,     Slots,  Slot, V1,V2).
+xpand(Functor = Slots, '_'(Functor,Slot,Val1,Val2,T1,T2)) :-
+  length(Slots, Arity),
+  length(Vs1,   Arity),
+  length(Vs2,   Arity),
+  T1 =.. [Functor | Vs1],
+  T2 =.. [Functor | Vs2],
+  xpand1(Vs1,Vs2,Slots,Slot, Val1, Val2).
 
-o(X,A) :- o(X,_,A).
+xpand1([X|Vs],[Y|Vs],[S|_], S,X,Y).
+xpand1([V|Xs],[V|Ys],[_|Ss],S,X,Y) :- xpand1(Xs,Ys,Ss,S, X,Y).
 
-o(X of  Y, A,B) :- slot(_,X,Old,New,A,B), o(Y,Old,New).
-o(X and Y, A,B) :- o(X,A,C), o(Y,C,B).
-o(a  F,    A,B) :-slot(F,_,_,_,A,B).
-o(an F,    A,B) :- slot(F,_,_,_,A,B)
-o(X @= Y/Z) --> slot(_,X,Y,Z).
-o(X ++ Y  ) --> slot(_,X,Z,[Y|Z]).
-o(X := Y  ) --> slot(_,X,_,Y).
-o(X =  Y  ) --> slot(_,X,Y,Y).
-o(X in Y  ) --> slot(_,X,Z,Z), member(Z,Y).
-o(X =< Y  ) --> slot(_,X,Z,Z), Z=<Y.
-o(X >= Y  ) --> slot(_,X,Z,Z), Z>=Y.
-o(X \= Y  ) --> slot(_,X,Z,Z), Z\=Y.
-o(X  < Y  ) --> slot(_,X,Z,Z), Z <Y.
-o(X  > Y  ) --> slot(_,X,Z,Z), Z >Y.
-iscu
+def($F   )  --> '_'(F,_,_,_).
+def(X/   Y) --> '_'(_,X,Y,Y).
+def(X/ Y/Z) --> '_'(_,X,Y,Z).
+def(X +  Y) --> '_'(_,X,Z, [Y|Z]).
+def(X =  Y) --> '_'(_,X,Y,Y).
+def(T in X) --> '_'(_,X,Z,Z), {member(T,Z)}.
+def(X =< Y) --> '_'(_,X,Z,Z), {Z=<Y}.
+def(X >= Y) --> '_'(_,X,Z,Z), {Z>=Y}.
+def(X \= Y) --> '_'(_,X,Z,Z), {Z\=Y}.
+def(X  < Y) --> '_'(_,X,Z,Z), {Z <Y}.
+def(X  > Y) --> '_'(_,X,Z,Z), {Z >Y}.
+
+emp = [name,age,shoesize].
+
+p --> $emp, name=tim, age=10, shoesize=20.
+
+fred=[a,b].
+
+q --> $fred, a=10, love in shoesize, b=10,a/Old,{print(Old)}.
+
+terms = [all].
+
+reads(F) --> 
+  $terms,!, {open(F,read,S)}, reads1(S), {close(F)}.
+reads1(S) -->
+  $terms,!, read(S, X), (X=end_of_file -> true | data(X), reads1(S)).
+
 
