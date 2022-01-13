0c4e3ae5ad97711fe765d91d2d37c7eb692216f6
Tim Menzies <timm@ieee.org>
2022-01-13 00:52:20 -0500

    save

--- pl/db.pl
+++ pl/db.pl
@@ -3,22 +3,21 @@
 :- op(998,xfy, has ).
 :- op(701,xfx, in ).
 
-:- discontiguous has/5, isa/2.
+:- discontiguous has/5, isa/2, new/2.
 
-xpand(def(F,Has), has(X,V0,V1,Term0,Term1)) :-
-  member(X=_,Has),
-  args(Has,V0,V1,L0,L1,X),
+so(F=L, new(F,New))  :- maplist(arg(2),L,New0), New =.. [F|New0].
+so(F=L, isa(F,Term)) :- length(L,A), functor(Term,F,A).
+so(F=L, has(X,V0,V1,Term0,Term1)) :-
+  member(X=_,L),
+  so1(L,V0,V1,L0,L1,X),
   Term0 =.. [F|L0],
   Term1 =.. [F|L1].
-xpand(def(F,Has), isa(F,Term)) :-
-  length(Has,A),
-  functor(Term,F,A).
 
-args([],      _, _, [],      [],      _).
-args([H=_|Has],V0,V1,[V0|V0s],[V1|V1s],H) :-       args(Has,_, _, V0s,V1s,H).
-args([H=_|Has],V0,V1,[V |V0s],[V |V1s],X) :- H\=X, args(Has,V0,V1,V0s,V1s,X).
+so1([],      _, _,[],      [],      _).
+so1([H=_|L],V0,V1,[V0|V0s],[V1|V1s],H) :-       so1(L,_, _, V0s,V1s,H).
+so1([H=_|L],V0,V1,[V |V0s],[V |V1s],X) :- H\=X, so1(L,V0,V1,V0s,V1s,X).
 
-term_expansion(T = L,Xs) :- bagof(X,xpand(def(T,L),X) ,Xs).
+term_expansion(T = L,Xs) :- bagof(X,so(T=L,X) ,Xs).
 
 with(T=Out,With) :- isa(T,Term), o(With,Term,Out).
 :-op(999,xfx,with).
