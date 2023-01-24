fc07b28fe5c777924e1db8b4613e8be50b11bf78
Tim Menzies <timm@ieee.org>
2023-01-23 20:25:21 -0500

    save

--- pl/one.pl
+++ pl/one.pl
@@ -20,10 +20,17 @@ xpand1([V|Xs],[V|Ys],[_|Ss],S,X,Y) :- xpand1(Xs,Ys,Ss,S, X,Y).
 new(F,_,T) :- slot(F,_,_,_,T).
 
 num=[at,txt,mu,m2,lo,hi].
-
-new(num,at,txt) :-
+new(num,At,Txt) -->
   new(num),
+  at=At,
+  txt=Txt,
   n = 0,
   m2= 0,
   hi= -1E31,
   lo=  1E31.
+
+sym=[at,txt,mode,most,seen].
+
+inc([H,V, [],        [H=V |T]).
+inc([H,V, [H=V0|T],  [H=V1|T]):- !,V1 is V0+V.
+inc([H,V, [H   |T0], [H   |T]):- inc(H,V,T0,T).
