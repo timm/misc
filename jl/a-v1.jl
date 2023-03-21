f475a230f853b4f65f8f46d395a8369265af9e04
Tim Menzies <timm@users.noreply.github.com>
2023-03-21 15:43:37 -0400

    as

--- jl/a-v1.jl
+++ jl/a-v1.jl
@@ -28,13 +28,13 @@ int(x)  = floor(Int,x)
 any(a)  = a[ int(length(a) * rand()) + 1]
 many(a,n=the.divs.few) = length(a) < n  ? a : [any(a)  for _ in 1:n]
  
-function say(i)
+function oo(i) println(o(i));  i end
+function o(i)
   s,pre="$(typeof(i)){",""
   for f in sort!([x for x in fieldnames(typeof(i)) if !("$x"[1] == '_')])
-    g = getfield(i,f)
-    s = s * pre * "$f=$g" 
+    s = s * pre * "$f=$getfield(i,f)" 
     pre=", " end
-  print(s * "}") end
+  s * "}" end
 
 @with_kw mutable struct Num  
   pos=0; txt=""; w=1; n=0; lo=10^32; hi=-1*10^32; mu=0; m2=0; sd=nothing  end
