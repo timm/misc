4edef63edf0420253ad20bba085b788504260df2
Tim Menzies <timm@ieee.org>
2023-04-19 01:02:01 -0400

    as

--- py/twos.py
+++ py/twos.py
@@ -1,6 +1,15 @@
+# vim: set ts=2 sw=2 et:
+"""
+  -a --aaa asdas = 23
+  -h --help  asass = False
+
+"""
+import re
 import sys
 import ast
 import math
+import random
+from copy import deepcopy
 from dataclasses import dataclass as rec
 from typing import Dict,Any,List
 
@@ -81,27 +90,48 @@ class DATA:
   def add(i,row):
     if x: 
       [col.add(row.cells[col.at]) for cols in [i.x, i.y] for col in cols]
+      i.rows += [row]
     else: 
       i.names = row.cells
       i.cols = [COL(at=i,txt=s) for i,s in enumerate(i.names)]
       for col in i.cols:
-        if col.txt[-1] != "X": (i.x if col.txt[-1] in "+-" else ii.y).append(col)
+        if col.txt[-1] != "X": 
+           (i.x if col.txt[-1] in "+-" else ii.y).append(col)
     return i
+
+#-------------------------
+def ok(f):
+  def fun(b4):
+    print(f.__name__)
+    random.seed(b4.seed)
+    tmp = f()
+    global the
+    for k,v in b4.items(): the[k]=v
+    return 1 if tmp==False else 0
+  funs += [f]
+  return f
+#-------------------------
+@ok
+def aaA(): print(1)
+@ok
+def the(): print(the)
+#-------------------------
 
 def coerce(x):
   if x=="?": return x
   try: return ast.literal_eval(x)
   except: return x
 
-#-------------------------
-def okAA(): print(1)
+class BAG(dict):
+  __getattr__ = dict.get
+  __setattr__ = dict.__setitem__
 
-def runs(todo):
-  fails=0
-  for k,fun in locals().items():
-    if k[:2]=="ok" and (todo==k[2:] or todo=="."):
-      fails += 1 if fun()==False else 0
-  sys.exit(fails)
+def runs(todo)
+  sys.exit(sum((f(deepcopy(the)) for f in funs)))
+#-------------------------
+the=BAG()
+for m in re.finditer(r"\n\s*-\w+\s*--(\w+)[^=]*=\s*(\S+)",__doc__):
+  the[m[1]] = coerce(m[2])
 
 if __name__ == "__main__":
-  runs(sys.argv[1] if len(sys.argv) == 2 else "nothing")
+  runs(sys.argv[1] if len(sys.argv) == 2 else lambda: print(__doc__))
