be293de5f61b984722d843097d3f3357c8df23bf
Tim Menzies <timm@ieee.org>
2023-05-25 14:02:29 +1000

    as

--- pl/ab.pl
+++ pl/ab.pl
@@ -8,23 +8,23 @@ Tim Menzies
 
 #### Background knowledge*/
 
-     happy/t        :- rich/t,healthy/t.
-     happy/t        :- tranquility /hi.
-     tranquility/hi :- conscience/clear.
-     tranquility/hi :- satiated/t.
-     satiated/t     :- diet/fatty.
-     healthy/t      :- diet/light. /*
+happy/t        :- rich/t,healthy/t.
+happy/t        :- tranquility /hi.
+tranquility/hi :- conscience/clear.
+tranquility/hi :- satiated/t.
+satiated/t     :- diet/fatty.
+healthy/t      :- diet/light. /*
 
 ###  Current facts. */
 
-     rich/t.
+rich/t.
  
-     %tim/f.
-     %satiated/f.  /*
+%tim/f.
+%satiated/f.  /*
 
 ### High level goal */
 
-     todo/t        :- happy/t. /*
+todo/t        :- happy/t. /*
 
 ## Output
 
@@ -78,63 +78,62 @@ Here's anohter run. Hmmm... different order. Why?
 
 ### Working memory management */
  
-     reset  :-
-     	dynamic(assuption/3),
-     	retractall(assumption(_,_,_)).
-     
-     report :- write('% assumptions\n'), report1, fail.
-     report :- nl.
-     
-     report1 :-
-     	setof(Z*(X/Y),assumption(X,Y,Z),All),
-     	member(One,All),
-     	write(One),
-     	nl. /*
+reset  :-
+	dynamic(assuption/3),
+	retractall(assumption(_,_,_)).
+
+report :- write('% assumptions\n'), report1, fail.
+report :- nl.
+
+report1 :-
+	setof(Z*(X/Y),assumption(X,Y,Z),All),
+	member(One,All),
+	write(One),
+	nl. /*
 
 
 ## Standard stuff */
  
-     goes :- go, fail.
-     goes.
-     
-     goes(X) :- go(X),fail.
-     goes(_).
-     
-     go    :- reset, run,    report.
-     go(X) :- reset, run(X), report.
-     
-     run    :- maybe(todo/t).
-     run(X) :- maybe(X). /*
+goes :- go, fail.
+goes.
+
+goes(X) :- go(X),fail.
+goes(_).
+
+go    :- reset, run,    report.
+go(X) :- reset, run(X), report.
+
+run    :- maybe(todo/t).
+run(X) :- maybe(X). /*
 
 ## Assumption management */
  
-     assume(X/Y,_)   :- assumption(X,Z,_),!,Y=Z.
-     assume(X/Y,How) :- bassert(assumption(X,Y,How)).
-     
-     bassert(X) :- assert(X).
-     bassert(X) :- retract(X),fail. /*
+assume(X/Y,_)   :- assumption(X,Z,_),!,Y=Z.
+assume(X/Y,How) :- bassert(assumption(X,Y,How)).
+
+bassert(X) :- assert(X).
+bassert(X) :- retract(X),fail. /*
 
 ## Assumption generation */
  
 maybe(X) :- once(maybe0(X,Y)), maybe1(Y).
-     
-     maybe0((X,Y),      (X,Y)).
-     maybe0(X/Y,        fact(X/Y))      :- clause(X /_,true).
-     maybe0(X/Y,        rule(X/Y))      :- clause(X/_,_).
-     maybe0(X/Y,        abducible(X/Y)).
-     
-     maybe1((X,Y))        :- maybe(X),maybe(Y).
-     maybe1(abducible(X)) :- assume(X,0).
-     maybe1(fact(X))      :- assume(X,1), one(X).
-     maybe1(rule(X))      :- assume(X,2), one(clause(X,Y)), maybe(Y). /*
+
+maybe0((X,Y),      (X,Y)).
+maybe0(X/Y,        fact(X/Y))      :- clause(X /_,true).
+maybe0(X/Y,        rule(X/Y))      :- clause(X/_,_).
+maybe0(X/Y,        abducible(X/Y)).
+
+maybe1((X,Y))        :- maybe(X),maybe(Y).
+maybe1(abducible(X)) :- assume(X,0).
+maybe1(fact(X))      :- assume(X,1), one(X).
+maybe1(rule(X))      :- assume(X,2), one(clause(X,Y)), maybe(Y). /*
 
 ## Random ordering
 
 Return solutions to goal C<X>, in some random order. */
  
-     one(X) :-
-     	setof(N/X,(X, N is random(10000)), All),
-     	member(_/X,All).
+one(X) :-
+	setof(N/X,(X, N is random(10000)), All),
+	member(_/X,All).
 
-     :- print(100),goes, halt.
-
+:- print(100),goes, halt.
