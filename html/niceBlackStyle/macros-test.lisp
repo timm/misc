a73ca432abb64531026d1e6143b4bfd087b37700
Tim Menzies <timm@ieee.org>
2023-07-25 11:46:55 -0400

    as

--- html/niceBlackStyle/macros-test.lisp
+++ html/niceBlackStyle/macros-test.lisp
@@ -10,7 +10,7 @@
 (defun make-person (name yob role)
   (make-person0 :name name 
                 :role role
-                :age (- (sixth (multiple-value-list (get-decoded-time))) yob)))
+               :age (- (sixth (multiple-value-list (get-decoded-time))) yob)))
 
 (defun make-crew (crew)
   (make-crew0 :persons (loop for (name yob role) in crew collect (make-person name yob role))))
