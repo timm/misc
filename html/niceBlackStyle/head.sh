7198ecd985c35ec22d8f928cc6524704d029967c
Tim Menzies <timm@ieee.org>
2023-07-25 22:39:10 -0400

    saving

--- html/niceBlackStyle/head.sh
+++ html/niceBlackStyle/head.sh
@@ -7,6 +7,7 @@ EOF
 gawk 'sub(/^# /,""){print "<title>"$0"</title>"}' $1
 
 cat<<-'EOF'
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family='Roboto Mono'">
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="b.css">
