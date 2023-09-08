c4e5d231819a62bc834f65aa56626f95f30cf70c
Tim Menzies <timm@ieee.org>
2023-09-07 23:44:48 -0400

    as

--- lua/pipes/lib.lua
+++ lua/pipes/lib.lua
@@ -1,5 +1,38 @@
 local M={}
+---- maths
+function M.rnd(num, numDecimalPlaces,    mult)
+  mult = 10^(numDecimalPlaces or 0)
+  return math.floor(num * mult + 0.5) / mult end
+
+---- lists
 function M.map(t,fun,    u) u={}; for k,v in pairs(t) do u[k] = fun(v) end; return u end
+function M.kap(t,fun,    u) 
+  u={}; for k,v in pairs(t) do v,k=fun(k,v); u[k==nil and 1+#u or k]=v  end; return u end
+
+function M.sort(t,fun) table.sort(t,fun); return t end
+
+function M.copy(t,    u) 
+  u={}; for k,v in pairs(t) do u[copy(k)]=copy(v) end; 
+  return setmetable(u,getmetatable(t)) end
+
+---- settings
+function M.settings(s)
+  t={_help=s}
+  s:gsub("\n[%s]+[-][%S][%s]+[-][-]([%S]+)[^\n]+= ([%S]+)",function(k,v) t[k]=M.coerce(v) end)
+  return t end
+
+function M.cli(t)
+  for k,v in pairs(t,    s) do
+    s=tostring(v)
+    for n,x in ipairs(arg) do
+      if x=="-"..(k:sub(1,1)) or x=="--"..k then
+        t[k]= M.coerce(s=="false" and "true" or s=="true" and "false" or arg[n+1]) end end end 
+  if t.help then print(t._help) end
+  return t end
+
+---- strings
+M.cat = table.concat
+M.fmt = string.format
 
 function M.trim(s)  return s:match'^%s*(.*%S)' or '' end
 
@@ -15,19 +48,7 @@ function M.csv(sFilename,fun,     src,s)
   while true do
     s = io.read(); if s then fun(M.coerces(s)) else return io.close(src) end end end
 
-function M.settings(s)
-  t={_help=s}
-  s:gsub("\n[%s]+[-][%S][%s]+[-][-]([%S]+)[^\n]+= ([%S]+)",function(k,v) t[k]=M.coerce(v) end)
-  return t end
-
-function M.cli(t)
-  for k,v in pairs(t,    s) do
-    s=tostring(v)
-    for n,x in ipairs(arg) do
-      if x=="-"..(k:sub(1,1)) or x=="--"..k then
-        t[k]= M.coerce(s=="false" and "true" or s=="true" and "false" or arg[n+1]) end end end 
-  if t.help then print(t._help) end
-  return t end
+function M.oo(x,  n) print(M.o(x,  n)); return x end
 
 function M.o(x,  n,    t)
   if type(x) == "function" then return "f()" end
@@ -35,14 +56,7 @@ function M.o(x,  n,    t)
   if type(x) == "number"   then return M.rnd(x,d==nil and 2 or d) end
   t={}; for k,v in pairs(x) do 
           if tostring(k):sub(1,1) ~= "_" then 
-            t[1+#t] = #x>0 and M.o(v,n) or string.format(":%s %s",k,M.o(v,n)) end end 
-  if #x==0 then table.sort(t) end
-  return "{"..table.concat(t," ").."}" end
-  
-function M.oo(x,  n) print(M.o(x,  n)); return x end
-
-function M.rnd(num, numDecimalPlaces,    mult)
-  mult = 10^(numDecimalPlaces or 0)
-  return math.floor(num * mult + 0.5) / mult end
+            t[1+#t] = #x>0 and M.o(v,n) or M.fmt(":%s %s",k,M.o(v,n)) end end 
+  return "{" .. M.cat(#x==0 and sort(t) or t," ") .. "}" end
 
 return M
