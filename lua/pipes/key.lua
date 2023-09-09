302e9d3915342803a0775aeef6a05d53e6e74aac
Tim Menzies <timm@ieee.org>
2023-09-09 00:33:41 -0400

    saving

--- lua/pipes/key.lua
+++ lua/pipes/key.lua
@@ -1,97 +1,113 @@
 #!/usr/bin/env lua
-l=require"lib"
-local the=l.settings[[
+local lint,list,maths,obj,rand,run,settings,str = require"lib"
+local the=settings.create[[
 
 bins: discretize
 
-USAGE: ./bins
+USAGE: ./key.lua [OPTIONS]
 
 OPTIONS
-  -b --bins number of bins = 5
-  -f --file file to load   = 
-  -h --help show help      = false]]
+  -b --bins      number of bins         = 5
+  -d --decimals  print first `decimals` = 2
+  -f --file      csv file to load       = 
+  -g --go        start up action        = nothing
+  -h --help      show help              = false]]
 
-local box,csv,kap,oo,push,sort = l.box,l.csv,l.kap,l.oo,l.push,l.sort
-----------1---------2---------3---------4----------5----------6----------7---------8---------9
+-------------------- ------------------- --------------------- -------------------- ----------
+local csv,kap,oo,push,sort = str.csv,list.kap,str.oo,list.push,list.sort
 local cos,exp,log,max,min,pi = math.cos,math.exp,math.log,math.max,math.min,math.pi
-local DATA,ROW
+local DATA,ROW,NUM,SYM=obj"DATA", obj"ROW", obj"NUM", obj"SYM"
 
-local NUM,SYM
+-------------------- ------------------- --------------------- -------------------- ----------
+function SYM:init(at,txt) 
+  return {n=0,at=at or 0,txt=txt or "",most=0,mode=nil,has={}} end
 
-function SYM(at,txt) return {is=SYM, n=0,at=at or 0,txt=txt or "",most=0,mode=nil,has={}} end
-function NUM(at,txt) return {is=NUM, n=0,at=at or 0,txt=txt or "",mu=0,m2=0,sd=0} end  
+function SYM:add(x)
+  if x~="?" then 
+    self.n = self.n + 1 
+    self.has[x] = 1 + (self.has[x] or 0)
+    if self.has[x] > self.most then 
+      self.most, self.mode = self.has[x],x end end end
 
-local function add2Col(col,x,    d)
-  if x=="?" then return x end
-  col.n = col.n + 1 
-  if   col.is == SYM 
-  then col.has[x] = 1 + (col.has[x] or 0)
-       if col.has[x] > col.most then col.most, col.mode = col.has[x],x end 
-  else d      = x - col.mu
-       col.mu = col.mu + d/col.n 
-       col.m2 = col.m2 + d*(x - col.mu) 
-       col.sd = exp(-.5*((x - col.mu)/col.sd)^2) / (col.sd*((2*pi)^0.5)) 
-       col.lo = min(col.lo,x)
-       col.hi = max(col.hi,x) end end
+function SYM:bin(x) return x end
 
-local _bins={}
-_bins[ 3] = { -.43,	 .43}
-_bins[ 4] = { -.67,     0,	 .67}
-_bins[ 5] = { -.84,	 -.25,	 .25,  .84}
-_bins[ 6] = { -.97,	 -.43,    0,	 .43,  .97}
-_bins[ 7] = { -1.07,	-.57,	-.18,	 .18,  .57, 1.07}
-_bins[ 8] = { -1.15,	-.67,	-.32, 	 0,	 .32,  .67, 1.15}
-_bins[ 9] = { -1.22,	-.76,	-.43,	-.14,	 .14,	 .43,  .76,	1.22}
-_bins[10] = { -1.28,	-.84,	-.52,	-.25,	   0,	 .25,  .52,	 .84,	1.28}
+-------------------- ------------------- --------------------- -------------------- ----------
+function NUM:init(at,txt) 
+	return {is=NUM, n=0,at=at or 0,txt=txt or "",mu=0,m2=0,sd=0} end  
 
-local function bin(col,x,     tmp)
+function NUM:add(x,    d)
+  if x ~="?" then 
+    self.n  = self.n + 1 
+    d       = x - self.mu
+    self.mu = self.mu + d/self.n 
+    self.m2 = self.m2 + d*(x - self.mu) 
+    self.sd = exp(-.5*((x - self.mu)/self.sd)^2) / (self.sd*((2*pi)^0.5)) 
+    self.lo = min(self.lo,x)
+    self.hi = max(self.hi,x) end end
+
+NUM._bins= {
+    [ 3] = { -.43,	 .43},
+    [ 4] = { -.67,     0,	 .67},
+    [ 5] = { -.84,	 -.25,	 .25,  .84},
+    [ 6] = { -.97,	 -.43,    0,	 .43,  .97},
+    [ 7] = { -1.07,	-.57,	-.18,	 .18,  .57, 1.07},
+    [ 8] = { -1.15,	-.67,	-.32, 	 0,	 .32,  .67, 1.15},
+    [ 9] = { -1.22,	-.76,	-.43,	-.14,	 .14,	 .43,  .76,	1.22},
+    [10] = { -1.28,	-.84,	-.52,	-.25,	   0,	 .25,  .52,	 .84,	1.28}}
+
+function NUM:bin(x,     tmp)
   if x=="?"      then return x end
   if col.is==SYM then return x end 
   tmp = (x- col.mu)/col.sd
   for b,x in pairs(breaks[the.bins])  do if tmp <= x then return b end end
   return the.bins end
-
-function COLS(t,    cols)
-  cols = {is=COLS, all={}, x={}, y={}, names=t}
+  
+-------------------- ------------------- --------------------- -------------------- ----------
+function COLS:init(t)
+  self.all={}; self.x={}; self.y={}; self.names=t
   for at,txt in pairs(t) do 
-    col = push(i.cols, (txt:find"^A-Z" and NUM or SYM)(at,txt))
+    col = push(self.all, (txt:find"^A-Z" and NUM or SYM)(at,txt))
     if txt:find"X$" then
-      push(txt:find"[+-]$" and cols.y or cols.x, col) end end 
-  return cols end 
+      push(txt:find"[+-]$" and self.y or self.x, col) end end end
    
-function ROW(t,data) return {_data=data,rows=t; bins=copy(t)} end
+-------------------- ------------------- --------------------- -------------------- ----------
+function ROW:init(t,data) 
+  return {_data=data,rows=t; bins=list.copy(t)} end
 
-function DATA(src,    data,add1)
-  function _add1(row)
-    if   data.cols 
-    then push(data.rows, row)
-         for _,cols in pairs{data.cols.x, data.cols.y} do 
-           for _,col in pairs(cols) do 
-             add(col, row.cells[col.at]) end end 
-    else data.cols = cols(row.cells) end end 
-  -----------------------------------
-  data = {is=DATA, rows={}}
-  if   type(s) == "string" 
-  then csv(the.file,  function(t) _add1(data,ROW(t,data)) end) 
-       _discretize(data)
-  else for _,row in pairs(src or {}) do _add1(data,row) end       
-  return data end
-
-function _discretize(data)
-  for _,col in pairs(data.cols.all) do 
-    for _,row in pairs(data,rows) do 
-      row.bins[col.at] = bin(col, row.cells[col.at]) end end end end end  
-
-local function dist(row1,row2)
-  XXX over anutjomg
+function ROW.dist(i,j)
+ --- XXX over anutjomg
   d,n = 0,0
   for _,col in pairs(row1._data.cols.x) do 
     n = n+1
     d = d+ )_dist1(col, row1.dist[col.at], row2.dist[col.at]])^the.p end
   return (d/n)^(1/the,p)
 
-the = l.cli(the)
-print(table.concat(row,", "))
-for _,row  in pairs(rows) do
-  print(table.concat(row,", "))
-  print(table.concat(map(row,bin),", ")) end
+-------------------- ------------------- --------------------- -------------------- ----------
+function DATA:init(src)
+  self.rows={}
+  if   type(s) == "string" 
+  then csv(the.file,  function(t) self:add(data,ROW(t,data)) end) 
+       self:bins()
+  else for _,row in pairs(src or {}) do self:add(data,row) end
+  end       
+  return data end
+
+function DATA:all(row,fun,     x)
+  for _,cols in pairs{self.cols.x, self.cols.y} do 
+    for _,col in pairs(cols) do 
+      x= row[col.at]
+      if x ~= "?" then fun(col,x) end end end end
+   
+function DATA:add(row)
+    if   self.cols 
+    then push(self.rows, row)
+         self:all(row, function(col,x) col:add(x) end) 
+    else self.cols = COLS(self.cells) end end 
+
+function DATA:bins()
+  for _,row in pairs(rows) do
+    self:all(row, function(col,x) row.bins[col.at] = col:bin(x) end) end end
+
+-------------------- ------------------- --------------------- -------------------- ----------
+
+return {the=the, DATA=DATA, ROW=ROW, SYM=SYM, NUM=NUM, COLS=COLS}
--- lua/pipes/lib.lua
+++ lua/pipes/lib.lua
@@ -1,5 +1,27 @@
+----------1---------2---------3---------4----------5----------6----------7---------8---------9
+---- lint
+local lint={}
+lint.b4={}; for k,v in pairs(_ENV) do b4[k]=k end
+
+function lint.rogues() 
+  for k,v in pairs(_ENV) do 
+    if not b4[k] then print("E> rogue",k," of " type(v)) end end end
+
+---- names
 local exp,sqrt,log,cos,floor,pi = math.exp,math.sqrt,math.log,math.cos,math.floor,math.pi
-local cli,maths,list,rand,run,str = {},{},{},{},{},{}
+local lint,list,maths,rand,run,settings,str = {},{},{},{},{},{},{}
+local obj
+
+---- object
+local id=0
+local function obj(s,    t) 
+  t = {_name=s} 
+  t.__index = t 
+  return setmetatable(t, {
+     __call=function(_,...)
+        id = id + 1
+        local i = setmetatable({_id=id},t);
+        return setmetatable(t.init(i,...) or i,t) end}) end
 
 ---- maths
 function maths.rnd(num, numDecimalPlaces,    mult)
@@ -7,28 +29,34 @@ function maths.rnd(num, numDecimalPlaces,    mult)
   return floor(num * mult + 0.5) / mult end
 
 ---- lists
-function list.map(t,fun,    u) u={}; for k,v in pairs(t) do u[k] = fun(v) end; return u end
-function list.kap(t,fun,    u) 
-  u={}; for k,v in pairs(t) do v,k=fun(k,v); u[k==nil and 1+#u or k]=v  end; return u end
+function list.map(t,fun,    u) 
+  u={}; for k,v in pairs(t) do u[k] = fun(v) end; return u end
 
-function list.sort(t,fun) table.sort(t,fun); return t end
+function list.kap(t,fun,    u) 
+  u={}; for k,v in pairs(t) do v,k=fun(k,v); u[k==nil and 1+#u or k]=v  end
+  return u end
+
+function list.sort(t,fun) 
+  table.sort(t,fun); return t end
 
 function list.copy(t,    u)
   u={}; for k,v in pairs(t) do u[str.list.copy(k)] = str.list.copy(v) end; 
   return setmetable(u,getmetatable(t)) end
 
 ---- settings
-function cli.create(s)
+function settings.create(s)
   t={_help=s}
-  s:gsub("\n[%s]+[-][%S][%s]+[-][-]([%S]+)[^\n]+= ([%S]+)",function(k,v) t[k]=str.coerce(v) end)
+  s:gsub("\n[%s]+[-][%S][%s]+[-][-]([%S]+)[^\n]+= ([%S]+)",
+         function(k,v) t[k]=str.coerce(v) end)
   return t end
 
-function cli.update(t)
+function settings.update(t)
   for k,v in pairs(t,    s) do
     s=tostring(v)
     for n,x in ipairs(arg) do
       if x=="-"..(k:sub(1,1)) or x=="--"..k then
-        t[k]= str.coerce(s=="false" and "true" or s=="true" and "false" or arg[n+1]) end end end 
+        t[k]= str.coerce(s=="false" and "true" or 
+                         s=="true"  and "false" or arg[n+1]) end end end 
   if t.help then print(t._help) end
   return t end
 
@@ -52,30 +80,23 @@ function rand.norm(mu,sd,     r)
   r=rand.rand
   return (mu or 0) + (sd or 1)* sqrt(-2*log(r())) * cos(2*pi*r()) end
 
---------------------20-----------------40-----------------60-----------------80 
-
-local breaks={
-    3={−0.43,0.43},
-    4={−0.67,0,0.67},
-    5={−0.84,−0.25,0.25,0.84},
-    6={−0.97,−0.43,0,0.43,0.97},
-    7={−1.07,−0.57,−0.18,0.18,0.57,1.07},
-    8={−1.15,−0.67,−0.32,0,0.32,0.67,1.15},
-    9={−1.22,−0.76,−0.43,−0.14,0.14,0.43,0.76,1.22},
-    10={,−1.28,−0.84,−0.52,−0.25,0,0.25,0.52,0.84,1.28}}
-
-
 --- cli
-function run.go(the,fun,    ok,b4,result,out)
-  b4={}; for k,v in pairs(the) do b4[k]=v end
-  math.randomseed(the.seed or 1234567891)
-  rand.seed  = the.seed or 1234567891
+function run.run(settings,name, fun,    ok,b4,result,out)
+  b4={}; for k,v in pairs(settings) do b4[k]=v end
+  math.randomseed(settings.seed or 1234567891)
+  rand.seed  = settings.seed or 1234567891
   ok, result = pcall(fun)
   out        = ok and result or false
-  if not ok then print("❌ FAIL ",str.eman(fun),":",result) end
-  for k,v in pairs(b4) do the[k]=v end
+  if not ok then print("❌ FAIL ",name,":",result) end
+  for k,v in pairs(b4) do settings[k]=v end
   return out end 
 
+function run.runs(settings)
+  for k,fun in pairs(_ENV) do
+    pre,name = name:match"(%w+)_(.+)"
+    if pre=="eg_" and "settings.go"==name or "settings.go"=="all" then
+       run.run(settings,fun
+    if k:find"^eg_" then
 ---- str
 str.cat = table.concat
 str.fmt = string.format
@@ -84,29 +105,36 @@ function str.trim(s)  return s:match'^%s*(.*%S)' or '' end
 
 function str.coerce(s)
   s=str.trim(s) 
-  return math.tointeger(s) or tonumber(s) or s=="true" or (s ~= "false" and s) end
+  return (math.tointeger(s) or tonumber(s) or 
+          s=="true" or (s ~= "false" and s)) end
 
 function str.coerces(s,    t)
-  t={}; for s1 in s:gmatch("([^,]+)") do t[1+#t]=str.coerce(s1) end; return t end
+  t={}; for s1 in s:gmatch("([^,]+)") do t[1+#t]=str.coerce(s1) end
+  return t end
+
+function str.eman(x)
+  for k,v in pairs(_ENV) do if x==v then return k end end
+  return "?" end
 
 function str.csv(sFilename,fun,     src,s)
   src = io.input(sFilename)
   while true do
     s = io.read(); if s then fun(str.coerces(s)) else return io.close(src) end end end
 
-function str.oo(x,  n) print(str.o(x,  n)); return x end
+function str.oo(x,  n) 
+  print(str.o(x,  n)); return x end
 
 function str.o(x,  n,    t)
   if type(x) == "function" then return str.eman(x).."()" end
   if type(x) ~= "table"    then return tostring(x) end
-  if type(x) == "number"   then return str.rnd(x,d==nil and 2 or d) end
+  if type(x) == "number" then
+    if math.floor(x) ~= x then 
+      x = str.rnd(x, d or the.decimals or  2) end
+    return tostring(x) end 
   t={}; for k,v in pairs(x) do 
           if tostring(k):sub(1,1) ~= "_" then 
-            t[1+#t] = #x>0 and str.o(v,n) or str.fmt(":%s %s",k,str.o(v,n)) end end 
-  return "{" .. str.cat(#x==0 and sort(t) or t," ") .. "}" end
+            if #x>0 then t[1+#t] = str.o(v,n) 
+            else         t[1+#t] = str.fmt(":%s %s", k, str.o(v,n)) end end 
+  return (t._name or "").. "{" .. str.cat(#x==0 and sort(t) or t," ") .. "}" end
 
-function str.eman(x)
-  for k,v in pairs(_ENV) do if x==v then return k end end
-  return "?" end
-
-local cli,list,maths,rand,run,str
+return lint,list,maths,obj,rand,run,settings,str 
