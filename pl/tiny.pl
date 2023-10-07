c4fe8d0315d644d6e112dbed9b03c96cf3e93123
Tim Menzies <timm@users.noreply.github.com>
2023-10-07 14:01:01 -0400

    as

--- pl/tiny.pl
+++ pl/tiny.pl
@@ -6,29 +6,32 @@ with goal-expansions commented out:
 
 :- listing(p), listing(q).
 
-p(emp(A, B, C), D) :-
+p(emp(tim, 10, A), B) :-
     true,
-    slot(_, name, tim, tim, emp(A, B, C), E),
-    slot(_, age, 10, 10, E, F),
-    slot(_, shoesize, G, G, F, H),
-    G>20,
-    D=H.
+    true,
+    true,
+    true,
+    A>20,
+    B=emp(tim, 10, A).
 
-q(fred(A, B), C) :-
+q(fred(10, A), B) :-
     true,
-    slot(_, a, 10, 10, fred(A, B), D),
-    slot(_, shoesize, E, E, D, F),
-    member(love, E),
-    G=F,
-    slot(_, b, 10, 10, G, H),
-    /(a, Old, H, I),
+    true,
+    slot(_, shoesize, C, C, fred(10, A), fred(Old, 10)),
+    member(love, C),
+    fred(Old, 10)=fred(Old, 10),
+    true,
+    true,
     print(Old),
-    C=I.
-
+    B=fred(Old, 10).
 */
 
 :- discontiguous slot/6.
-%---------------------------------------------------------
+:- op(1200, xfx, --->).
+:- op(700, xfx, :=).
+:- op(700, xfx, in).
+
+%--------- ---------- --------- --------- --------- --------- ----
 % term accessors 
 term_expansion(X=L0, L) :- findall(Y, xpand(X=L0,Y), L).
 
@@ -43,13 +46,9 @@ xpand(Functor = Slots, slot(Functor,Slot,Val1,Val2,T1,T2)) :-
 xpand1([X|Vs],[Y|Vs],[S|_], S,X,Y).
 xpand1([V|Xs],[V|Ys],[_|Ss],S,X,Y) :- xpand1(Xs,Ys,Ss,S, X,Y).
 
-:- op(1200, xfx, --->).
-:- op(700, xfx, :=).
-:- op(700, xfx, in).
-
 % some inline goal-expansions
-term_expansion((X0 ---> Y0), [(X :- Y), 
-                              goal_expansion(X,Y)]) :-
+term_expansion((X0 ---> Y0), 
+               [(X :- Y), goal_expansion(X,Y)]) :-
   dcg_translate_rule((X0 --> Y0), (X :- Y)).
 
 % some DCG magic
@@ -64,9 +63,12 @@ in(T,X) ---> slot(_,X,Z,Z), {member(T,Z)}.
 >( X,Y) ---> slot(_,X,Z,Z), {Z >Y}.
 
 % define some swaps (to be done at load time)
-goal_expansion(of(F,X,X),true) :- ground(F), slot(F,_,_,_,X,X).
+one(X) :- bagof(_,X,[X]).
 
-%---------------------------------------------------------
+goal_expansion(of(F,X,X),true) :- one(slot(F,_,_,_,X,X)).
+goal_expansion(slot(A,B,C,D,E,F),true) :- one(slot(A,B,C,D,E,F)).
+
+%--------- ---------- --------- --------- --------- --------- ----
 % some assertions in timm's tiny object language
 
 emp = [name,age,shoesize].
@@ -75,10 +77,7 @@ p --> of(emp), name=tim, age=10,  shoesize>20.
 
 fred=[a,b].
 
-q --> of(fred), a=10, love in shoesize, b=10,a/Old,{print(Old)}.
-
-terms = [all].
+q --> of(fred), a=10, love in shoesize, b=10,a = Old,{print(Old)}.
 
 :- listing(p).
 :- listing(q).
-
