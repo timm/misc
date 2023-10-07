db665591dd62ec932c9ff11e2655c262d20104c9
Tim Menzies <timm@ieee.org>
2023-10-07 07:07:32 -0400

    fixed

--- pl/tiny.pl
+++ pl/tiny.pl
@@ -6,61 +6,34 @@ with goal-expansions commented out:
 
 :- listing(p), listing(q).
 
-p(A, B) :-
-    of(emp, A, C),
-    =(name, tim, C, D),
-    =(age, 10, D, E),
-    >(shoesize, 20, E, B).
+p(emp(A, B, C), D) :-
+    true,
+    slot(_, name, tim, tim, emp(A, B, C), E),
+    slot(_, age, 10, 10, E, F),
+    slot(_, shoesize, G, G, F, H),
+    G>20,
+    D=H.
 
-q(A, B) :-
-    of(fred, A, C),
-    =(a, 10, C, D),
-    in(love, shoesize, D, E),
-    =(b, 10, E, F),
-    /(a, Old, F, G),
+q(fred(A, B), C) :-
+    true,
+    slot(_, a, 10, 10, fred(A, B), D),
+    slot(_, shoesize, E, E, D, F),
+    member(love, E),
+    G=F,
+    slot(_, b, 10, 10, G, H),
+    /(a, Old, H, I),
     print(Old),
-    B=G.
+    C=I.
 
-# EXAMPLE2:
-
-with goal-expansions uncommented (lots of load-time optimizations)
-
-:- listing(p), listing(q).
-
-p(emp(tim, 10, A), B) :-
-    true,
-    true,
-    true,
-    >(shoesize, 20, emp(tim, 10, A), B).
-
-q(fred(10, A), B) :-
-    true,
-    true,
-    in(love, shoesize, fred(10, A), fred(C, 10)),
-    true,
-    /(a, Old, fred(C, 10), D),
-    print(Old),
-    B=D.
- */
+*/
 
 :- discontiguous slot/6.
-
+:- op(1200, xfx, --->).
 :- op(700, xfx, :=).
 :- op(700, xfx, in).
 
-% some DCG magic
-=( X,Y) --> slot(_,X,Y,Y).
-:=(X,Y) --> slot(_,X,_,Y).
-+( X,Y) --> slot(_,X,Z, [Y|Z]).
-in(T,X) --> slot(_,X,Z,Z), {member(T,Z)}.
-=<(X,Y) --> slot(_,X,Z,Z), {Z=<Y}.
->=(X,Y) --> slot(_,X,Z,Z), {Z>=Y}.
-\=(X,Y) --> slot(_,X,Z,Z), {Z\=Y}.
-<( X,Y) --> slot(_,X,Z,Z), {Z <Y}.
->( X,Y) --> slot(_,X,Z,Z), {Z >Y}.
-
 %---------------------------------------------------------
-% timm's tiny object language
+% term accessors 
 term_expansion(X=L0, L) :- findall(Y, xpand(X=L0,Y), L).
 
 xpand(Functor = Slots, slot(Functor,Slot,Val1,Val2,T1,T2)) :-
@@ -74,14 +47,25 @@ xpand(Functor = Slots, slot(Functor,Slot,Val1,Val2,T1,T2)) :-
 xpand1([X|Vs],[Y|Vs],[S|_], S,X,Y).
 xpand1([V|Xs],[V|Ys],[_|Ss],S,X,Y) :- xpand1(Xs,Ys,Ss,S, X,Y).
 
-%---------------------------------------------------------
+% some inline goal-expansions
+term_expansion((X0 ---> Y0), [(X :- Y), 
+                              goal_expansion(X,Y)]) :-
+  dcg_translate_rule((X0 --> Y0), (X :- Y)).
+
+% some DCG magic
+=( X,Y) ---> slot(_,X,Y,Y).
+:=(X,Y) ---> slot(_,X,_,Y).
++( X,Y) ---> slot(_,X,Z, [Y|Z]).
+in(T,X) ---> slot(_,X,Z,Z), {member(T,Z)}.
+=<(X,Y) ---> slot(_,X,Z,Z), {Z=<Y}.
+>=(X,Y) ---> slot(_,X,Z,Z), {Z>=Y}.
+\=(X,Y) ---> slot(_,X,Z,Z), {Z\=Y}.
+<( X,Y) ---> slot(_,X,Z,Z), {Z <Y}.
+>( X,Y) ---> slot(_,X,Z,Z), {Z >Y}.
+
 % define some swaps (to be done at load time)
-goal_expansion(X,X)            :- predicate_property(X, built_in),!.
 goal_expansion(of(F,X,X),true) :- ground(F), slot(F,_,_,_,X,X).
-goal_expansion(X,true) :- 
-  clause(X,slot(A,B,C,D,E,F)),
-  slot(A,B,C,D,E,F).
-%
+
 %---------------------------------------------------------
 % some assertions in timm's tiny object language
 
