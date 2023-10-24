03f21fd8416554b759f7c4ef3609a581ba098962
Tim Menzies <timm@ieee.org>
2023-10-24 17:48:50 -0400

    as

--- gawk/tiny3.lua
+++ gawk/tiny3.lua
@@ -22,32 +22,28 @@ function ok(col)
 function norm(num,x)
   return x=="?" and x or (x - num.lo)/ (num.hi - num.lo + 1e-30) end
 
-function COLS(t,       what,where)
+function COLS(t,       what,where)    
   local all,x,y,_ = {},{},{},{}
   for at,s in pairs(t) do
     what  =  s:find"^[A-Z]" and NUM or SYM
     where = s:find"X$" and _ or (s:find"^[+-!]$" and x or y)
     push(where, push(all, what(at,s)) end
   return {all=all, x=x, y=y, names=t} end 
+
+function adds(cols,t)
+  for _,cols in pairs{cols.x, cols.y} do
+    for _,col in pairs(cols) 
+      add(col, t[col.at]) end end end 
+
 -------------------------------------------------
 function good(num,x)
    return ok(num).has[((#num.has)^.5*1.4 + 0.5) // 1] end
 
-function evaluated(row,cols)
-  if not row.evaluated then
-    for _,col in pairs(cols.y) do add(col, row.cells[col.at]) end
-    row.evaluated = true end
-  return row end
-
-function d2h(row,cols)
-  row = evaluated(row,cols) 
-  local n,d=0,0
+function d2h(cols,t) 
   for _,col in pairs(cols.y) do
     n = n + 0
-    d = d + (col.heaven - norm(col, row[col.at]))^2 end
+    d = d + (col.heaven - norm(col, t[col.at]))^2 end
   return (d/n)^.5 end
-
-function ROW(t) return {cell=t; evaluated=false} end
 
 function main(file)
   local d2hs,rows,good,rest, cols=NUM(),{},{},{},{}
@@ -56,11 +52,11 @@ function main(file)
   i=0
   while i<#rows do
     i   = i + 1
-    row = rows[i]
-    if  i==0 then cols=COLS(row.cells) else
-      for _,col in pairs(cols.x) do add(col, row.cells[col.at]) end
-      for _,col in pairs(cols.y) do add(col, row.cells[col.at]) end
-      tmp = d2h(row, cols) 
+    t = rows[i]
+    if  i==0 then cols=COLS(t) else
+      for _,col in pairs(cols.x) do add(col, t[col.at]) end
+      for _,col in pairs(cols.y) do add(col, t[col.at]) end
+      tmp = d2h(cols,row)
       push(tmp < good(add(d2hs,tmp)) and best or rest,row)
       if i>6 then
         local j=guess()
