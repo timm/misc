8b91b75823223e0c56564c79a67434f8f221301f
Tim Menzies <timm@ieee.org>
2023-12-01 17:28:22 -0500

    saved

--- gawk/lean/dostat.lua
+++ gawk/lean/dostat.lua
@@ -4,11 +4,11 @@ local obj   = require"obj"
 local stats = require"stats"
 
 local the   = lib.settings[[
-dostats: generate stats for each class
+dostat: count stats over all data
 (c) 2023 Tim Menzies, BSD-2
 
 USAGE:
-  lua dostats.lua [OPTIONS]
+  lua dostat.lua [OPTIONS]
 
 OPTIONS:
   -f --file  data file               = data/diabetes.csv
@@ -21,15 +21,22 @@ local oo,mid,div          = lib.oo,  obj.mid, obj.div
 
 -- ## Examples -------------------------------------------------
 local eg={}
+function eg.sym(      sym1,m,d)
+  sym1 = SYM()
+  for _,x in pairs{1,1,1,1,2,2,3} do obj.col(sym1,x) end
+  m,d = obj.mid(sym1), obj.div(sym1)
+  print(m,d)
+  return 1== m and 1.37 <  d and d < 1.38 end
 
-function eg.datas(data1,   datas,rows1)
-  datas = {}
-  for n,t in lib.csv(the.file) do
-    row1 = ROW(t)
-    if n==0 then all = DATA({row1}) else
-      k = t[all.cols.klass.at]
-      datas[k] = datas[k] or obj.clone(all)
-      data(datas[k], row1) end end end 
+function eg.num(     num1,m,d)
+  num1 = NUM()
+  for i=1,100 do obj.col(num1, i) end
+  m, d = obj.mid(num1), obj.div(num1)
+  print(m,d, stats.stdev(obj.has(num1)))
+  return 50 == m and m < 51 and 29 < d and d < 32 end
+
+function eg.data(data1)
+  oo(obj.stats(DATA(the.file))) end
 
 -- ## main ---------------------------------------------------
 lib.run(the,eg)
--- gawk/lean/obj.lua
+++ gawk/lean/obj.lua
@@ -3,68 +3,68 @@ local stats = require"stats"
 local l={}
 
 -- ## One Col ---------------------------------------------
-function l.COL(n,s)
+function l.COL(n,s) --> NUM or SYM
   return ((s or ""):find"^[A-Z]" and l.NUM or l.SYM)(n,s) end
 
-function l.SYM(n,s)
+function l.SYM(n,s) --> SYM
   return {at=n, txt=s, isIgnored = (s or ""):find"X$",
           isSym=true, has={},ok=false } end
 
-function l.NUM(n,s) 
+function l.NUM(n,s) --> NUM
   return {at=n, txt=s, isIgnored = (s or ""):find"X$",
           has={}, isSorted=false,
           heaven = (s or ""):find"-$" and 0 or 1} end
 
-function l.col(col1,any,     t) 
+function l.col(col1,any,     t) --> nil 
   if any ~= "?" then
     t = col1.has 
     if   col1.isSym
     then t[any] = 1+(t[any] or 0) 
     else t[1+#t]=any; col1.isSorted=false end end end
 
-function l.has(col1)
+function l.has(col1) --> any
   if not (col1.isSym or col1.isSorted) then table.sort(col1.has); col1.isSorted=true end
   return col1.has end
 
-function l.mid(col1)
+function l.mid(col1) --> any
   return (col1.isSym and stats.mode or stats.median)(l.has(col1)) end
 
-function l.div(col1)
+function l.div(col1) --> n
   return (col1.isSym and stats.entropy or stats.spread)(l.has(col1)) end
 
 -- ## Many Cols ---------------------------------------------
-function l.COLS(t,      x,y,all)
+function l.COLS(t,      x,y,all) --> COLS
   x, y, all = {}, {}, lib.kap(t, l.COL) 
   for at, col1 in pairs(all) do
     if not col1.isIgnored then
       (col1.txt:find "[-!+]$" and y or x)[at]=col1 end end
   return {names=t, x=x, y=y, all=all} end
 
-function l.cols(cols1, t)
+function l.cols(cols1, t) --> nil
   for _, col1 in pairs(cols1.all) do
     l.col(col1, t[col1.at]) end end
 
 -- ## Cols and Rows ---------------------------------------------
-function l.DATA(s_or_ts,    data1)
+function l.DATA(s_or_ts,    data1) --> DATA
   data1 = {rows={},cols=nil}
   if   type(s_or_ts)=="string"
 	then for _,t in lib.csv(s_or_ts) do l.data(data1,t) end
 	else for _,t in pairs(s_or_ts)   do l.data(data1,t) end end
 	return data1 end
 
-function l.data(data1, t)
+function l.data(data1, t) --> nil
   if   data1.cols
   then l.cols(data1.cols, t)
        data1.rows[ 1+#data1.rows ] = t
   else data1.cols = l.COLS(t) end end
 
-function l.stats(data1, fun,ndecs,cols1,     t)
+function l.stats(data1, fun,ndecs,cols1,     t) --> t[s=n]
     t = { N = #data1.rows }
     for _, col1 in pairs(cols1 or data1.cols.y) do
         t[col1.txt] = lib.rnd((fun or l.mid)(col1), ndecs) end
     return t end
 
-function l.clone(data1,  ts,     data2)
+function l.clone(data1,  ts,     data2) --> DATA
   data2 = DATA({data1.col.names})
   for _,t in pairs(ts or {}) do  data(data2,t) end 
   return data2 end
