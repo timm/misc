fd2641bf3f5280ca6f6e86b2b9e353e9074dad04
Tim Menzies <timm@ieee.org>
2024-04-20 02:02:20 +0100

    as

--- gawk/lite/lite.lua
+++ gawk/lite/lite.lua
@@ -1,32 +1,63 @@
-local l,m={},{}
-function m.settings() return {
+local push
+-------------------------------------------------------
+local settings,the
+
+function settings() return {
   file="../../data/auto93.csv",
   seed=1234567891,
-  magic={y="[-+!]$", x="[^-+!]$",min="-$", num="^[A-Z]"}} end
+  magic={y="[-+!]$", min="-$", num="^[A-Z]"}} end
 
-local the=m.settings()
+the=settings()
+-------------------------------------------------------
+local col,columns,DATA,new
 
-function m.data(strs)
-  return {names=  strs,
-          xs=     l.kap(strs, function(s) if s:find(the.magic.x) then return s end end)
-          ys=     l.kap(strs, function(s) if s:find(the.magic.y) then return s end end)
-          heaven= l.kap(strs, function(s) return s:find(the,magic.min) and -1 or 1 end)
-          rows= {}, n={}, seen={}, mu={}, m2={}, hi={}, lo={}} end
+function DATA(strs) return {
+  names=strs, 
+  rows={},
+  y={}, x={}, heaven={}, 
+  n={}, 
+  seen={}, 
+  mu={}, m2={}, hi={}, lo={}}  end
 
-function m.init(data)
-  for c,name in pairs(data.names) do
-    data.n[c]=0
-    if   name:find(the.magic.num)
-    then data.mu[c] = 0
-         data.m2[c] = 0
-         data.lo[c] =  1E30
-         data.hi[c] = -1E30  
-    else data.seen[c]={} end end 
-  return data end  
+function new(strs,    data) 
+  data=DATA(strs)
+  for c,str in pairs(strs) do 
+    columns(data,c,str)
+    col(data,c,str) end 
+  return data end
 
-function m.nump(data,c) return data.mu[c] end
+function columns(data,c,str)
+  data.n[c]=0
+  if not str:find(the.magic.y) then push(data.cols.x, c) else
+    push(data.cols.y, c)
+    data.cols.heaven[c] = str:find(the.magic.min) and 0 or 1
 
-function m.adds(data,lst) for _,t in pairs(lst) do m.add(data,t) end; return data end
+function col(data,c,str)
+  if not str:find(the.magic.num) then data.seen[c]={} else
+    data.mu[c] = 0
+    data.m2[c] = 0
+    data.lo[c] =  1E30
+    data.hi[c] = -1E30 end end 
+-------------------------------------------------------
+local add,add1,adds,nump
+
+function adds(data,lst) for _,t in pairs(lst) do add(data,t) end; return data end
+
+function add(data,t) 
+  push(data.rows, t)
+  for c,x in pairs(t) do
+    if x ~= "?" then 
+      data.n[c] = data.n[c]+1
+      add1(data,c,x) end end end
+
+function add1(data,c,x,    delta)
+  if not nump(data,c) then data.seen[c][x] = 1 + (data.seen[c][x] or 0)  else
+    delta      = x - data.mu[c]
+    data.mu[c] = data.mu[c] + delta/data.n[c]
+    data.m2[c] = data.m2[c] + delta*(x - data.mu[c])
+    if x > data.hi[c] then data.hi[c] = x end
+    if x < data.lo[c] then data.lo[c] = x end end end
+  
+function nump(data,c) return data.mu[c] end
 #--------------------------------------------------------------
-function l.kap(t,fun,...)
-  local u={};for k,v in pairs(t) do u[k]=fun(v,...) end; return u end
+function push(t,x) t[1+#t]=x; return x end
