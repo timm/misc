38d7b1234bd9034cb7a8381c4d1ed4a9b32232ac
Tim Menzies <timm@ieee.org>
2024-04-21 20:36:11 +0100

    as

--- gawk/lite/lite.lua
+++ gawk/lite/lite.lua
@@ -4,91 +4,125 @@ lite.lua : very simple sequential model optimizer.
 Look around a little, learn a little, decide what to do next
 (c) 2024 Tim Menzies <timm@ieee.org> BSD 2 clause.
 ]]
-local push,entropy
+local SYM,NUM,DATA = {},{},{}
+local klass,adds,cat,entropy,fmt,kat,kap,map,normal,show,push,sort
 -------------------------------------------------------
 local function settings() return {
   file  = "../../data/auto93.csv",
   seed  = 1234567891,
-  magic = {y="[-+!]$", 
-           min="-$", 
-           num="^[A-Z]"}}as end
+  magic = {y="[-+!]$", --[a]
+           min="-$",
+           num="^[A-Z]"
+          }} end
 
 local the=settings()
 -------------------------------------------------------
-local function SYM(name,at)
+function SYM:new(name,at)
   return {name=name or "", at=at or 0, n=0, seen={}, most=0, mode=nil} end
 
-local function NUM(name,at)
-  return {isNum=true, at=at or 0, name = name or "", n=0,
+function NUM:new(name,at)
+  return {at=at or 0, name = name or "", n=0,
           heaven=(name or ""):find(the.magic.min) and 0 or 1,
           mu=0,m2=0,lo=1E30, hi=-1E30} end
 
-local function DATA(strs,    all,x,y)
+function DATA:new(strs,    all,x,y)
   all,x,y = {},{},{}
   for n,s in pairs(strs) do
     push(all,
-      push(s:find(the.magic.y) and y or x,
-           (s:find(the.magic.num) and NUM or SYM)(s,n))) end
+      push(s:find(the.magic.y) and y or x,  
+        (s:find(the.magic.num) and NUM or SYM)(s,n))) end
   return {rows={}, cols={names=strs, x=x, y=y, all=all}} end
 -------------------------------------------------------
-local function addSym(sym,x)
-  sym.seen[x] = 1 + (sym.seen[x] or 0)  end
+function SYM:add(x)
+  self.n = self.n + 1
+  self.seen[x] = 1 + (self.seen[x] or 0)  end
 
-local function addNum(num,x,     delta)
-  num.n  = num.n + 1
-  delta  = x - num.mu
-  num.mu = num.mu + delta/num.n
-  num.m2 = num.m2 + delta*(x - num.mu)
-  if x > num.hi then num.hi = x end
-  if x < num.lo then num.lo = x end end
+function NUM:add(x,     delta)
+  self.n  = self.n + 1
+  delta  = x - self.mu
+  self.mu = self.mu + delta/self.n
+  self.m2 = self.m2 + delta*(x - self.mu)
+  if x > self.hi then self.hi = x end
+  if x < self.lo then self.lo = x end end
 
-local function add(data,t,    x)
-  print(data.rows)
-  push(data.rows, t)
-  for _,col in pairs(data.cols.all) do
+function DATA:add(t,    x)
+  push(self.rows, t)
+  for _,col in pairs(self.cols.all) do
     x = t[col.at]
-    if x ~= "?" then 
-      col.n = col.n + 1
-      (col.isNum and addNum or addSym)(col,x) end end end
-
+    if x ~= "?" then col:add(col,x) end end end
 --------------------------------------------------------------
-local div,mid,norm,sd -- norm (defined above)
+function SYM:mid() return self.mode end
+function NUM:mid() return self.mu end
 
-function div(col)    return col.isNum and sd(col) or entropy(col.has) end
-function mid(col)    return col.isNum and col.mu or col.mode end
-function norm(num,x) return x=="?"and x or (x - num.lo)/(num.hi - num.lo + 1E-30) end
-function sd(num)     return num.n < 2 and 0 or (num.m2/(num.n - 1))^.5 end
+function SYM:div() return entropy(self.has) end
+function NUM:div() return self.n < 2 and 0 or (self.m2/(self.n - 1))^.5 end
 
-function loss(data,t,    d)
+function NUM:norm(x) return x=="?"and x or (x - self.lo)/(self.hi - self.lo + 1E-30) end
+
+function DATA:loss(t,    d)
   d = 0
-  for _,c in pairs(data.cols.y) do d=d + (norm(c, t[c.at]) - c.heaven)^2 end
-  return (d/#data.cols.y)^.5 end
+  for _,col in pairs(self.cols.y) do d=d + (col:norm(t[col.at]) - col.heaven)^2 end
+  return (d/#self.cols.y)^.5 end
 ----------------------------------------------------------------
 -- ## Misc library functions
-function push(t,x) t[1+#t]=x; return x end
 
+-- ### Stats
 function entropy(t,   N,e)
   e,N=0,0
   for _,n in pairs(t) do N=N+n end 
   for _,n in pairs(t) do e=e + n/N*math.log(n/N,2) end
   return -e end
+
+function normal(mu,sd,    r)
+  r = math.random
+  return (mu or 0) + (sd or 1) * math.sqrt(-2 * math.log(r()))* math.cos(2 * math.pi * r()) end
+
+-- ### Lists
+function adds(thing,t) for _,x in pairs(t) do thing:add(x) end; return thing end
+
+function sort(t,fun) table.sort(t,fun); return t end
+function push(t,x)   t[1+#t]=x; return x end
+
+function map(t,fun,    u) u={}; for _,v in pairs(t) do push(u, fun(v))   end; return u end
+function kap(t,fun,    u) u={}; for k,v in pairs(t) do push(u, fun(k,v)) end; return u end
+
+-- ### Objects
+function klass(str,t)
+  t.__index   = t
+  t.__tostring= function(...) return str..kat(...) end
+  setmetatable(t, {__call = function(_,...)
+                              local i = setmetatable({},t)
+                              return setmetatable(t.new(i,...) or i, t) end}) end
+
+-- ### Print control
+fmt = string.format
+
+function cat(t) return '{' .. table.concat(map(t,show), ", ") .. '}' end
+
+function kat(t,     fun) -- like "cat", but assummes symbolic indexes
+  fun = function (k,v) if k[1] ~= "_" then return fmt("%s=%s",k, show(v)) end end
+  return cat(sort(kap(t,fun ))) end
+
+function show(x, nDecs,    mult) 
+  if type(x) ~= "number" then return tostring(x) end
+  if math.floor(x) == x  then return tostring(x) end
+  mult = 10^(nDecs or the.decimals)
+  return tostring(math.floor(x * mult + 0.5) / mult) end
 ----------------------------------------------------------------
 -- ## Examples
 local eg = {}
-
-local function normal(mu,sd,    R)
-  R=math.random
-  return (mu or 0) + (sd or 1) * math.sqrt(-2 * math.log(R()))
-                               * math.cos(2 * math.pi * R()) end
 
 local function run(x)
   the = settings()
   math.randomseed(the.seed or 1234567891)
   return eg[x]() end
 
-function eg.num(  n)
-  n=NUM()
-  for _=1,10^4 do addNum(n,normal(10,2)) end
-  print(sd(n), mid(n)) end
+function eg.num(  n,t)
+  t={}; for _=1,10^4 do push(t,normal(10,2)) end;  n=adds(NUM(),t)
+  print(n:div(), n:mid()) end
+----------------------------------------------------------------
+-- ## Start uo
+kap({SYM=SYM, NUM=NUM, DATA=DATA},klass)
 
-if  not pcall(debug.getlocal, 4, 1) and arg[1] then run(arg[1]) end
+if not pcall(debug.getlocal, 4, 1) and arg[1] then run(arg[1]) end
+return {help=help, the=the, SYM=SYM, NUM=NUM, DATA=DATA}
