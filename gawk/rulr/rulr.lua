348f81e805ad5dd36fdfbbfe3721a6f68435a43b
timm <timm@ieee.org>
2024-06-10 01:11:57 -0400

    ruler

--- gawk/rulr/rulr.lua
+++ gawk/rulr/rulr.lua
@@ -4,73 +4,135 @@ local the={about={what="rulr",
                   why="experiment with fast clustering",
                   who="Tim Menzies",
                   when=2024,
-                  copyright="BSD, two clause"}m
-           a=1}
+                  copyright="BSD, two clause"},
+           train="auto93.csv"}
 
 local l,b4={},{}; for k,_ in pairs(_ENV) do b4[k]=k end
-
 local DATA,NUM,SYM,COLS={},{},{},{}
 
-function COLS.new(names,     i)
-  i = l.is(COLS,{all={}, x={}, y={}, names=names})
+-------------------------------------------------------
+function COLS.new(names,     self)
+  self = l.is(COLS, {all={}, x={}, y={}, names=names})
   for n,s in pairs(names) do
-    push(i.all, (s:find"^[A-Z]" and NUM or SUM)(s,n))
-    if not s:find"X$" then push(s:find"[-+!]$" and i.y or i.x, col) end end
-  return i end
-
-function DATA.new(it,   i) 
-  i = l.is(DATA,{rows={}, cols=COLS.new(it())}) 
-  for t in it do i:add(t) end  
-  return i end
-
-function Data
+    push(self.all, (s:find"^[A-Z]" and NUM or SUM)(s,n))
+    if not s:find"X$" then push(s:find"[-+!]$" and self.y or self.x, col) end end
+  return self end
 
 -------------------------------------------------------
+function DATA.new(it,   self) 
+  self = l.is(DATA, {rows={}, cols=COLS.new(it())}) 
+  for t in it do self:add(t) end  
+  return self end
+
+function DATA:add(t)
+  l.push(self.rows, t)
+  for _,cols in pairs{self.x, self.y} do
+    for _,col in pairs(cols) do col:add(t[col.at]) end end end
+
+-------------------------------------------------------
+function SYM.new(s,n)
+  return l.is(SYM, {txt=s, at=n, n=0, has={}, mode=nil, most=0, 
+                    heaven=(s or ""):find"-$" and 0 or 1}) end
+
+function SYM:mid() return self.mode end
+function SYM:div() return entropy(self.has) end
+
+function SYM:add(x)
+  if x ~= "?" then
+    self.n = 1 + self.n
+    self.has[x] = 1 + (self.has[x] or 0)
+    if self.has[x] > self.most then self.most, self.mode = self.has[x], x end end end
+
+--------------------------------------------------------
+function NUM.new(s,n)
+  return l.is(NUM, {txt=s, at=n, n=0, mu=0, m2=0, sd=0, lo=1E30, hi= -1E30}) end
+
+function NUM:mid() return self.mu end
+function NUM:div() return self.sd end
+
+function NUM:add(x,     d)
+  if x ~= "?" then
+    self.n  = 1 + self.n
+    self.lo = math.min(x, self.lo)
+    self.hi = math.min(x, self.hi)
+    d       = x - self.mu
+    self.mu = self.mu + d/i.n
+    self.m2 = self.m2 + d*(x - i.mu)
+    self.sd = (self.m2/(self.n - 1 + 1E-30))^0.5 end end
+
+--------------------------------------------------------
+-- ## Misc Functions
+
+-- Shortcuts
 l.cat = table.concat
 l.fmt = string.format
 
-function l.is(x,y) x.__index=x; setmetatable(y,x); return x end
+-- Objects
+function l.is(x,y) x.__tostring=l.o; x.__index=x; setmetatable(y,x); return x end
+
+-- ### Lists
+function l.sort(t,fun) table.sort(t,fun); return t end
 
 function l.push(t,x) t[1+#t]=x; return x end
 
 function l.items(t,n)
   k,n=0,#t
-  function() if k < n then k=k+1; return t[k] end end end
+  return function() if k < n then k=k+1; return t[k] end end end
 
-function l.sort(t,fun) table.sort(t,fun); return t end
+-- ### Maths
+function l.entropy(t,   e,N)
+  n=0; for _,n in pairs(t) do N = N + n end
+  e=0; for _,n in pairs(t) do e = e - (n/N)*math.log(n/N,2) end 
+  return e end
 
+-- ### String to Thing
 function l.coerce(s,     other) 
   _other = function(s) if s=="nil" then return nil  end
                        return s=="true" or s ~="false" and s or false end 
   return math.tointeger(s) or tonumber(s) or _other(s:match'^%s*(.*%S)') end
 
+function l.cells(s,    t)
+  t={}; for s1 in s:gsub("%s+", ""):gmatch("([^,]+)") do t[1+#t]=l.coerce(s1) end
+  return t end
+
 function l.csv(src)
   src = src=="-" and io.stdin or io.input(src)
-  return function(      s,t)
-    s=io.read()
-    if s then
-      t={}; for s1 in s:gsub("%s+", ""):gmatch("([^,]+)") do t[1+#t]=l.coerce(s1) end
-      return t
-    else io.close(src) end end end
+  return function(      s)
+    s = io.read()
+    if s then return l.cells(s) else io.close(src) end end end
 
-### Pretty print
+-- ### Pretty print
 
 function l.oo(t) print(l.o(t)); return t end
 
-function l.o(t)
- if type(t) ~= "table" then return tostring(t) end
-   return (t._name or "") .. "(" .. l.cat((#t==0 and l.dict or l.list)(t,{}) ," ") .. ")" end
-
-function l.list(t,u) 
+local function list(t,u) 
   for _,v in pairs(t) do u[1+#u] = l.o(v) end ; return u end
 
-function l.dict(t,u) 
-  for k,v in pairs(t) do u[1+#u] = l.fmt(":%s %s",k,l.o(v)) end; return l.sort(u) end
+local function dict(t,u) 
+  for k,v in pairs(t) do u[1+#u] = l.fmt(":%s %s",k,l.o(v)) end; return l.sort(u) end 
 
+function l.o(t)
+  if type(t) ~= "table" then return tostring(t) end
+    return (t._name or "") .. "{" .. l.cat((#t==0 and dict or list)(t,{}) ," ") .. "}" end
+
+-- ### List
 function l.rogues() 
   for k,v in pairs(_ENV) do if not b4[k] then print("Rogue?",k,type(v)) end end end
+
 -------------------------------------------------------
+-- ## Start-up Actions
+
+-- Where to store the actions.
 local eg={}
 
+function eg.oo() l.oo(the) end
 
-eg[arg[1]]()
+function eg.version() print("rulr version 0.2") end
+
+function eg.csv()
+  for t in l.csv(the.train)  do l.oo(t) end end
+
+function eg.data()
+  DATA.new(l.csv(the.train)) end
+
+eg[arg[1] or "version"]()
