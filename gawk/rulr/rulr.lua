cc9aa167f6cebf97711e180b23bf017b0006b9e4
timm <timm@ieee.org>
2024-06-10 21:04:03 -0400

    as

--- gawk/rulr/rulr.lua
+++ gawk/rulr/rulr.lua
@@ -5,6 +5,7 @@ local the={about={what="rulr",
                   who="Tim Menzies",
                   when=2024,
                   copyright="BSD, two clause"},
+           bins=16,
            seed=1234567891,
            train="auto93.csv"}
 
@@ -13,11 +14,15 @@ local DATA,NUM,SYM,COLS={},{},{},{}
 
 -------------------------------------------------------
 function COLS.new(names,     self,col)
-  self = l.is(COLS, {all={}, x={}, y={}, names=names})
-  for n,s in pairs(names) do
-    col = l.push(self.all, (s:find"^[A-Z]" and NUM or SYM).new(s,n))
-    if not s:find"X$" then l.push(s:find"[-+!]$" and self.y or self.x, col) end end
+  self = l.is(COLS, { all={}, x={}, y={}, names=names })
+  for n,s in pairs(names) do self:add2Col(n,s) end
   return self end
+
+function COLS:add2Col(n,s,    col)
+  col= (s:find"^[A-Z]" and NUM or SYM).new(s,n)
+  l.push(self.all, col)
+  if not s:find"X$" then 
+    l.push(s:find"[-+!]$" and self.y or self.x, col) end end
 
 -------------------------------------------------------
 function DATA.new(it,   self) 
@@ -64,6 +69,70 @@ function NUM:div() return self.sd end
 
 function DATA:mids(cols) 
   return l.map(cols or self.cols.y, function(col) return l.rnd(col:mid()) end) end
+
+--------------------------------------------------------
+function SYM:bin(x) return x end
+function NUM:bin(x) return math.min(the.bins, 1+((the.bins * self:norm(x))//1)) end
+
+local BIN={}
+function BIN.new(at,txt,lo,hi,n)
+  return l.is(BIN,{at=at, txt=txt, lo=lo, hi=hi or lo, n=n or 0, ys={}}) end
+
+function BIN:add(x,y)
+  self.n  = self.n + 1
+  self.lo = math.min(x, self.lo)
+  self.hi = math.max(x, self.hi)
+  self.ys[y] = 1 + (self.ys[y] or 0) end
+
+function BIN:mergeable(other, small)
+  both = BIN.new(self.at, self.txt, self.lo, other.hi, self.n + other.n)
+  for _,ys in pairs{self.ys, other.ys} do
+    for k,v in pairs(ys) do
+      both.ys[k] = v + (both.ys[k] or 0) end end
+  e1,e2,e12 = l.entropy(self.ys), l.entropy(other.ys), l.entropy(both.ys)
+  if self.n < small or other.n < small then return both end
+  if e12 <= (self.n*e1 + other.n*e2) / both.n then return both end end 
+  
+function DATA:bins(klasses,     all,tmp,n)
+  all = {}
+  for _,col in pairs(self.cols.x) do
+    tmp, n = self:divides(col,klasses)
+    for _,bin in pairs(col:merges(tmp, n/the.bins)) do l.push(all, bin) end end
+  return all end
+
+function DATA:divides(col,klasses)
+  out,n = {},0
+  for klass,rows in pairs(klasses) do
+    for _,row in pairs(rows) do
+      n += 1
+      x = row[col.at]
+      if x != "?" then 
+        k = col:bins(x) 
+        out[k] = out[k] or BIN.new(col.at, col.txt, x)
+        out[k]:add(x,klass) end end end 
+  return map(out, function(x) return x end), n end
+
+function SYM:merges(x,_) return x end
+function NUM:merges(b4,enough,    j,now,a,b,ab)
+  j, now = 1, {}
+  while j <=  len(b4) do
+    a = b4[j]
+    if j <  len(b4) then
+      b = b4[j+1]
+      ab = a:mergable(b,enough) 
+      if ab then
+        a = ab
+        j = j+1 end end
+    now += [a]
+    j += 1 
+  end
+  if len(now) < len(b4) then return self:merges(now,enough) end
+  for j=2,len(row) do
+    now[j].lo    = now[j-1].hi
+    now[1].lo    = -1E30
+    now[#now].hi =  1E30 end
+  return now end
+
 --------------------------------------------------------
 -- ## Misc Functions
 
@@ -76,6 +145,8 @@ function l.is(x,y)
   x.__tostring=l.o; x.__index=x; setmetatable(y,x); return y end
 
 -- ### Lists
+function l.by(s) return function(a,b) return a[s] < b[s] end end
+
 function l.sort(t,fun) table.sort(t,fun); return t end
 
 function l.push(t,x) t[1+#t]=x; return x end
@@ -100,6 +171,12 @@ function l.cdf(x,mu,sigma,     z,fun)
   fun = function(z) return 1 - 0.5*2.718^(-0.717*z - 0.416*z*z) end
   z = (x - mu) / sigma
   return z >= 0 and fun(z) or 1 - fun(-z) end
+
+function l.rnd(n, ndecs)
+  if type(n) ~= "number" then return n end
+  if math.floor(n) == n  then return n end
+  local mult = 10^(ndecs or 2)
+  return math.floor(n * mult + 0.5) / mult end
 
 -- ### String to Thing
 function l.coerce(s,     _other) 
