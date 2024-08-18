277fadcd7829e198dff39b9a3a8e7d199959fd48
timm <timm@ieee.org>
2024-08-17 20:50:50 -0400

    as

---
+++ pandoc/lisp2html/a.lisp
@@ -0,0 +1,25 @@
+;vim: set filetype=lisp :
+;% Love is in the air
+;% Tim Menzies
+;% June 4, 2023
+
+"# adsaas
+
+## asd sa asd
+
+asdad asd as dasdlasd;asd asddasddasdlasdlasd asd asdalasd;asdas
+das asdas"
+
+    (defun aa (x) (format nil "~a" x))
+    
+    (defun fred (&rest aa) 
+      (dolist (i aa) (print i)))
+
+    (defstruct aa (b 1) (c c))
+
+"sadasd sa sad asd sadasd asasdas asd as as adas dasa
+asasassa  asdas asasa
+
+- asda _asdas_ freds
+- asdas
+- asda"
