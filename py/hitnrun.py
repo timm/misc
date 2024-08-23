0dc137efe2345cadd12431101c3fcc2791852360
timm <timm@ieee.org>
2024-08-23 04:03:53 -0400

    as

--- py/hitnrun.py
+++ py/hitnrun.py
@@ -1,43 +1,48 @@
 #!/usr/bin/env python3 -B
 # vim : set et sw=2 ts=2 :
 from fileinput import FileInput as file_or_stdin
-from dataclasses import dataclass, field, fields
-import datetime
+import re,sys,ast,math,random,inspect,datetime
 from math import exp,log,cos,sqrt,pi
-import re,sys,ast,math,random,inspect
 from time import time
 R=random.random
 
+#------------------------------------------------------------------------------
 class o:
   def __init__(i,**d): i.__dict__.update(d)
-  def __repr__(i)    : return str(i.__dict__)
+  def __repr__(i)    : return i.__class__.__name__ + show(i.__dict__)
 
-the = o(k=1, m=2,  train="../data/auto93.csv")
+the = o(k=1, m=2,  train="../data/auto93.csv",seed=123457891)
 
-@dataclass
+#------------------------------------------------------------------------------
 class COL(o): 
+  def __init__(i,at=0,txt=" "): 
+    i.at=0; i.txt=" "; i.n=0
+
   def adds(i,lst): [i.add(x) for x in lst]; return i
 
-@dataclass
+#------------------------------------------------------------------------------
 class SYM(COL): 
-  def __init__(i): i.at,i.txt, i.n,i.most,i.mode ==0, txt=" ", n=0, most=0; mode=None; has:DICT()
-  def coerce(i, x): return x 
+  def __init__(i,**d): 
+    super().__init__(**d)
+    i.most=0; i.mode=None; i.has={}
+
   def add(i,x):
     i.n += 1
     i.has[x] = i.has.get(x,0) + 1
     if i.has[x] > i.most: i.most,i.mode = i.has[x], x
 
+  def like(i, x, prior): return (i.has.get(x,0) + the.m*prior) / (i.n + the.m)
+
   def sub(i,x):
     i.n      -= 1
     i.has[x] -= 1
 
-@dataclass
+#------------------------------------------------------------------------------
 class NUM(COL): 
-  mu=0; m2=0; sd=0; lo=1E32; hi= -1E32; goal=1
-
-  def __post_init__(i) -> None: i.goal = 0 if i.txt and i.txt[-1] == "-" else 1
-
-  def coerce(i, x): return x if x == "?" else coerce(x) 
+  def __init__(i,**d): 
+    super().__init__(**d)
+    i.mu=0; i.m2=0; i.sd=0; i.lo=1E32; i.hi= -1E32
+    i.goal = 0 if i.txt and i.txt[-1] == "-" else 1
 
   def add(i,x):
     i.n  += 1 
@@ -48,6 +53,12 @@ class NUM(COL):
     if x  < i.lo: i.lo = x 
     i.sd  = 0 if i.n < 2 else (i.m2/(i.n - 1))**.5
 
+  def like(i, x, _):
+    v     = i.sd**2 + 1E-30
+    nom   = exp(-1*(x - i.mu)**2/(2*v)) + 1E-30
+    denom = (2*pi*v) **0.5
+    return min(1, nom/(denom + 1E-30))
+
   def sub(i,x):
     i.n  -= 1
     d     = x - i.mu
@@ -55,22 +66,13 @@ class NUM(COL):
     i.m2 -= d*(x - i.mu) 
     i.sd  = 0 if i.x < 2 else (i.m2/(i.n - 1))**.5
 
-  def like(i, x, prior): return (i.has.get(x,0) + the.m*prior) / (i.n + the.m)
-
-  def like(i, x, _):
-    v     = i.sd**2 + 1E-30
-    nom   = exp(-1*(x - i.mu)**2/(2*v)) + 1E-30
-    denom = (2*pi*v) **0.5
-    return min(1, nom/(denom + 1E-30))
-
-@dataclass
+#------------------------------------------------------------------------------
 class COLS:
-  names: LIST(); all:LIST(); x:LIST(); y:LIST(); klass= None
-
-  def __post_init__(i):
-    for at,txt in enumerate(i.names):
+  def __init__(i,names):
+    i.names, i.x, i.y, i.all = names,[],[],[]
+    for at,txt in enumerate(names):
       a,z = txt[0],txt[-1]
-      col = (NUM if a.isupper() else SYM)(at, txt)
+      col = (NUM if a.isupper() else SYM)(at=at, txt=txt)
       i.all.append(col)
       if z != "X":
         (i.y if z in "!+-" else i.x).append(col)
@@ -81,25 +83,26 @@ class COLS:
     [col.add(row[col.at]) for cols in [i.x, i.y] for col in cols if row[col.at] != "?"]
     return row
 
-@dataclass
+#------------------------------------------------------------------------------
 class DATA:
-  cols:LIST(); rows:LIST()
+  def __init__(i): i.rows=[]; i.cols=None
 
   def clone(i,rows):
     return DATA().add(i.cols.names).adds(rows)
 
   def add(i,row):
     if     i.cols: i.rows += [i.cols.add(row)]
-    else:  i.cols = COLS(row,[],[],[]) 
-    return i
+    else:  i.cols = COLS(row)
 
-  def reads(i,fname):
+  def csv(i,fname):
    infile = sys.stdin if fname=="-" else open(fname)
    with infile as src:
      for line in src:
       line = re.sub(r'([\n\t\r ]|#.*)', '', line)
       if line: 
         i.add([coerce(s) for s in line.split(",")])
+   random.shuffle(i.rows)
+   return i
 
   def adds(i,rows): [i.add(row) for row in rows]; return i
 
@@ -108,6 +111,7 @@ class DATA:
     likes = [c.like(r[c.at], prior) for c in i.cols.x if r[c.at] != "?"]
     return sum(log(x) for x in likes + [prior] if x>0)
 
+#------------------------------------------------------------------------------
 def coerce(x):
   try             : return ast.literal_eval(x)
   except Exception: return x
@@ -119,6 +123,13 @@ def normal(mu,sd):
    w = x1*x1 + x2*x2
    if w < 1: return mu + sd * x1 * sqrt((-2*log(w))/w)
 
+def show(x):
+  if isinstance(x,float): return f"{x:g}"
+  if not isinstance(x,dict): return str(x)
+  return "{"+' '.join([f":{k} {show(x[k])}" for k in x if k[0] != "_"])+"}"
+
+
+#------------------------------------------------------------------------------
 class eg:
   def noop():  pass
   def one(): print(1)
@@ -126,6 +137,8 @@ class eg:
     n = NUM().adds([normal(10,1) for x in range(100)]); print(f"{n.mu:g} {n.sd:g}")
 
   def data():
-    d=DATA([],[]).reads(the.train)
+    d=DATA().csv(the.train)
+    print(d.cols.y[0])
 
+random.seed(the.seed)
 [getattr(eg, s[1:], eg.noop)() for s in sys.argv]
