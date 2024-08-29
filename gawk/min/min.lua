7ce9c4a7bbded1a2fd5d13710b51c58fd1b42cb8
timm <timm@ieee.org>
2024-08-29 08:20:15 -0400

    min

--- gawk/min/min.lua
+++ gawk/min/min.lua
@@ -20,8 +20,11 @@ local function gt(key)     return function(a,b) return a[key] > b[key] end end
 local function up(fun)     return function(a,b) return fun(a) > fun(b) end end
 local function down(fun)   return function(a,b) return fun(a) < fun(b) end end
 local function sort(t,fun) table.sort(t,fun); return t end
-
 local function push(t,x)   t[1+#t]=x; return x end
+
+local function keys(t,    u) 
+  u={}; for k,_ in pairs(t) do push(u,k) end
+  return sort(u) end   
 
 local function shuffle(t,    j)
   for i = #t, 2, -1 do j = math.random(i); t[i], t[j] = t[j], t[i] end
@@ -36,22 +39,24 @@ local function csv(file,fun,      src,s,cells,n)
   function cells(s,    t)
     t={}; for s1 in s:gmatch"([^,]+)" do t[1+#t] = coerce(s1) end; return t end
   src = io.input(file)
-  n   = -
+  n   = -1
   while true do
     s = io.read()
     if s then n=n+1; fun(n,cells(s)) else return io.close(src) end end end
 
-local function o(x,    u)
-  if type(x) == "number" then return string.format("%.3g",x) end
+local fmt = string.format
+
+local function _table2string(x,t)
+  if   #x > 0 
+  then for k,v in pairs(x) do t[1+#t] = o(v) end
+  else for _,k in keys(x)  do 
+         if not o(k):find"^_" then t[1+#t] = fmt(":%s %s", k, o(x[k])) end end end
+  return t end
+
+local function o(x,    t)
+  if type(x) == "number" then return fmt("%.3g",x) end
   if type(x) ~= "table"  then return tostring(x) end
-  u={}
-  if #x > 0 then
-    for k,v in pairs(x) do u[1+#u] = o(v) end
-  else 
-    for k,v in pairs(x) do
-      if o(k):sub(1,1) ~= "_" then
-        u[1+#u] = string.format(":%s %s", k, o(v)) end end end
-  return "{" .. table.concat(#x==0 and sort(u) or u," ") .. "}" end
+  return "{" .. table.concat( _table2string(x,{}), " ") .. "}" end
 
 local function oo(x) print(o(x)) end
 
@@ -70,7 +75,7 @@ function NUM:new(i,is)
   return new(NUM, {n=0, i=i, is=is, mu=0, sd=0, m2=0, lo=big, hi=-big,
                    goal = is:find"-$" and 0 or 1}) end
 
-function COLS:new(names,     all,x,y)
+function COLS:new(names,     all,x,y,col)
   all,x,y = {},{},{}
   for i,is in pairs(names) do
     col = push(all, (is:find"^[A-Z]" and NUM or SYM):new(i,is))
@@ -98,7 +103,7 @@ function DATA:from(rows)
 function NUM:norm(x)
   return x=="?" and x or (x - self.lo) / (self.hi - self.lo + 1/big) end
 
-- ----------------------------------------------------------------------------------
+-- ----------------------------------------------------------------------------------
 function DATA:add(row) 
   if   self.cols 
   then push(self.rows, self.cols:add(row))
@@ -129,7 +134,7 @@ function SYM:add(x,  n)
       self.most, self.mode = self.has[x], x end end end 
 
 -- ----------------------------------------------------------------------------------
-function DATA:chebyshev(row)
+function DATA:chebyshev(row,    tmp,d)
   d=0; for _,col in pairs(self.cols.y) do
          tmp = math.abs(col.goal - col:norm(row[col.i]))
          if tmp > d then d = tmp end end
@@ -141,7 +146,7 @@ function DATA:shuffle()
 
 function DATA:sort(    fun)
   fun = function(row) return self:chebyshev(row) end
-  self.rows = sorted(self.rows, function(a,b) return fun(a) < fun(b) end)
+  self.rows = sort(self.rows, function(a,b) return fun(a) < fun(b) end)
   return self end
 
 function DATA:bestRest(      best,rest)
@@ -160,39 +165,34 @@ function NUM:like(x,...)
   if sd==0 then return x==mu and 1 or 1E-32 end
   return math.exp(-.5*((x - mu)/sd)^2) / (sd*((2*math.pi)^0.5)) end
 
-function DATA:like(rows, n, nClasses,     col,prior,out,v,inc)
+function DATA:like(row, n, nClasses,     col,prior,out,v,inc)
   prior = (#self.rows + the.k) / (n + the.k * nClasses)
   out   = math.log(prior)
-  for _,col in pairs(cols or self.cols.x) do
+  for _,col in pairs(self.cols.x) do
     v = row[col.i]
     if v ~= "?" then
       inc = col:like(v,prior)
       if inc > 0 then out = out + math.log(inc) end end end
   return out end
 
-function DATA:acquire(rows, score,      todo,done,guess,loop)
-  rows  = rows or self.rows
-  score = score or function(B,R) return B - R end
-  
-  function guess(todo,best,rest,n,     t,u,fun,best,rest)
-    fun = function(row) return score(best:like(row, n, 2), 
-                                     rest:like(row, n, 2)) end
-    t,u = {},{}
-    for i,row in pairs(todo) do push(t, {i <= the.cut and fun(row) or 0, row}) end
-    for _,xy in pairs(sort(t, lt(1))) do push(u,xy[2]) end
-    return u end
-
-  todo, done = {}, {}
-  for i,row in pairs(rows) do push(i <= the.begin and done or todo, row) end
-
+function DATA:acquire(rows, score,      todo,done)
+  todo, done = {},{}
+  for i,row in pairs(rows or self.rows) do 
+    push(i <= the.begin and done or todo, row) end
   while #done < the.Break do
-    local best,rest = self:clone(done):bestRest()
-		print(#best.rows, #rest.rows)
-    todo = guess(todo, best, rest, #done)
-    push(done, pop(todo))
-    done = self:clone(done):sort().rows end
-
+    todo = self:rank(todo, done, score or function(B,R) return B - R end) 
+    push(done, pop(todo)) end
+    done = self:clone(done):sort().rows 
   return done[#done] end
+
+function DATA:rank(todo,done,score,     best,rest,fun,tmp)
+  best,rest = self:clone(done):bestRest()
+  fun = function(row) return score(best:like(row,#done,2),rest:like(row,#done,2)) end
+  tmp = {}
+  for i,row in pairs(todo) do push(tmp, {row, i <= the.cut and fun(row) or -big}) end
+  todo = {}
+  for _,one in pairs(sort(tmp, lt(1))) do push(todo, one[2]) end
+  return todo end
 
 -- ---------------------------------------------------------------------------------
 local eg = {}
@@ -219,7 +219,7 @@ function eg.bayes(_,      d,fun)
   for n,row in pairs(sort(d.rows, down(fun))) do
    if n % 30 == 0 then print(n, fun(row)) end end end
 
-function eg.cheb(_,      d,nu)
+function eg.cheb(_,      d,num)
   d   = DATA:new():csv(the.train) 
   num = NUM:new()
   for _,row in pairs(d.rows) do num:add(d:chebyshev(row)) end
