cfeba5b5198b71ba58785209e644d3480f004edf
timm <timm@ieee.org>
2024-08-29 21:49:36 -0400

    saving

--- gawk/min/min.lua
+++ gawk/min/min.lua
@@ -1,41 +1,62 @@
 #!/usr/bin/env lua
-local big = 1E32
+local the,help = {},[[
+min.lua : multiple-objective active learning
+(c) 2024, Tim Menzies <timm@ieee.org>, BSD-2.
 
-local the = {
-  begin = 4,
-  Break = 30,
-  cut   = 100,
-  k     = 1,
-  m     = 2,
-  seed  = 1234567891,
-  train = "../../../moot/optimize/misc/auto93.csv",
-  Top   = .5
-}
- 
--- ----------------------------------------------------------------------------------
-local pop = table.remove
+USAGE:
+  lua min.lua [OPTIONS] [ARGS]
 
-local function lt(key)     return function(a,b) return a[key] < b[key] end end
-local function gt(key)     return function(a,b) return a[key] > b[key] end end
-local function up(fun)     return function(a,b) return fun(a) > fun(b) end end
-local function down(fun)   return function(a,b) return fun(a) < fun(b) end end
-local function sort(t,fun) table.sort(t,fun); return t end
-local function push(t,x)   t[1+#t]=x; return x end
+OPTIONS:
+  -b --begin  int   initial samples = 4
+  -B --Break  int   max samples     = 30
+  -c --cut    int   items to sort   = 100
+  -k --k      int   Bayes param     = 1
+  -m --m      int   Bayes param     = 2
+  -s --seed   int   random seed     = 1234567891
+  -t --train  str   csv file        = ../../../moot/optimize/misc/auto93.csv
+  -T --Top    float best set size   = .5]]
 
-local function keys(t,    u) 
-  u={}; for k,_ in pairs(t) do push(u,k) end
-  return sort(u) end   
+--  |  o  |_  
+--  |  |  |_) 
 
-local function shuffle(t,    j)
+local big,coerce,csv,down,fmt,gt,keys,lt,new,o,oo,pop,push,shuffle,sort,trim,up
+
+-- shortcuts
+big = 1E32
+pop = table.remove
+
+-- meta
+function new(klass,obj)
+  klass.__index=klass; klass.__tostring=o; return setmetatable(obj,klass) end
+
+-- sorting
+function lt(key)     return function(a,b) return a[key] < b[key] end end
+function gt(key)     return function(a,b) return a[key] > b[key] end end
+function up(fun)     return function(a,b) return fun(a) > fun(b) end end
+function down(fun)   return function(a,b) return fun(a) < fun(b) end end
+
+function sort(t,fun) table.sort(t,fun); return t end
+function keys(t,    u) 
+  u={}; for k,_ in pairs(t) do push(u,k) end return sort(u) end   
+
+-- strings
+function trim(s)  return s:match"^%s*(.-)%s*$" end
+
+-- lists
+function push(t,x)   t[1+#t]=x; return x end
+
+function shuffle(t,    j)
   for i = #t, 2, -1 do j = math.random(i); t[i], t[j] = t[j], t[i] end
   return t end
 
-local function trim(s)  return s:match"^%s*(.-)%s*$" end
-local function coerce1(s) return s=="true" and true or s ~= "false" and s end
-local function coerce(s)   
-  return math.tointeger(s) or tonumber(s) or coerce1(trim(s)) end
+-- string to things
+function coerce(s,     fun)   
+  fun = function(s) return s=="true" and true or s ~= "false" and s end
+  return math.tointeger(s) or tonumber(s) or fun(trim(s)) end
 
-local function csv(file,fun,      src,s,cells,n)
+help:gsub("[-][-]([%S]+)[^=]+=[%s]+([%S]+)", function(k,v) the[k] = coerce(v) end)
+
+function csv(file,fun,      src,s,cells,n)
   function cells(s,    t)
     t={}; for s1 in s:gmatch"([^,]+)" do t[1+#t] = coerce(s1) end; return t end
   src = io.input(file)
@@ -44,26 +65,24 @@ local function csv(file,fun,      src,s,cells,n)
     s = io.read()
     if s then n=n+1; fun(n,cells(s)) else return io.close(src) end end end
 
-local fmt = string.format
+-- thing to string
+fmt = string.format
 
-local function _table2string(x,t)
-  if   #x > 0 
-  then for k,v in pairs(x) do t[1+#t] = o(v) end
-  else for _,k in keys(x)  do 
-         if not o(k):find"^_" then t[1+#t] = fmt(":%s %s", k, o(x[k])) end end end
-  return t end
+function oo(x) print(o(x)) end
 
-local function o(x,    t)
+function o(x,     list,hash)
+  list= function(t) for _,v in pairs(x) do t[1+#t] = o(v) end; return t end
+  hash= function(t) for _,k in pairs(keys(x)) do 
+                      if   not o(k):find"^_" 
+                      then t[1+#t] = fmt(":%s %s", k, o(x[k])) end end 
+                    return t end
   if type(x) == "number" then return fmt("%.3g",x) end
-  if type(x) ~= "table"  then return tostring(x) end
-  return "{" .. table.concat( _table2string(x,{}), " ") .. "}" end
+  if type(x) ~= "table"  then return tostring(x)   end
+  return "{" .. table.concat(#x>0 and list{} or hash{}, " ") .. "}" end
 
-local function oo(x) print(o(x)) end
+--   _  ._   _    _.  _|_   _  
+--  (_  |   (/_  (_|   |_  (/_ 
 
-local function new(klass,obj)
-  klass.__index=klass; klass.__tostring=o; return setmetatable(obj,klass) end
-
--- ----------------------------------------------------------------------------------
 local NUM,SYM,COLS,DATA = {},{},{},{}
 
 function SYM:new(i,is)
@@ -95,15 +114,18 @@ function DATA:csv(file)
 
 function DATA:from(rows)
   for row in pairs(rows or {}) do self:add(row) end
-  return self end
-
--- ----------------------------------------------------------------------------------
--- Misc methods
+  return self end
+--   _.        _   ._     
+--  (_|  |_|  (/_  |   \/ 
+--    |                /  
 
 function NUM:norm(x)
   return x=="?" and x or (x - self.lo) / (self.hi - self.lo + 1/big) end
 
--- ----------------------------------------------------------------------------------
+--       ._    _|   _.  _|_   _  
+--  |_|  |_)  (_|  (_|   |_  (/_ 
+--       |                       
+
 function DATA:add(row) 
   if   self.cols 
   then push(self.rows, self.cols:add(row))
@@ -133,7 +155,9 @@ function SYM:add(x,  n)
     if self.has[x] > self.most then
       self.most, self.mode = self.has[x], x end end end 
 
--- ----------------------------------------------------------------------------------
+--   _|  o   _  _|_ 
+--  (_|  |  _>   |_ 
+
 function DATA:chebyshev(row,    tmp,d)
   d=0; for _,col in pairs(self.cols.y) do
          tmp = math.abs(col.goal - col:norm(row[col.i]))
@@ -156,7 +180,10 @@ function DATA:bestRest(      best,rest)
     (i <= (#self.rows)^the.Top and best or rest):add(row) end
   return best,rest end
 
--- ----------------------------------------------------------------------------------
+--  |_    _.       _    _ 
+--  |_)  (_|  \/  (/_  _> 
+--            /           
+
 function SYM:like(x, prior)
   return ((self.has[x] or 0) + the.m*prior)/(self.n +the.m) end
 
@@ -194,7 +221,10 @@ function DATA:guessNextBest(todo,done,score,     best,rest,fun,tmp)
   for _,one in pairs(sort(tmp, lt(1))) do push(todo, one[2]) end
   return todo end
 
--- ---------------------------------------------------------------------------------
+--   _    _  
+--  (/_  (_| 
+--        _| 
+
 local eg = {}
 
 function eg.train(x) the.train = x end
@@ -232,10 +262,16 @@ function eg.acq(_,      d,num)
 	  num:add( d:chebyshev(d:shuffle():acquire()) ) end
 	print(num.mu) end
 
-function eg.push(_) os.execute("git commit -am saving; git push; git status") end
+function eg.push(_, s) 
+  s = "git commit -am saving; git push; git status" 
+  print(s)
+  os.execute(s) end
+
 function eg.the(_) oo(the) end
 
--- ----------------------------------------------------------------------------------
+--   _  _|_   _.  ._  _|_ 
+--  _>   |_  (_|  |    |_ 
+
 math.randomseed(the.seed)
 
 for i,s in pairs(arg) do 
