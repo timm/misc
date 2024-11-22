3ae178452fc82ad85e385cdaa06eb8d15f5fd5d7
timm <timm@ieee.org>
2024-11-22 16:17:53 -0500

    as

--- py/zap.py
+++ py/zap.py
@@ -1,5 +1,7 @@
+#!/usr/bin/env python3.13 -B
+
 """
-tiny.py : fast active learning
+zap.py : fast active learning
 (c) 2024 Tim Menzies <timm@ieee.org> MIT license
 
 USAGE:
@@ -14,8 +16,8 @@ OPTIONS:
 def eg_help(_): print("\n" + __doc__)
 
 # -----------------------------------------------------------------------------
-import re,ast,sys
-from math import log,exp,pi
+import random,re,ast,sys
+from math import sqrt,log,exp,pi
 from typing import Iterable
 
 BIG=1E32
@@ -44,7 +46,13 @@ Col       = Num | Sym
 
 # -----------------------------------------------------------------------------
 def show(d):
-  return '('+' '.join(f":{k} {v}" for k,v in d.items() if k[0] !_ "_")+')'
+  if type(d)==str        : return d
+  if type(d)==type(show) : return  d.__name__+'()'
+  if type(d)==dict:
+    return '('+' '.join(f":{k} {show(v)}" for k,v in d.items() if str(k)[0] !="_")+')'
+  if d==d//1           : return str(d//1)
+  if type(d)==float    : return f"{d:g}"
+  return str(d)
 
 def coerce(s):
   try: return ast.literal_eval(s)
@@ -57,11 +65,11 @@ def csv(f):
 
 # -----------------------------------------------------------------------------
 def Num(txt=" ",at=0) -> Num: 
-  return Obj(this=Num, at=at, txt=txt, n=0, mu=0, m2=0, lo=BIG, hi= -BIG)
+  return Obj(this=Num, at=at, txt=txt, n=0, mu=0, m2=0, lo=BIG, hi= -BIG,
+             goal= 0 if txt[-1]=="-" else 1)
 
 def Sym(txt=" ",at=0) -> Sym: 
-  return Obj(this=Sym, at=at, txt=txt, n=0, has={}, 
-             most=0, mode=None, goal= 0 if txt[-1]=="-" else 1)
+  return Obj(this=Sym, at=at, txt=txt, n=0, has={}) 
 
 def Data(src: Iterable[row]) -> Data:
   i = Obj(rows=[], cols = None)
@@ -91,27 +99,32 @@ def addCols(i:Data,row:row, n:toggle=1):
   return row
 
 def addCol(i: Col, x:atom, n:toggle=1):
+  print(i)
   i.n += n
   if i.this is Sym:
-    now = i.has[x] = i.has.get(x,0) + n
-    if now > i.most: i.most,i.mode= now, x
+   i.has[x] = i.has.get(x,0) + n
   else:
     if x < i.lo: i.lo = x
     if x > i.hi: i.hi = x
-    d = n*(x - i.mu)
-    i.mu += d / i.n
-    i.m2 += d * (x - i.mu)
+    d     = x - i.mu
+    i.mu += n*(d / i.n)
+    i.m2 += n*(d * (x - i.mu))
+  return i
 
-# some shortcuts
+# shortcuts and demos
 add = addCol
 
-def adds(i: Col,lst, n:toggle): [addCol(i,x,n) for x in lst]; return i
+def adds(i: Col,lst, n:toggle=1): [addCol(i,x,n) for x in lst]; return i
 
-def eg_load(f: str): d= Data(csv(f)); print(d.cols.x)
+def eg_load(f: str): d= Data(csv(f)); [print(col) for col in d.cols.x]
 
+def eg_addSub(_):
+  num, at = Num(), {x:add(num,x).mu for x in range(20)}
+  for x in sorted(at.keys(), reverse=True)[:18] :
+    print(x,add(num,x,-1).mu,at[x-1])
 # -----------------------------------------------------------------------------
 def mid(i:Col):
-  return i.mode if i.this is Sym else i.mu
+  return max(i.has, key=i.get) if i.this is Sym else i.mu
 
 def div(i:Col):
   if i.this is Sym: 
@@ -123,7 +136,7 @@ def mids(i:Data): return {col.txt:mid(col) for col in i.cols.all}
 def divs(i:Data): return {col.txt:div(col) for col in i.cols.all}
 
 def ydist(i:Data,row):
-  return sum(abs(norm(col,row[i.at]) - col.goal)**2 for col in i.cols.y)
+  return sqrt(sum(abs(norm(col,row[col.at]) - col.goal)**2 for col in i.cols.y))
 
 def norm(i:Num,x:num) -> num: # 0..1
   return x if x=="?" else (x - i.lo) / (i.hi - i.lo + 1/BIG)
@@ -131,16 +144,21 @@ def norm(i:Num,x:num) -> num: # 0..1
 # -----------------------------------------------------------------------------
 def like(i,x,prior):
   if i.this is Sym:
-    return (i.has.get(x,0) + the.bayes.m*prior) / (i.n + the.bayes.m)
+    return (i.has.get(x,0) + the.m*prior) / (i.n + the.m)
   else:
     v   = div(i)**2 + 1/BIG
     tmp = exp(-1*(x - mid(i))**2/(2*v)) / (2*pi*v) ** 0.5
     return max(0,min(1, tmp + 1/BIG)) 
 
 def likes(i,row, nall,nh):
-  prior = (len(i.rows) + the.bayes.k) / (nall + the.bayes.k * nh)
+  prior = (len(i.rows) + the.k) / (nall + the.k * nh)
   tmp   = [like(col, row[col.at], prior) for col in i.cols.x]
   return log(prior) + sum(log(x) for x in tmp if x > 0)
+
+def eg_like(f):
+   d= Data(csv(f))
+   for row in sorted(d.rows, key=lambda r: likes(d,r,1000,2))[::30]:
+       print(likes(d,row,1000,2))
 
 def bestish(row,best,rest):
   nall= len(best.rows) + len(rest.rows)
@@ -148,7 +166,7 @@ def bestish(row,best,rest):
   r= likes(rest,row, nall, 2)
   return b - r
 
-def lurch(i,     done,maybe,yes,Y,N):
+def zappy(i):
   lives,maybe,yes,done = 5,0,0, clone(i, i.rows[:the.top])
   Y = lambda row: ydist(done,row)
   N = lambda : int(sqrt(len(done.rows)))
@@ -164,7 +182,8 @@ def lurch(i,     done,maybe,yes,Y,N):
         adds(best, row)
         best.rows.sort(key=Y)
         best.rows = tmp[:N()]
-        [addData(rest, adds(best, row, -1)) for row in tmp[N():]]
+        print(tmp[N()])
+        [addData(rest, addCols(best, row, -1)) for row in tmp[N():]]
         continue
     lives -= 1
     adds(rest, row)
@@ -173,8 +192,11 @@ def lurch(i,     done,maybe,yes,Y,N):
   rest.rows.sort(key=br)
   return best,rest
 
+def eg_zap(f):
+   zappy(Data(csv(f)))
+
 # -----------------------------------------------------------------------------
 if __name__== "__main__":
   for j,s in enumerate(sys.argv):
     if todo := vars().get(re.sub("^--","eg_",s), None):
-      todo(sys.argv[j+1] if j < len(sys.argv) - 1 else "")
+      todo(sys.argv[j+1] if j < len(sys.argv) - 1 else None)
