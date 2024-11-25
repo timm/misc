49cbab296c412deb957cc7eeb3755002809d178c
timm <timm@ieee.org>
2024-11-25 09:22:50 -0500

    as

--- gawk/b0/nb.lua
+++ gawk/b0/nb.lua
@@ -1,9 +1,12 @@
+#!/usr/bin/env lua
 -- vim: set tabstop=2 shiftwidth=2 expandtab :
 
 local b4={}; for k,_ in pairs(_ENV) do b4[k]=k end
+local function rogues()
+  for k,v in pairs(_ENV) do if not b4[k] then print("?", k, type(v)) end end end
 
-local l,eg,the = {},{},{k=1,m=2,big=1E32}
-local sum,coerce,csv,o,new 
+local the,eg = {k=1,m=2,seed=1234567891, big=1E32},{}
+local sum,coerce,csv,o,new,push,shuffle
 local exp,log,min,max,pi = math.exp, math.log, math.min, math.max, math.pi
 
 -------------------------------------------------------------------------------
@@ -17,25 +20,20 @@ function Num:new(txt,at)
 function Sym:new(txt,at) 
   return new(Sym, {at=at or 0, txt=txt or "", n=0, has={},most=0,mode=nil}) end
 
-function Cols:new(names,    all,x,y,klass,col)
-  all,x,y = {},{},{}
-  for at,s in pairs(names) do
-    col = (s:find"^[A-Z]" and Num or Sym):new(s,at)
-    all[1+#all] = col
-    if s:find"!$" then klass = col end
-    if s:find"[!+-]$" then y[1+#y] = col else x[1+#x] = col end end
-  return new(Cols, {all=all, y=y, x=x, klass=klass}) end
+function Cols:new()
+  return new(Cols, {all={}, y={}, x={}, klass=nil,initialized=false}) end
 
 function Data:new() 
-  return new(Data, {rows={}, cols=nil}) end
+  return new(Data, {rows={}, cols=Cols:new()}) end
+
+function eg.data(f)
+  for _,col in pairs(Data:new():adds(f).cols.all) do 
+    print(o(col)) end end
 
 function eg.cols(_,    names)
   names={"cylinders", "Displacement", "HorsepowerX", 
          "Weight-", "Acceleration+", "Model", "origin", "Mpg+"}
-  for _,col in pairs(Cols:new(names).all) do print(o(col)) end end
-
-function eg.data(f)
-  for _,col in pairs(Data:new():load(f).cols.all) do print(o(col)) end end
+  for _,col in pairs(Cols:new():initialize(names).all) do print(o(col)) end end
 
 -------------------------------------------------------------------------------
 function Sym:add(x)
@@ -52,22 +50,32 @@ function Num:add(x,    d)
   self.m2 = self.m2 + d*(x - self.mu)
   self.sd = self.n < 2 and 0 or (self.m2 / (self.n - 1))^0.5 end 
 
-function Data:load(src)
-  if type(src) == "string" 
-  then for   row in l.csv(src)       do self:add(row) end 
-  else for _,row in pairs(src or {}) do self:add(row) end end
+function Data:adds(src,      FILE,LIST)
+  if     src == nil or tyoe(src) == "string" 
+  then   for   row in csv(src) do self:add(row) end 
+  else   for _,row in pairs(t) do self:add(row) end end
   return self end
     
 function Data:add(row)
-  if   self.cols 
-  then self.rows[1+#self.rows] = self.cols:add(row)
-  else self.cols = Cols:new(row) end end
+  if   self.cols.initialized
+  then push(self.rows, self.cols:add(row))
+  else self.cols:initialize(row) end end
+
+function Data:klass(row) return row[self.cols.klass.at] end
 
 function Cols:add(row)
   for at,x in pairs(row) do
     if x ~="?" then
       self.all[at]:add(x) end end
   return row end 
+
+function Cols:initialize(names,    col)
+  for at,s in pairs(names) do
+    col = push(self.all, (s:find"^[A-Z]" and Num or Sym):new(s,at))
+    if s:find"!$" then self.klass = col end
+    push(s:find"[!+-]$" and self.y or self.x, col) end 
+  self.initialized = true 
+  return self end
 
 -------------------------------------------------------------------------------
 function Sym:like(x,prior,    v,tmp) 
@@ -81,16 +89,24 @@ function Num:like(x,_,     v,tmp)
     return max(0,min(1, tmp + 1/the.big)) end end
 
 function Data:like(row,  nall,nh,    prior,LIKE,LOG) 
-  prior = (#self.rows + the.k) / (nall + the.k*nh)
-  LIKE  = function(col) return LOG( col:like(row[col.at], prior) ) end
-  LOG   = function(x)   return x and x>0 and log(x) or 0 end
+  LIKE = function(col) return LOG( col:like(row[col.at], prior) ) end
+  LOG  = function(x)   return x and x>0 and log(x) or 0 end
+  prior= (#self.rows + the.k) / (nall + the.k*nh)
   return LOG(prior) + sum(self.cols.x, LIKE) end
 
 function eg.like(f,    d)
-  d=Data:new():load(f)
-  for _,row in pairs(d.rows) do print(d:like(row,1000,2)) end end
+  d=Data:new():adds(f)
+  for k,row in pairs(shuffle(d.rows)) do 
+    if k>20 then break else print(row[#row],d:like(row,1000,2)) end end end
 
 -------------------------------------------------------------------------------
+local l={}
+
+function l.push(t,x) t[1+#t]=x; return x end
+
+function l.shuffle(t,    k) 
+  for j = #t,2,-1 do k=math.random(j); t[j],t[k] = t[k],t[j] end; return t end
+
 function l.sum(t,f,   n)
   n=0; for _,x in pairs(t) do n=n+f(x) end; return n end
 
@@ -100,7 +116,7 @@ function l.coerce(s,     FUN,TRIM)
   return math.tointeger(s) or tonumber(s) or FUN(TRIM(s)) end
 
 function l.csv(file,     src) 
-  if file and file ~="-" then src=io.input(file) end
+  if file ~="-" then src=io.input(file) end
   return function(     s,t)
     s = io.read()
     if s 
@@ -121,16 +137,19 @@ function l.o(x,        t,FMT,NUM,LIST,DICT)
 function l.new(kl,t)
   kl.__index=kl; kl.__tostring = l.o; return setmetatable(t,kl) end
 
-sum,coerce,csv,o,new = l.sum,l.coerce,l.csv,l.o,l.new
-
 function eg.the(_) print(o(the)) end
 function eg.csv(f) for row in csv(f) do print(o(row)) end end
 
 -------------------------------------------------------------------------------
-if arg[0] == "nb.lua" then 
-  for j,s in pairs(arg) do
-    s=s:gsub("[-][-]","")
-    if eg[s] then  eg[s](arg[j+1]) end end 
-  for k,v in pairs(_ENV) do if not b4[k] then print("?", k, type(v)) end end end
+sum,coerce,csv,o,new,push = l.sum,l.coerce,l.csv,l.o,l.new,l.push
+shuffle = l.shuffle
 
-return {Num=Num, Sym=Sym, Data=Data, the=the, lib=l}
+math.randomseed(the.seed)
+
+if arg[0]:find"nb.lua" 
+then for j,s in pairs(arg) do
+       s=s:gsub("^[-][-]","")
+       if eg[s] then  eg[s](arg[j+1]) end 
+       math.randomseed(the.seed) end
+     rogues() 
+else return {Num=Num, Sym=Sym, Data=Data, the=the, lib=l} end
---
+++ gawk/b0/notes.md
@@ -0,0 +1,12 @@
+manage your namespace, bunc together related functions into a few tables,
+check for rogue locals.
+
+to work bash pipes in, out of the box,. lua reads fro stdin.
+so this is easy
+
+cat ../../data/auto93.csv| ./nb.lua --csv
+
+ to work pipls in oo. retrunr self a lot
+vscode a static code analyzer of vscode
+
+the __name__== "__main__" trick in lua
