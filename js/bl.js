9a9d68a5f900d5ec46ff8b1b95b37950ad01031d
timm <timm@ieee.org>
2025-04-23 03:50:26 -0400

    as

--- js/bl.js
+++ js/bl.js
@@ -1,10 +1,10 @@
-const fs  = require("fs");
-const min = Math.min, max = Math.max, sqrt = Math.sqrt, abs = Math.abs
-const log = Math.log, exp = Math.exp, PI = Math.PI;
-const o   = (x, y = {}) => { for (let k in y) x[k] = y[k]; return x; };
-const out = console.log
+fs  = require("fs");
+min = Math.min, max = Math.max, sqrt = Math.sqrt, abs = Math.abs
+log = Math.log, exp = Math.exp, PI = Math.PI;
+o   = (x, y = {}) => { for (let k in y) x[k] = y[k]; return x; };
+out = console.log
 
-const the = { 
+the = { 
   Few:   50, 
   Stop:  32, 
   acq:   "xploit", 
@@ -21,12 +21,12 @@ const the = {
   rseed: 123456789, 
   start: 4 };
 
-const Num = (txt = " ", at = 0) => o({
+Num = (txt = " ", at = 0) => o({
   it: "Num", txt: txt, at: at, n: 0, 
   mu: 0, sd: 0, m2: 0, lo: 1e30, hi: -1e30, rank: 0,
   goal: txt.at(-1) === "-" ? 0 : 1 });
 
-const Sym = (txt = " ", at = 0) => o({
+Sym = (txt = " ", at = 0) => o({
   it: "Sym", txt: txt, at: at, n: 0, 
   has: {} });
 
@@ -40,5 +40,5 @@ const Cols = names => names.reduce((cols, s, n) => {
   return cols;
 }, o({ it: "Cols", names, all: [], x: [], y: [], klass: -1 }));
 
-out({"adas","asdas",23})
+out(Cols(["adas","asdas","Age-"]).all)
 out(Num("adssa",23))
