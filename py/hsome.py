91b33441759b3e20c53c22a69f407e1990596755
timm <timm@ieee.org>
2025-05-04 21:35:43 -0400

    as

--- py/hsome.py
+++ py/hsome.py
@@ -5,22 +5,27 @@ class o:
   __init__ = lambda i,**d: i.__dict__.update(**d)
   __repr__ = lambda i: i.__class__.__name__ + str(i.__dict__)
 
+the = o(p=2, file="data.csv", some=128)
+
+#------------------------------------------------------------------------------
 def col(txt=' ',at=0): return (Num if txt[0].isupper() else Sym)(txt,at)
 
 class Col(o):
   def __init__(i,txt=' ',at=0): 
     i.txt, i.at, i.n, i.w, i.goal = txt, at, 0, 0, txt[-1] in "!+-"
-  def sub(i,x,n=1): 
-    return i.add(x, n=n, flip=-1)
+    
+  def sub(i,x,n=1): return i.add(x, n=n, flip=-1)
 
+#------------------------------------------------------------------------------
 class Num(Col):
   def __init__(i,*_):
     super().__init__(*_); 
     i.lo, i.hi, i.mu, i.m2 = 1e32, -1e32, 0, 0 
     i.goal = 0 if txt[-1]=="-" else 1
-  def add(i,x, n=1,flip=1): 
+    
+  def add(i, x, n=1, flip=1): 
     if x!="?": 
-     i.n+= flip*n
+     i.n += flip*n
      i.lo = min(i.lo,x); i.hi=max(i.hi,x)
      if flip < 0 and i.n < 2:
        i.mu = i.sd = 0
@@ -28,16 +33,17 @@ class Num(Col):
        d = x-i.mu
        i.mu += flip*d/i.n
        i.m2 += flip*d*(x - i.mu)
-  def norm(i,x): 
-    return x if x=="?" else (x-i.lo)/(i.hi-i.lo+1e-32)
+       
   def dist(i,a,b):
     a,b = i.norm(a), i.norm(b)
     a = a if a!="?" else (1 if b<.5 else 0)
     b = b if b!="?" else (1 if a<.5 else 0)
     return abs(a - b)
+  def norm(i,x): return x if x=="?" else (x-i.lo)/(i.hi-i.lo+1e-32)
   def mid(i): return i.mu
   def var(i): return i.n < 2 and 0 or (max(0,i.m2)/(i.n - 1))^0.5
 
+#------------------------------------------------------------------------------
 class Sym(Col):
   def __init__(i,*_): super().__init__(*_); i.has={}
   def add(i,x, n=1,flip=1): 
@@ -49,6 +55,7 @@ class Sym(Col):
   def mid(i): max(i.has, key=i.has.get)
   def var(i): return -sum(n/i.n * math.log(n/i.n,2) for n in i.has.values())
 
+#------------------------------------------------------------------------------
 class Data(o):
   def __init__(i,src=[]): 
     i.rows,i.cols = [],None
@@ -66,7 +73,7 @@ class Data(o):
     return (sum(fun(c)**p for c in i.cols.x) / len(i.cols.x))**(1/p)
   def kpp(i,k, rows=None):
     rows = rows or i.rows
-    row1,*rows = random.choices(rows, k=min(len(rows,128))
+    row1,*rows = random.choices(rows, k=min(len(rows,the.some))
     out = [row1]
     while len(out) < k:
       tmp = [min(i.dist(x, y)**2 for y in out) for x in rows]
@@ -78,6 +85,7 @@ class Data(o):
           break
     return out
   
+#------------------------------------------------------------------------------
 class Cols(o):
   def __init__(i,names):
     i.x, i.y, i.names,i.klass = [],[],names,None
@@ -89,6 +97,7 @@ class Cols(o):
   def add(i,row): return [c.add(row[c.at]) for c in i.all]
   def sub(,row) : return [c.sub(row[c.at]) for c in i.all]
   
+#------------------------------------------------------------------------------
 def csv(file):
   for line in open(file):
     if s := line.strip():
@@ -105,9 +114,8 @@ def adds(src=[], out=None):
     out = out or (Sym if type(x)=="string" else Num)()
     out.add(x)
   return out
-  
-the = o(p=2)
 
+#------------------------------------------------------------------------------
 def eq(x,y): return x == y
 def le(x,y): return x <= y
 def gt(x,y): return x >  y
