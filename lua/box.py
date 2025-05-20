34db836722e75bd2792b58e92d392fce523b5019
timm <timm@ieee.org>
2025-05-20 14:29:17 -0400

    as

--- lua/box.py
+++ lua/box.py
@@ -6,7 +6,8 @@ many=random.choices
 
 class o:
   __init__ = lambda i, **d: i.__dict__.update(**d)
-  __repr__ = f.__name__ if (f:=i.__dict__.get("it")) else "").say(i.__dict__) 
+  __repr__ = lambda i: \
+             (f.__name__ if (f:=i.__dict__.get("it")) else "").say(i.__dict__) 
 
 the = o(p=2)
 
@@ -36,13 +37,13 @@ def Cols(names):
     all += [(Num if s[0].isupper() else Sym)(txt=s,at=c)]
     if s[-1] != "X": 
       (y if s[-1] in "+-" else x).append(all[-1])
-  return o(it=Cols,all=all ## all the columns
-                   x=x,    ## just the x columns
-                   y=y)))  ## just the y columns 
+  return o(it=Cols,all=all, ## all the columns
+                   x=x,     ## just the x columns
+                   y=y)     ## just the y columns 
 
 def Data(src=[]):
   src = iter(src)
-  return adds(o(it=Data, rows=[]                ## contains the rows
+  return adds(o(it=Data, rows=[],               ## contains the rows
                          cols=Cols(next(src))), ## summaries of the rows 
               src)
               
@@ -58,7 +59,7 @@ def add(i,v, flip=1,purge=False):
   def _sym():
     i.has[v] = flip + i.has.get(v,0)
 
-  def _data()
+  def _data():
     if flip < 0:  
       if purge: i.rows.remove(v) 
       [sub(v[col.at], col) for col in i.cols.all]  
@@ -85,12 +86,15 @@ def norm(i,v):
   return v if (v=="?" or i.it is not Num) else (v - i.lo)/(i.hi - i.lo + 1/BIG)
 
 def dist(col,v,w):
-  if v=="?" and w=="?": return 1
-  if col.it is Sym    : return v != w 
-  v,w = norm(col,v), norm(col,w)
-  v = v if v != "?" else (0 if w > 0.5 else 1)
-  w = w if w != "?" else (0 if v > 0.5 else 1)
-  return abs(v - w)
+  if v=="?" and w=="?": 
+    return 1
+  elif col.it is Sym: 
+    return v != w 
+  else:
+    v,w = norm(col,v), norm(col,w)
+    v = v if v != "?" else (0 if w > 0.5 else 1)
+    w = w if w != "?" else (0 if v > 0.5 else 1)
+    return abs(v - w)
   
 def minkowski(dims):
   total, n = 0, 1 / BIG
@@ -100,10 +104,10 @@ def minkowski(dims):
   return (total / n)**(1 / the.P)
 
 def xdist(data, row1, row2):  
-  return minkowski(dist(col,row1[col.at], row2[col.at]) for col in data.cols.x)
+  return minkowski(dist(c,row1[c.at], row2[c.at]) for c in data.cols.x)
 
 def ydist(data, row):  
-  return minkowski((norm(col,row[col.at]) - col.heaven) for col in data.cols.y)
+  return minkowski(abs(norm(c,row[c.at]) - c.heaven) for c in data.cols.y)
 
 def poles(data): # -> List[Row]
   r0, *some = many(i.rows, k=the.some + 1)
@@ -115,7 +119,7 @@ def poles(data): # -> List[Row]
 def lsh(data, poles): # -> Dict[Tuple, List[Row]]
   buckets = {}
   for row in data.rows:
-    k = tuple(i.project(row, a, b) for a, b in zip(poles, poles[1:]))
+    k = tuple(bucket(row, a, b) for a, b in zip(poles, poles[1:]))
     buckets[k] = buckets.get(k) or clone(data)
     add(buckets[k], row)
   return buckets
@@ -123,8 +127,26 @@ def lsh(data, poles): # -> Dict[Tuple, List[Row]]
 def project(data, row, a, b): # -> 0,1,2 .. the.bins-1
   D = lambda row1,row2: xdist(data,row1,row2)
   c = D(a,b)
-  x = (D(row, a)**2 + c**2 - D(row, b)**2) / (2 * c)
-  return min(int(x / c * the.bins), the.bins - 1) 
+  if c==0: return 0
+  return (D(row, a)**2 + c**2 - D(row, b)**2) / (2 * c *c)
+
+def bucket(data,row,a,b):
+  return min(int( project(data,row,a,b) * the.bins), the.bins - 1)
+
+def extrapolate(data,row,a,b):
+  ya, yb = ydist(data,a), ydist(data,b)
+  return ya + project(data,row,a,b) * (yb - ya)  
+
+def neighbors(c):
+ def gen(i=0, p=None,d=len(c)):
+   p = p or []
+   if i == d:
+     if tuple(p) != c: yield tuple(p)
+   else:
+     for dx in (-1, 0, 1):
+       x = c[i] + dx
+       if 0 <= x < d: yield from gen(i+1, p + [x],d)
+ return list(gen())
 
 #------------------------------------------------------------------------------
 def csv(path):
@@ -140,7 +162,7 @@ def coerce(x):
   y = x.lower()
   return (y == "true") if y in ("true", "false") else x
 
-def cat(x):
+def cat(x): 
   it = type(x)
   if it is list:  return "{" + ", ".join(map(cat, x)) + "}"
   if it is float: return str(int(x)) if x == int(x) else f"{x:.3g}"
