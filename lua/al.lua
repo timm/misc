cd1aa1b3a3a951410d78ec4af2a9b3ce639ecc36
timm <timm@ieee.org>
2025-06-24 10:12:39 +0300

    as

--- lua/al.lua
+++ lua/al.lua
@@ -14,6 +14,7 @@ local the={_about = {
 --## Misc utils
 local big  = 1E32
 local fmt  = string.format
+local R    = math.random
 
 -- bulk add
 local function adds(i,t) 
@@ -42,31 +43,28 @@ local function map(t,fn,    u)
   u={}; for _,v in pairs(t) do u[1+#u] = fn(v) end; return u end  
 
 -- make class
-local function new(kl,t) 
-  kl.__index=kl; return setmetatable(t,kl) end
+local o -- defined below, needed here for "__tostring"
 
--- Thimgs to strings.
--- local function o(x,      t,A,D,N)
---   t={}
---   A=function() for _,v in pairs(x) do t[1+#t]=o(v) end end
---   N=function() return x//1 == x and "%s" or "%.3g" end
---   D=function() for k,v in pairs(x) do 
--- 	         if not tostring(k):find"^_" 
--- 		 then t[1+#t]=fmt(":%s %s",k,o(v)) end end end
---   if type(x) == "number" then return fmt(N(), x) end
---   if type(x) ~= "table"  then return tostring(x) end
---   if #x>0 then A() else D(); table.sort(t) end
---   return "{" .. table.concat(t, " ") .. "}" end
+local function klass(name)
+  local kl = {}
+  kl.__index = kl
+  kl.__tostring = function(self) return name .. o(self) end
+  setmetatable(kl, {
+    __call= function(_,...) return setmetatable({},kl):_new(...) end})
+  return kl end
 
-local function o(x,      u)
-  if type(x)=="number" then return fmt(x//1==x and "%s" or "%.3g", x)
+local function normal(mu, sd)
+  return mu + sd * (-2 * math.log(R()))^.5 * math.cos(2 * math.pi * R()) end
+
+function o(x,      u,_yes)
+  if type(x)=="number" then return fmt(x//1==x and "%s" or "%.3g", x) end
   if type(x)~="table"  then return tostring(x) end
   u = {}
-  if #x>0 then u = map(x,o) 
-  else for k,v in pairs(x) do
-         if k:sub(1,1)~="_" then u[#u+1]=fmt(":%s %s",k,o(v)) end end
-         table.sort(u) end
-  return "{" .. table.concat(u, " ") .. "}" end
+  _yes = function(k) return  tostring(k):sub(1,1)~="_" end 
+  if #x>0 then u = map(x,o) else
+    for k,v in pairs(x) do if _yes(k) then u[#u+1]=fmt(":%s %s",k,o(v)) end end
+    table.sort(u) end
+  return "{" .. table.concat(u, " ") .. "}" end 
 
 -- Push `x` to end of  list, return `x`.
 local function push(t,x)
@@ -78,10 +76,14 @@ local function sum(t,fn,   n)
 
 ----------------------------------------------------------------------
 -- ## Sym
-local Sym={}
+local Sym=klass"Sym"
 
-function Sym.new(i,at,txt)
-  return new(Sym,{at=at or 0, txt=txt or "", n=0, has={}}) end
+function Sym._new(i,at,txt)
+  i.at  = at or 0
+  i.txt = txt or ""
+  i.n   = 0
+  i.has = {}
+  return i end
 
 function Sym.add(i,v) 
   if v~= "?" then i.n=i.n+1; i.has[v] = 1 + (i.has[v] or 0) end end
@@ -94,56 +96,59 @@ function Sym.div(i,      P)
 
 function Sym.mid(i,     most,mode)
   most=0
-  for k,n in pairs(i.has) do if x > most then mode,most=k,n end end
+  for k,n in pairs(i.has) do if n > most then mode,most=k,n end end
   return mode end
 
 -----------------------------------------------------------------------
 -- ## Num
-local Num={}
+local Num=klass"Num"
 
-function Num.new(i,at,txt)
-  return new(Num,{at=at or 0, txt=txt or "", n=0,
-                  mu=0, m2=0,hi=-big,lo=big,
-                  w= tostring(txt):find"-$" and 0 or 1}) end
+function Num._new(i,at,txt)
+  i.at          = at or 0
+  i.txt         = txt or ""
+  i.n,i.mu,i.m2 = 0,0,0
+  i.lo, i.hi    = big, -big
+  i.w           = tostring(txt or ""):find"-$" and 0 or 1 
+  return i end
 
 function Num.add(i,v,    d)
   if v ~= "?" then
-    i.n  = i.n+1
+    i.n  = i.n + 1
     i.lo = math.min(i.lo,v)
     i.hi = math.max(i.hi,v)
-    d    = v - i.muv
+    d    = v - i.mu
     i.mu = i.mu + d/i.n
     i.m2 = i.m2 + d*(v - i.mu) end end
 
 function Num.bin(i,x) 
-  return (the.bin * i:cdf(x)) //1 end
+  return (the.bin * i:cdf(x)) // 1 end
 
-function Num.cdf(i,x,    fn)
+function Num.cdf(i,x,    fn,z)
   fn = function(z) return 1 - 0.5*math.exp(-0.717*z - 0.416*z*z) end
   z = (x - i.mid()) / i.div()
   return z>=0 and fn(z) or 1 - fn(-z) end
 
 function Num.div(i) 
-   return i.n < 2 and 0 or (math.max(0, i.m2)/(i.n-1 ))^0.5 end
-
-function Num.norm(i,x)
-  return x=="?" and x or (x - i.lo)/(i.hi - i.lo + 1/big) end
+   return i.n < 2 and 0 or (i.m2/(i.n-1 ))^0.5 end
 
 function Num.mid(i) 
   return i.mu end
 
+function Num.norm(i,x)
+  return x=="?" and x or (x - i.lo)/(i.hi - i.lo + 1/big) end
+
 -----------------------------------------------------------------------
 -- ## Cols
-local Cols={}
+local Cols=klass"Cols"
 
-function Cols.new(i,t,      all,x,y,klass,col)
-  all,x,y = {},{},{}
+function Cols._new(i,t,      all,x,y,klass,col)
+  i.all, i.x, i.y, i.name = {},{},{},t
   for n,s in pairs(t) do
-    col = push(all, (s:find"^[A-Z]" and Num or Sym):new(n,s))
+    col = push(i.all, (s:find"^[A-Z]" and Num or Sym)(n,s))
     if not s:find"X$" then
-      push(s:find"[+!-]$" and y or x,col)
-      if s:find"!$" then klass = col end end end 
-  return new(Cols, {all=all,x=x,y=x,klass=klass,names=t}) end 
+      push(s:find"[+!-]$" and i.y or i.x,col)
+      if s:find"!$" then i.klass = col end end end 
+  return i end
 
 function Cols.add(i,t)
   for _,col in pairs(i.all) do col:add(t) end
@@ -151,20 +156,20 @@ function Cols.add(i,t)
 
 --------------------------------------------------------------------------
 --## Data
-local Data={}
+local Data=klass"Data"
 
-function Data.new(i,t) 
-  i = new(Data,{rows={}, cols=nil})
-  if   type(t) == "string" 
-  then for row in csv(t)         do i:add(row) end
-  else for row in pairs(t or {}) do i:add(row) end end 
-  return i end  
+function Data._new(i,src) 
+  i.rows, i.cols = {}, nil
+  return type(src)=="string" and i:fromFile(src) or adds(i,src) end 
 
 function Data.add(i,t)
-  if i.cols then push(i.rows,i.cols:add(t)) else i.cols=Cols:new(t) end end 
+  if i.cols then push(i.rows, i.cols:add(t)) else i.cols=Cols(t) end end 
 
 function Data.clone(i,  t)
-  return adds(Data:new({i.cols.names}),t) end
+  return adds(Data({i.cols.names}),t) end
+
+function Data.fromFile(i,file)
+  for u in csv(file) do i:add(u) end; return i end
 
 -----------------------------------------------------------------------
 -- ## Command-line
@@ -184,7 +189,7 @@ local function eg(t)
 eg{flag="-b", txt="number of bins", arg="int", fn=function(b)
    the.bins = atom(b) end}
 
-eg{flag="-p", txt="distance coeffecient", arg="int", fn=function(p)
+eg{flag="-p", txt="distance term", arg="int", fn=function(p)
    the.p = atom(p) end}
 
 eg{flag="-f", txt="data file", arg="file", fn=function(f) 
@@ -198,6 +203,20 @@ eg{flag="-h", txt="show help", fn= function(_)
 
 eg{flag="--the", txt="show config", fn=function(_) 
    print(o(the)) end}
+
+eg{flag="--normal", txt="test normal", fn=function(_,t,x) 
+  t={}
+  for _ =1,10000 do x=(normal(10,1) // .5)*.5; t[x] = x + (t[x] or 0) end
+  for x,n in pairs(t) do 
+    print(fmt("%10s %10s : %s",x,n, ("*"):rep(n//500))) end end}
+
+eg{flag="--sym", txt="test sym", fn=function(_) 
+   print(o(adds(Sym(),{"a","a","a","a","b","b","c"}))) end}
+
+eg{flag="--num", txt="test sym", fn=function(_,    n) 
+   n=Num()
+   for _ = 1,10000 do n:add(normal(10,1)) end
+   print(n:mid(), n:div()) end}
 
 if arg[0]:find"??al.lua" then
    for i,s in pairs(arg) do
