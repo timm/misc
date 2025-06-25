b661335b9a8665a4c9de4c5ec4dd15bb95d55122
timm <timm@ieee.org>
2025-06-25 04:22:15 +0300

    as

--- lua/al.lua
+++ lua/al.lua
@@ -17,37 +17,43 @@ local big  = 1E32
 local fmt  = string.format
 local R    = math.random
 
--- bulk add
-local function adds(i,t) 
+-- bulk add  
+-- (obj,{any}) --> obj 
+local function adds(i,t)  
   for _,x in pairs(t or {}) do 
     i = i or (type(x)=="number" and Num() or Sym())
     i:add(x) end 
   return i end 
 
--- coerce
-local function atom(s) 
-  fn = function(s) return (s=="true" and true) or (s~= false" and s) end
+-- coerce   
+-- (str) --> bool | num | str
+local function atom(s)  
+  fn = function(s) return (s=="true" and true) or (s~= "false" and s) end
   return tonumber(s) or math.tointeger(x) or fn(s:match"^%s*(.-)%s*$") end
 
--- Csv string to csv cells.
-local function cells(s,fn,    t)
+-- Csv string to csv cells.   
+-- (str,fun) --> {any}
+local function cells(s,fn,    t) 
   t={}; for s1 in s:gmatch("([^,]+)") do t[1+#t]=fn(s1) end; return t end
 
--- Iterate over csv rows from file
-local function csv(file,     src)
+-- Iterate over csv rows from file  
+-- (str) --> {bool | num | str 
+local function csv(file,     src) 
   src = io.input(file)
   return function(    line) 
     line = io.read()
     if line then return cells(line, atom) else io.close(src) end end end
 
--- Filter all items through `fn`
-local function map(t,fn,    u) 
+-- Filter all items through `fn`  
+-- ({any},fun) --> {any}
+local function map(t,fn,    u)  
   u={}; for _,v in pairs(t) do u[1+#u] = fn(v) end; return u end  
 
--- make class
-local o -- defined below, needed here for "__tostring"
+local o -- defined below, needed here for klass' "__tostring"
 
-local function klass(name)
+-- make class  
+-- (str) --> klass
+local function klass(name) 
   local kl = {}
   kl.__index = kl
   kl.__tostring = function(self) return name .. o(self) end
@@ -55,10 +61,14 @@ local function klass(name)
     __call= function(_,...) return setmetatable({},kl):_new(...) end})
   return kl end
 
-local function normal(mu, sd)
+-- Sample from a gaussian   
+-- (num,num) --> 0..1
+local function normal(mu,sd) 
   return mu + sd * (-2 * math.log(R()))^.5 * math.cos(2 * math.pi * R()) end
 
-function o(x,      u,_yes)
+-- Pretty print most things.    
+-- (any) --> str
+function o(x,      u,_yes) 
   if type(x)=="number" then return fmt(x//1==x and "%s" or "%.3g", x) end
   if type(x)~="table"  then return tostring(x) end
   u = {}
@@ -68,44 +78,57 @@ function o(x,      u,_yes)
     table.sort(u) end
   return "{" .. table.concat(u, " ") .. "}" end 
 
--- Push `x` to end of  list, return `x`.
-local function push(t,x)
+-- Push `x` to end of  list, return `x`.    
+-- ({any},any) --> x
+local function push(t,x) 
    t[1+#t]=x; return x end
 
--- Sum items, filtered by `fn`.
-local function sum(t,fn,   n) 
+-- Sum items, filtered by `fn`.   
+-- ({any},fun) --> num
+local function sum(t,fn,   n)  
   n=0; map(t,function(v) n=n+fn(v) end); return n end
 
 ----------------------------------------------------------------------
 -- ## Sym
+
+-- Keeps summaries of numeric columns
 local Sym=klass"Sym"
 
-function Sym._new(i,at,txt)
-  i.at  = at or 0
-  i.txt = txt or ""
-  i.n   = 0
-  i.has = {}
+-- Constructor
+function Sym._new(i,at,txt) -- (int, str) --> Sym
+  i.at  = at or 0       -- col position
+  i.txt = txt or ""     -- col name
+  i.n   = 0             -- items seen
+  i.has = {}            -- symbol counts
   return i end
 
-function Sym.add(i,v) 
+-- Update
+function Sym.add(i,v) -- (any) --> nil
   if v~= "?" then i.n=i.n+1; i.has[v] = 1 + (i.has[v] or 0) end end
 
-function Sym.bin(i,x) return x end
+-- Discretize (symbols discretize to themselves).
+function Sym.bin(i,x)  -- (any) --> x
+  return x end
 
-function Sym.div(i,      P)
+-- Symboolic diversity (how much we avoid central tendancy) is entropy.
+function Sym.div(i,      P) -- () --> float
   P = function(n) return n/i.n * math.log(n/i.n, 2) end
   return sum(i.has,P) end
 
-function Sym.mid(i,     most,mode)
+-- Symboolic central tendancy is model.
+function Sym.mid(i,     most,mode) -- () --> any
   most=0
   for k,n in pairs(i.has) do if n > most then mode,most=k,n end end
   return mode end
 
 -----------------------------------------------------------------------
 -- ## Num
+--
+-- Keeps summaries of numeric columns
 local Num=klass"Num"
 
-function Num._new(i,at,txt)
+-- Constructor
+function Num._new(i,at,txt) -- (int,str) --> Num
   i.at          = at or 0
   i.txt         = txt or ""
   i.n,i.mu,i.m2 = 0,0,0
@@ -113,7 +136,8 @@ function Num._new(i,at,txt)
   i.w           = tostring(txt or ""):find"-$" and 0 or 1 
   return i end
 
-function Num.add(i,v,    d)
+-- Update, uses Welford's algorithm (for incremntal variance calcs).
+function Num.add(i,v,    d) -- (v) --> nil
   if v ~= "?" then
     i.n  = i.n + 1
     i.lo = math.min(i.lo,v)
@@ -122,27 +146,35 @@ function Num.add(i,v,    d)
     i.mu = i.mu + d/i.n
     i.m2 = i.m2 + d*(v - i.mu) end end
 
-function Num.bin(i,x) 
+-- Gaussian discretization.
+function Num.bin(i,x)  -- (num) --> int
   return (the.bin * i:cdf(x)) // 1 end
 
-function Num.cdf(i,x,    fn,z)
+-- CDF(x) = prob that x takes a value less than or equal to x.
+function Num.cdf(i,x,    fn,z) -- (num) --> 0..1
   fn = function(z) return 1 - 0.5*math.exp(-0.717*z - 0.416*z*z) end
   z = (x - i.mid()) / i.div()
   return z>=0 and fn(z) or 1 - fn(-z) end
 
-function Num.div(i) 
+-- Numeric diversity (how much we avoid central tendancy) is std.dev.
+function Num.div(i)  -- () --> float
    return i.n < 2 and 0 or (i.m2/(i.n-1 ))^0.5 end
 
-function Num.mid(i) 
+-- Numeric central tendancy is mean.
+function Num.mid(i)  -- () --> num
   return i.mu end
 
-function Num.norm(i,x)
+-- Normalize x to 0..1 for lo..hi
+function Num.norm(i,x) -- (num) --> 0..1
   return x=="?" and x or (x - i.lo)/(i.hi - i.lo + 1/big) end
 
 -----------------------------------------------------------------------
 -- ## Cols
+
+-- Finds, and stores, all the roles of our columns.
 local Cols=klass"Cols"
 
+-- Computes Nums and Syms from list `t` of column names.
 function Cols._new(i,t,      all,x,y,klass,col)
   i.all, i.x, i.y, i.name = {},{},{},t
   for n,s in pairs(t) do
@@ -152,6 +184,7 @@ function Cols._new(i,t,      all,x,y,klass,col)
       if s:find"!$" then i.klass = col end end end 
   return i end
 
+-- Update
 function Cols.add(i,t)
   for _,col in pairs(i.all) do col:add(t) end
   return t end
