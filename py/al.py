6a463a44bc89df63a51846489886dc155f35811e
timm <timm@ieee.org>
2025-06-27 04:15:23 +0300

    sadsa

--- py/al.py
+++ py/al.py
@@ -1,67 +1,15 @@
 import math, random, sys
 
-class o:
-  __init__ = lambda i, **d: i.__dict__.update(d)
-  __repr__ = lambda i     : f"{i.__class__.__name__}{vars(i)}"
+the = dict(Acq="xploit",  
+           assume=4,  # initial guesses
+           build=20,  # for theory building
+           check=5,   # for checking (theory does not change)
+           file="../../moot/optimize/misc/auto03.csv", 
+           guess=0.5, 
+           k=2, 
+           m=1, 
+           seed=1234567891)
 
-the = o(Acq="xploit", assume=4, Bins=4, build=20, check=5,
-        file="", guess=0.5, k=2, m=1, p=2, seed=1234567891)
-
-#--------------------------------------------------------------------
-class Sym(o):
-  def __init__(i, at=0, txt=""): i.at,i.txt,i.n,i.has=at,txt,0,{}
-
-  def bin(i,x): return x
-  def add(i,x): 
-    if x!="?": i.n+=1; i.has[x]=i.has.get(x,0)+1
-
-#--------------------------------------------------------------------
-class Num(o):
-  big = 1e32
-  def __init__(i, at=0, txt=""):
-    i.at,i.txt,i.n = at,txt,0
-    i.lo,i.hi = i.big, -i.big
-    i.heaven = 0 if txt.endswith("-") else 1
-
-  def norm(i,x): return (x-i.lo)/(i.hi-i.lo+1/i.big)
-  def bin(i,x): return int(min(the.Bins-1,i.norm(x)*the.Bins))
-  def add(i,x):
-    if x!="?": i.n+=1; i.lo=min(i.lo,x); i.hi=max(i.hi,x)
-
-#--------------------------------------------------------------------
-class Cols(o):
-  def __init__(i,names):
-    i.names,i.all,i.x,i.y = names,[],[],[]
-    i.klass = None
-    for n,t in enumerate(names):
-      c = Num(n,t) if t[0].isupper() else Sym(n,t)
-      i.all.append(c)
-      if t[-1] != "X":
-        (i.y if t[-1] in "!+-" else i.x).append(c)
-        if t[-1] == "!": i.klass = c
-
-  def add(i,t):
-    for c in i.all: c.add(t[c.at])
-    return t
-
-#--------------------------------------------------------------------
-class Data(o):
-  def __init__(i, src=[]): 
-    i.rows,i.cols = [],None
-    [i.add(x) for x in src]
-
-  def add(i,t):
-    if i.cols: i.rows += [i.cols.add(t)]
-    else: i.cols = Cols(t)
-    return i
-
-  def clone(i,rows=[]): return Data([i.cols.names]+rows)
-
-  def ydist(i,r):
-    d = sum((c.norm(r[c.at])-c.heaven)**2 for c in i.cols.y)
-    return (d/len(i.cols.y))**0.5
-
-#--------------------------------------------------------------------
 def atom(s):
   for fn in [int, float]:
     try: return fn(s)
@@ -75,83 +23,124 @@ def csv(file):
     for s in f:
       if s.strip(): yield [atom(x) for x in s.strip().split(",")]
 
+class o:
+  __init__ = lambda i, **d: i.__dict__.update(d)
+  __repr__ = lambda i     : f"{i.__class__.__name__}{vars(i)}"
+
+the = o(**the)
+
+#--------------------------------------------------------------------
+class Sym(o):
+  def __init__(i, at=0, txt=""): i.at,i.txt,i.n,i.has=at,txt,0,{}
+
+  def bin(i,x): return x
+  def add(i,x,inc=1,_=False): 
+    if x!="?": 
+      i.n+=inc; i.has[x]=i.has.get(x,0)+inc
+
+  def pdf(i,s, prior=0):
+    return (i.has.get(s,0) + the.m*prior) / (i.n + the.m + 1/Num.big)
+
+#--------------------------------------------------------------------
+class Num(o):
+  big = 1e32
+  def __init__(i, at=0, txt=""):
+    i.at,i.txt,i.n = at,txt,0
+    i.mu, i,m2,i.sd = 0,0,0
+    i.lo,i.hi = i.big, -i.big
+    i.heaven = 0 if txt.endswith("-") else 1
+
+  def norm(i,x): return (x-i.lo)/(i.hi-i.lo+1/Num.big)
+
+  def add(i,x, inc=1,_=False):
+    if x!="?": 
+      i.n+=inc; i.lo=min(i.lo,x); i.hi=max(i.hi,x)
+      if inc < 0 and i.n < 2:
+        i.sd = i.m2 = i.mu = i.n = 0
+      else:
+       d     = x - i.mu
+       i.mu += inc * (x / i.n)
+       i.m2 += inc * (d * (x - i.mu))
+       i.sd  = 0 if i.n <= 2 else (max(0,i.m2)/(i.n-1))**.5
+
+  def pdf(i,v,_):
+    sd  = i.sd or 1 / Num.big
+    var = 2 * sd * sd
+    z   = (v - i.mu) ** 2 / var
+    return min(1, max(0, math.exp(-z) / (2 * math.pi * var) ** 0.5))
+
+#--------------------------------------------------------------------
+class Cols(o):
+  def __init__(i,names):
+    i.names,i.all,i.x,i.y = names,[],[],[]
+    i.klass = None
+    for n,t in enumerate(names):
+      c = Num(n,t) if t[0].isupper() else Sym(n,t)
+      i.all.append(c)
+      if t[-1] != "X":
+        (i.y if t[-1] in "!+-" else i.x).append(c)
+        if t[-1] == "!": i.klass = c
+
+#--------------------------------------------------------------------
+class Data(o):
+  def __init__(i, src=[]): 
+    i.rows,i.cols = [],None
+    [i.add(x) for x in src]
+
+  def sub(i,t,zap=False): return i.add(t,-1,zap)
+
+  def add(i,t, inc=1, zap=False):
+    if not i.cols: i.cols = Cols(t)
+    else:
+      if inc > 0 : i.rows.append(t)
+      elif zap   : i.rows.remove(t) # slow for large lists
+      for col in i.cols.all: col.add(t[col.at], inc)
+    return i
+
+  def clone(i,rows=[]): return Data([i.cols.names]+rows)
+
+  def like(i, t, nall=100, nh=2):
+    prior = (i.n + the.k) / (nall + the.k*nh)
+    tmp = [c.pdf(v,prior) for c in i.cols.x if (v:=t[c.at]) != "?"]
+    return sum(math.log(n) for n in tmp + [prior] if n>0)
+
+  def ydist(i,r):
+    d = sum((c.norm(r[c.at])-c.heaven)**2 for c in i.cols.y)
+    return (d/len(i.cols.y))**0.5
+
+  def ydists(i,t=None):
+    return  sorted(t or i.rows, key=lambda r: i.ydist(r))
+
 #--------------------------------------------------------------------
-class Syms(o):
-  def __init__(i, d):
-    i.data, i.nall, i.nh, i.nk = d, 0, 0, {}
-    i.fs = {}  # flat dict: (k, at, bin) → count
+def acquire(yes, no, t, nall=100, nh=2):
+  b = math.exp(yes.like(t, nall, nh))
+  r = math.exp(no.like(t, nall, nh))
+  p = nall / the.build
+  q = dict(xploit=0, xplor=1).get(the.Acq, 1 - p)
+  return (b + r*q) / abs(b*q - r + 1 / Num.big)
 
-  def adds(i, rows, k, inc=1): 
-    [self.add(r,k,inc) for r in rows]; return rows
+def acquires(data, rows):
+  random.shuffle(rows)
+  nall = the.Assume
+  cut  = round(nall**the.Guess) # [done = best + rest] + todo
+  todo = data.rows[nall:]
+  done = data.ydists(rows[:nall])
+  best = data.clone(done[:cut])
+  rest = data.clone(done[cut:])
+  br   = data.clone(done)
+  _guess  = lambda t: acquire(best,rest, t, nall, 2)
+  _guesses = lambda t: sorted(t, key=_guess, reverse=True)
+  while len(todo) > 2 and nall < the.Build:
+    nall  += 1
+    hi,*lo = _guesses(todo[:the.Few*2])
+    todo   = lo[:the.Few] + todo[the.Few*2:] + lo[the.Few:]
+    br.add( best.add(hi))
+    best.rows = br.ydists(best.rows)
+    if len(best.rows) >= cut:
+      rest.add( best.sub( best.rows.pop(-1)))
+  return o(best = best, rest=rest, 
+           test = data.ydists(_guesses(todo)[:the.check])[0])
 
-  def add(i, r, k, inc=1):
-    i.nall += inc
-    i.nh += k not in i.nk
-    i.nk[k] = i.nk.get(k, 0) + inc
-    for c in i.data.cols.x:
-      if (v := r[c.at]) != "?":
-        key = (k, c.at, c.bin(v))
-        i.fs[key] = i.fs.get(key, 0) + inc
-    return r
-
-  def sub(i, r, k): return i.add(r, k, inc=-1)
-
-  def like(i, r, k):
-    prior = (i.nk[k] + the.k) / (i.nall + the.k * i.nh)
-    l = math.log(prior)
-    for c in i.data.cols.x:
-      if (v := r[c.at]) != "?":
-        key = (k, c.at, c.bin(v))
-        f = i.fs.get(key, 0)
-        l += math.log((f + the.m * prior) /
-                      (i.nk[k] + the.m + 1 / Num.big))
-    return l
-
-  def score(i, r):
-    b = math.exp(i.like(r, True))
-    u = math.exp(i.like(r, False))
-    p = i.nall / the.build
-    q = dict(xploit=0, xplor=1).get(the.Acq, 1 - p)
-    return (b + u*q) / abs(b*q - u + 1 / Num.big)
-
-  def acquires(i, rows):
-    rows = rows[:]
-    random.shuffle(rows)
-    seen = Syms(i.data) # use all x variables for discretimzation/normalizatipn
-    todo = rows[the.assume:]
-    done = i.clone(rows[:the.assume])
-    Y    = lambda r: done.ydist(r) # use done Y variables for ydist
-    done.rows = sorted(done.rows, key=Y, reversed=True)
-    cut  = int(the.assume ** the.guess)
-    best = seen.adds(done.rows[cut:],True)
-    rest = seen.adds(done,rows[:cut],False)
-    while len(todo)>2 and (len(best) + len(rest)) < the.Build:
-      hi,tmp = extractMost(todo[:the.Few], seen.score)
-      todo   = todo[the.Few:] + tmp
-      insertDescending(best, hi, Y)
-      if len(best) > (len(best) + len(rest)) > XXX:
-        demoted = best.pop()
-        rest += [demoted]
-        seen.sub(demoted,True)
-        seen.add(demoted,False)
-    return best, rest
-
-def extractMost(lst, key):
-  most = key(lst[-1])
-  for i in range(len(lst) - 1):
-    if (s := key(lst[i])) > most:
-      lst[i], lst[-1] = lst[-1], lst[i]
-      most = s
-  return lst.pop(), lst
-
-def insertDescending(lst, new, key):
-  lst.append(lst[-1])  # open a slot at the end
-  i = len(lst) - 2
-  while i >= 0 and key(new) > key(lst[i]):
-    lst[i + 1] = lst[i]
-    i -= 1
-  lst[i + 1] = new
-  return new
 #--------------------------------------------------------------------
 def eg__acquires():
   print("== testing acquires")
