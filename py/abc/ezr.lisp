6b1db25acce0ae720a9a638bb0d48637975ff1cf
timm <timm@ieee.org>
2025-07-13 11:40:11 -0400

    lib.lisp added

--- py/abc/ezr.lisp
+++ py/abc/ezr.lisp
@@ -1,108 +1,67 @@
 ;; <!-- vim: set lispwords+=has,loop,format ts=2 sw=2 sts=2 et : -->
+(load "lib")
+
 (defvar *help* "
 ezr.lisp: multi-objective explanation
-(c)2025, Tim Menzies <timm@ieee.org>, MIT license")
+(c) 2025, Tim Menzies <timm@ieee.org>, MIT license")
 
+;; Options
 (defvar  *options* '(
   (k     "-k"  "kth value"        2)
   (goal  "-g"  "start-up action"  "one")
   (seed  "-s"  "random number"    1234567891)
   (file  "-f"  "data file"        "../../../moot/optimize/misc/auto93.csv")))
 
-;;------------------------------------------------------------------------------
-;; ## Macros
-
+;; Access options
 (defmacro ? (x) `(fourth (assoc ',x *options*)))
 
-(defmacro has (x lst) 
-  `(cdr (or (assoc ,x ,lst :test #'equal)
-            (car (setf ,lst (cons (cons ,x 0) ,lst))))))
-
-(set-macro-character #\$
-  (lambda (stream char) `(slot-value self ',(read stream t nil t))))
-
-(defmacro prog+ (&body body)
-  #-sbcl `(progn ,@body)
-  #+sbcl `(handler-case
-            (progn ,@body)
-            (error (e)
-              (format t "❌ Error: ~A~%" e))))
+;; Structs
+(defstruct data rows cols)
+(defstruct cols x y all names klass)
+(defstruct sym (n 0) (at 0) (txt "") (has 0))
+(defstruct num (n 0) (at 0) (txt "") (mu 0)
+               (m2 0) (sd 0) (lo 1e32) (hi -1e32) (goal 1))
 
 ;;-----------------------------------------------------------------------------
-;; ## Structures
+;; ## Initialization
 
-(defstruct data rows cols)
-(defstruct cols x y all klass)
-(defstruct sym (n 0) (at 0) (txt "") (has 0))
-(defstruct num (n 0) (at 0) (txt "") (mu 0) (m2 0) (sd 0) 
-                     (lo 1e32) (hi -1e32) (goal 1))
+(defun adds (lst &optional it)
+  (dolist (x lst it)
+    (setf it (or it (if (numberp x) (make-num) (make-sym))))
+    (add it x)))
 
-(defmethod more ((self t) &key) self)
+(defun nuSym (&optional inits &key (at 0) (txt " "))
+  (adds inits (make-sym :at at :txt txt)))
 
-(defmethod more ((self num) &key)
-  (if (isChr $txt -1 #\-)  (setf $goal 0))
+(defun nuNum (&optional inits &key (at 0) (txt " "))
+  (adds inits (make-num :at at :txt txt
+                         :goal (if (chrp txt -1 #\-)  0 1))))
+
+(defmethod nuData (&optional inits &aux (self (make-data)))
+  (if (stringp src) 
+    (mapcsv (lambda (x) (add self x)) src)
+    (mapcar (lambda (x) (add self x)) src))
   self)
 
-(defmethod more ((self data) &key src)
-  (labels ((fn (row) (add self row)))
-    (if (consp src) (mapcar #'fn src) (mapcsv #'fn src))
-    self))
-
-(defmethod more ((self cols) &key)
-  (dolist (name $names self)
-    (labels ((keep (col)
-                   (push col $all)
-                   (unless (ischr name -1 #\X) 
-                     (if (member (chr name -1) (list #\! #\- #\+)) 
-                       (push col $y)
-                       (push col $x)))))
-      (keep (more (funcall (if (upper-case-p (chr name 0)) #'make-num #'make-sym)
-                           :txt name :at (length $all)))))))
+(defmethod nuCol (name &aux (self (make-cols :names row)))
+  (dolist (txt $names self)
+    (let ((col (funcall (if (upper-case-p (chr txt 0)) #'nuNum #'nuSym) 
+                        :txt txt :at (length $all))))
+      (push col $all)
+      (unless (chrp txt -1 #\X)
+        (if (chrp txt -1 #\!) (setf $klass col))
+        (if (member (chr txt -1) '(#\! #\- #\+)) 
+          (push col $y)
+          (push col $x))))))
 
 ;;------------------------------------------------------------------------------
 ;; ## Update
 
-(defmethod add ((i data) (row cons) &key (inc 1) zap)
+(defmethod add ((self data) (row cons) &key (inc 1) zap)
   (if $cols
     (push (add $cols row inc) $rows)
-    (setf $cols (more (make-cols :names row)))))
-
-;;------------------------------------------------------------------------------
-;; ## Functions
-
-;; ### Misc
-
-(defun args () (cdr #+clisp ext:*args* #+sbcl sb-ext:*posix-argv*))
-
-(defun chr (s i)   (char (string s) (if (minusp i) (+ (length s) i) i)))
-
-(defun isChr (s i c) (char= (chr s i) c))
-
-;; ### Random Numbers
-
-(defvar *seed* (? seed))
-
-(defun rint (&optional (n 100) &aux (base 1E10)) 
-  (floor (* n (/ (rand base) base))))
-
-(defun rand (&optional (n 1)) 
-  (setf *seed* (mod (* 16807.0d0 *seed*) 2147483647.0d0))
-  (* n (- 1.0d0 (/ *seed* 2147483647.0d0))))
-
-;; ### String 2 Thing
-
-(defun thing (s &aux (s1 (string-trim '(#\Space #\Tab) s))) 
-  (let ((x (let ((*read-eval* nil)) (read-from-string s1 ""))))
-    (if (or (numberp x) (member x '(t nil ?))) x s1)))
-
-(defun things (s &optional (sep #\,) (here 0)) ; --> list
-  (let ((there (position sep s :start here)))
-    (cons (thing (subseq s here there))
-          (if there (things s sep (1+ there))))))
-
-(defun mapcsv (file &optional (fun #'print) end)
-  (with-open-file (s (or file *standard-input*))
-    (loop (funcall fun (things (or (read-line s nil) (return end)))))))
+    (setf $cols (more (make-cols :names row))))
+  row)
 
 ;;---------------------------------------------------------------------------
 ;; ## Examples
@@ -118,14 +77,14 @@ ezr.lisp: multi-objective explanation
 
 ;;-----------------------------------------------------------------------------
 ;; ## Main
+
 (defun cli (options &aux it)
-  (loop :for (key flag help b4) :in options 
-    :collect (list key flag help 
-                   (if (setf it (member flag (args) :test #'string=))
-                     (cond ((eq b4 t) nil)
-                           ((eq b4 nil) t)
-                           (t (thing (second it))))
-                     b4))))
+  (loop :for (key flag help b4) :in options :collect
+    (list key flag help (if (setf it (member flag (args) :test #'string=))
+                          (cond ((eq b4 t) nil)
+                                ((eq b4 nil) t)
+                                (t (thing (second it))))
+                          b4))))
 
 (when (and *load-pathname* *load-truename*
            (equal (truename *load-pathname*) *load-truename*))
