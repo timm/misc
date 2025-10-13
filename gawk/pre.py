1693134b0976a8f7edf7d1e8c3a5692ea1116683
timm <timm@ieee.org>
2025-10-12 21:22:15 -0400

    as

--- gawk/pre.py
+++ gawk/pre.py
@@ -18,7 +18,7 @@ from types import SimpleNamespace as obj
 
 the = obj(bins=5, seed=42, p=2, Few=256, file="auto93.csv")
 
-BIG=1e32
+BIG = 1e32
 
 # Prudence check: does 'the' match the __doc__?
 got  = re.findall(r'(\w+)=(\S+)', __doc__)
@@ -29,24 +29,24 @@ assert not (bad := set(want) ^ set(got)), f"Mismatch: {bad}"
 ### Create 
 
 # Nums and Syms are for summarizing numerics and symbolics.
-def Sym(at=0,txt=" "): return obj(it=Sym, at=at, txt=txt,n=0, has={})
-def Num(at=0,txt=" "): return obj(it=Num, at=at, txt=txt,n=0, mu=0, m2=0, sd=0, 
+def Sym(at=0,txt=" "): return obj(it=Sym, n=0, at=at, txt=txt, has={})
+def Num(at=0,txt=" "): return obj(it=Num, n=0, at=at, txt=txt, mu=0, m2=0, sd=0,
                                   mins={}, lo=BIG, hi=-BIG, best=txt[-1]!="-")
 
 # Data stores (1) rows as well as (2) summaries of each column.
-def Data(src): 
+def Data(src):
   src = iter(src)
-  return adds(src, obj(it=Data, n=0, mid=None, rows={}, cols=Cols(next(src))))
+  return adds(src, obj(it=Data, n=0, rows={}, cols=Cols(next(src))))
 
 # Columns (which are Nums and Syms) are built from a list of names.
 def Cols(names):
   all = [(Num if s[0].isupper() else Sym)(i,s) for i,s in enumerate(names)]
-  return obj(it=Cols, names=names, all=all, 
-             y = [col for col in all if col.txt[-1]     in "-+"], 
+  return obj(it=Cols, names=names, all=all,
+             y = [col for col in all if col.txt[-1]     in "-+"],
              x = [col for col in all if col.txt[-1] not in "-+X"])
 
 # Copy the structure of a data, maybe add in some rows.
-def clone(data,rows=[]): 
+def clone(data,rows=[]):
   return Data([data.cols.names] + rows)
 
 #------------------------------------------------------------------------------
@@ -60,9 +60,8 @@ def adds(lst, it=None):
 # To sub, add a negative amount.
 def sub(x,v): return add(x, v, inc=-1)
 
-# To add v, jump over 'dont knows', increment summaries, return v.
-# To do the update,  change rows then recursively update column summaries.
-# If the increment `inc` is negative, that means remove a row.
+# To add v, jump over 'dont knows', increment summaries, then return v.
+# To do the update,  change the rows then recursively update column summaries.
 def add(x, v, inc=1):
   if v == "?": return v
   x.n += inc
@@ -70,17 +69,16 @@ def add(x, v, inc=1):
     [add(col, v[col.at], inc) for col in x.cols.all]
     if inc > 0: x.rows[id(v)] = v
     else: x.rows.pop(id(v))
-    x.mid = None
-  elif x.it is Sym: 
+  elif x.it is Sym:
     x.has[v] = inc + x.has.get(v,0)
   elif x.it is Num:
     x.lo, x.hi = min(v, x.lo), max(v, x.hi)
-    if inc < 0 and x.n < 2: 
+    if inc < 0 and x.n < 2:
       x.sd = x.mu = x.m2 = x.n = 0
     else:
       d = v - x.mu
       x.mu += inc * d / x.n
-      x.m2 += inc * d * (v - x.mu) 
+      x.m2 += inc * d * (v - x.mu)
       x.sd = 0 if x.n < 2 else sqrt(max(0, x.m2) / (x.n - 1))
   return v
 
@@ -103,13 +101,13 @@ def norm(col,x):
   return x if x=="?" or col.it is Sym else (x-col.lo)/(col.hi-col.lo+1/BIG)
 
 def poles(data,rows):
-   D = lambda r1,r2: distx(data, r1, r2)
-   x,*rest = random.choices(rows, few=the.Few)
-   y = max(rest, key=lambda r: D(r,x))
-   z = max(rest, key=lambda r: D(r,y))
-   c = D(y,z)
-   P = lambda r: (D(r,y)**2 + c**2 - D(r,y)**2)/ (2 * c) <= c/2
-   return [(row, P(row)) for row in rows]
+  D = lambda r1,r2: distx(data, r1, r2)
+  x,*rest = random.choices(rows, few=the.Few)
+  y = max(rest, key=lambda r: D(r,x))
+  z = max(rest, key=lambda r: D(r,y))
+  c = D(y,z)
+  P = lambda r: (D(r,y)**2 + c**2 - D(r,y)**2)/ (2 * c) <= c/2
+  return {id(row):P(row) for row in rows}
 
 #------------------------------------------------------------------------------
 ### Discretize
