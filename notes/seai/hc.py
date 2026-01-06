8ca50d5aaeb21a4527e58d7f666b6e2452a07827
timm <timm@ieee.org>
2026-01-06 14:34:01 -0500

    saving

--- notes/seai/hc.py
+++ notes/seai/hc.py
@@ -1,20 +1,37 @@
+#!/usr/bin/env python3 -B
 import random, sys, xai
+from xai import distx as x
+from xai import disty as y
+from xai import shuffle,Data,csv
+
 xai.the.data=sys.argv[2]
 random.seed(int(sys.argv[1]))
 
-data = xai.Data(xai.csv(xai.the.data))
+data = Data(csv(xai.the.data))
 
-def Y(r): return round(xai.disty(data,r),2)
+def Y(r): return round(y(data,r),2)
 
 def report(rows): 
-    a=sorted(rows[:],key=Y); print( len(a),"\t",Y(a[0]),Y(a[len(a)//2]))
+    a=sorted(rows[:],key=Y)
+    n=len(a)//10
+    sd = (Y(a[9*n]) - Y(a[n]))/2.56
+    print( f"{Y(a[0]):2}, {sd:.2f}", end=" ")
+    return sd*0.35
 
-labelled, rows = [], data.rows
-report(rows)
-while len(labelled) < 30:
-  labelled = labelled + xai.shuffle(rows)[:10]
-  best = min(labelled,key=Y)
-  X=lambda r:xai.distx(data,r,best)
-  rows = sorted(rows,key=X)[:len(rows)//2]
+for _ in range(20):
+  labelled, rows = [], shuffle(data.rows)
+  eps= report(rows)
+  last=1e32
+  while len(labelled) < 30:
+    labelled = labelled + shuffle(rows)[:8]
+    labelled.sort(key=Y)
+    n = len(labelled)//10
+    ok,no = labelled[n], labelled[9*n]
+    #ok,no = labelled[0], labelled[-1]
+    c= x(data,ok,no)
+    #d= lambda r: (x(data,r,ok)
+    d= lambda r: (x(data,r,ok)**2 + c*c - x(data,r,no)**2)/(2*c + 1e-32)
+    rows = sorted(rows,key=d)[:len(rows)//2]
+
   report(labelled)
-
+  print("")
